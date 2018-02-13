35ed4de3b6b4aee8838d929920e2e043cd737a7f
Benjamin <ben@privategrity.com>
2018-02-13 15:12:57 -0800

    Added the user structure with five hardcoded users.  It is a copy of the structure in server with significant components removed for simplicity (this ticket is blocking)

---
+++ globals/user_test.go
@@ -0,0 +1,45 @@
+package globals
+
+import (
+	"testing"
+)
+
+// TestUserRegistry tests the constructors/getters/setters
+// surrounding the User struct and the UserRegistry interface
+func TestUserRegistry(t *testing.T) {
+
+	if Users.CountUsers() != 5 {
+		t.Errorf("CountUsers: Start size of userRegistry not zero!")
+	}
+
+	usr := Users.GetUser(0)
+
+	if usr.Nick != "Phineas Flynn" {
+		t.Errorf("User 0 is not 'Phineas Flynn'")
+	}
+
+	usr = Users.GetUser(1)
+
+	if usr.Nick != "Ferb Flynn" {
+		t.Errorf("User 1 is not 'Ferb Flynn'")
+	}
+
+	usr = Users.GetUser(2)
+
+	if usr.Nick != "Cadance Flynn" {
+		t.Errorf("User 2 is not 'Cadance Flynn'")
+	}
+
+	usr = Users.GetUser(3)
+
+	if usr.Nick != "Perry the Platypus" {
+		t.Errorf("User 3 is not 'Perry the Platypus'")
+	}
+
+	usr = Users.GetUser(4)
+
+	if usr.Nick != "Heinz Doofenshmirtz" {
+		t.Errorf("User 4 is not 'Heinz Doofenshmirtz'")
+	}
+
+}

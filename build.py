6a29432451758a1f996d5b094d9fc775dd662448
Spencer Brown <spencer@privategrity.com>
2018-02-13 09:47:43 -0800

    Initial build and initialize scripts for the mobile apps.

---
+++ build.py
@@ -0,0 +1,6 @@
+#!/bin/env python3
+
+import os
+
+# Run this script to re-generate the .aar build artifact for the Android app.
+
---
+++ initialize.py
@@ -0,0 +1,7 @@
+#!/bin/env python3
+
+import argparse
+
+# This script will download and install, or use local versions of, the Android
+# NDK and gomobile. These are needed to build the client for mobile but they
+# can't be installed with glide for various dumb reasons.

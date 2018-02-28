d44ac95398d35266454ca10ecb47a35b5ca61d85
Rick Carback <rick@privategrity.com>
2018-02-28 23:33:18 +0000

    Add a disconnect function which calls the comms disconnect against the given server address

---
+++ io/disconnect.go
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package io
+
+import (
+	"gitlab.com/privategrity/comms/mixclient"
+)
+
+// Disconnect from the server
+func Disconnect(address string) {
+	mixclient.Disconnect(string)
+}

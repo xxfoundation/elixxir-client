8042b5057ababc7e424078ae629e191c21c2089b
willcarter <will@privategrity.com>
2018-03-06 13:51:12 -0800

    Fixed test errors

--- globals/user_test.go
+++ globals/user_test.go
@@ -8,13 +8,14 @@ package globals
 
 import (
 	"testing"
+	"gitlab.com/privategrity/server/globals"
 )
 
 // TestUserRegistry tests the constructors/getters/setters
 // surrounding the User struct and the UserRegistry interface
 func TestUserRegistry(t *testing.T) {
 
-	if Users.CountUsers() != 1000 {
+	if Users.CountUsers() != DEMO_USERS {
 		t.Errorf("CountUsers: Start size of userRegistry not zero!")
 	}
 
@@ -33,19 +34,19 @@ func TestUserRegistry(t *testing.T) {
 	reg, _ = Users.LookupUser(10003)
 	usr, _ = Users.GetUser(reg)
 	if usr.Nick != "Ben" {
-		t.Errorf("User 10008 is not 'Will'")
+		t.Errorf("User 10003 is not 'Ben'")
 	}
 
 	reg, _ = Users.LookupUser(10004)
 	usr, _ = Users.GetUser(reg)
 	if usr.Nick != "Rick" {
-		t.Errorf("User 10003 is not 'Ben'")
+		t.Errorf("User 10004 is not 'Rick'")
 	}
 
 	reg, _ = Users.LookupUser(10005)
 	usr, _ = Users.GetUser(reg)
 	if usr.Nick != "Spencer" {
-		t.Errorf("User 10004 is not 'Rick'")
+		t.Errorf("User 10005 is not 'Spencer'")
 	}
 
 }

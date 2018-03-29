6c535c78a4b95b97b2e810f4b5e71c53ba30615a
Spencer Brown <spencer@privategrity.com>
2018-03-29 14:37:19 -0700

    Automatically break up channel messages that are too long

--- channel/message.go
+++ channel/message.go
@@ -4,15 +4,17 @@ import (
 	"bytes"
 	"encoding/gob"
 	jww "github.com/spf13/jwalterweatherman"
+	"gitlab.com/privategrity/crypto/format"
 )
 
 // This is the message type that the subscribers to the channel send to the
 // channel bot, by serialization.
 type ChannelMessage struct {
 	GroupID uint64
-	// TODO is this the same as the user ID or should each channel map user IDs to speaker IDs somehow
+	// This is the same as the user ID of the person who sent the message to
+	// the channel
 	SpeakerID uint64
-	// TODO handle longer channel messages
+	// This holds the actual message.
 	Message string
 }
 
@@ -28,7 +30,7 @@ func (m ChannelMessage) SerializeChannelMessage() *bytes.Buffer {
 }
 
 func ParseChannelMessage(serializedChannelMessage *bytes.
-	Buffer) *ChannelMessage {
+Buffer) *ChannelMessage {
 	dec := gob.NewDecoder(serializedChannelMessage)
 	var result ChannelMessage
 	err := dec.Decode(&result)
@@ -36,4 +38,36 @@ func ParseChannelMessage(serializedChannelMessage *bytes.
 		jww.ERROR.Printf("Failed to decode gob for channel message: %v", err.Error())
 	}
 	return &result
+}
+
+func NewSerializedChannelMessages(GroupID, SpeakerID uint64,
+	Message string) []*bytes.Buffer {
+	// Try to serialize a gob with the message fields left untouched,
+	// and see if it fits in the length of a message payload
+	length := ChannelMessage{GroupID: GroupID, SpeakerID: SpeakerID,
+		Message: Message}.SerializeChannelMessage()
+
+	if uint64(length.Len()) > format.DATA_LEN {
+		// If in this loop, the gob was too long and we need to break the
+		// message up into multiple messages
+		// TODO: we should have some sort of ordering guarantee for
+		// sending these, like a unique identifier for the group of messages
+		result := make([]*bytes.Buffer, 0)
+		for uint64(length.Len()) > format.DATA_LEN {
+			partition := uint64(length.Len()) - format.DATA_LEN
+			nextMessagePayload := Message[partition:]
+			Message = Message[:partition]
+
+			nextChannelMessage := ChannelMessage{GroupID: GroupID,
+				SpeakerID: SpeakerID, Message: nextMessagePayload}.SerializeChannelMessage()
+			// prepend the resulting message
+			result = append([]*bytes.Buffer{nextChannelMessage}, result...)
+			length = ChannelMessage{GroupID: GroupID, SpeakerID: SpeakerID,
+				Message: Message}.SerializeChannelMessage()
+		}
+		result = append([]*bytes.Buffer{length}, result...)
+		return result
+	} else {
+		return []*bytes.Buffer{length}
+	}
 }
--- channel/message_test.go
+++ channel/message_test.go
@@ -25,20 +25,91 @@ func TestChannelMessageSerializationAndParsing(t *testing.T) {
 	}
 }
 
-func TestChannelMessageInNormalPayload(t *testing.T) {
-	expected := ChannelMessage{1, 5, "what do you guys think about straws?"}
-	serialization := expected.SerializeChannelMessage()
-	// in this example, the bot would be user 8 and would be sending to user 5,
-	// the only user in the channel
-	// TODO automate the generation of these messages to channel subscribers
-	messages, err := format.NewMessage(8, 5, serialization.String())
-	if err != nil {
-		t.Errorf(err.Error())
+func TestNewSerializedChannelMessages(t *testing.T) {
+	longMessageToChannel := "Beginning pretium venenatis dui vitae rhoncus. " +
+		"Nunc ut lorem id arcu eleifend porta ac a orci. " +
+		"Quisque mattis maximus porta. Sed congue, " +
+		"libero in ornare tincidunt, felis nunc tincidunt odio, " +
+		"eu pharetra nisi tortor non mauris. In nunc odio, " +
+		"vehicula eget dolor a, pretium fringilla lectus. " +
+		"Sed at placerat neque. Nulla pellentesque vestibulum nulla quis" +
+		" vulputate. Quisque ut tellus a orci vehicula facilisis. " +
+		"Aliquam pretium venenatis dui vitae rhoncus. " +
+		"Nunc ut lorem id arcu eleifend porta ac a orci. " +
+		"Quisque mattis maximus porta. Sed congue, " +
+		"libero in ornare tincidunt, felis nunc tincidunt odio, " +
+		"eu pharetra nisi tortor non mauris. In nunc odio, " +
+		"vehicula eget dolor a, pretium fringilla lectus. " +
+		"Sed at placerat neque. Nulla pellentesque vestibulum nulla quis" +
+		" vulputate. Quisque ut tellus a orci vehicula facilisis." +
+		"Nunc ut lorem id arcu eleifend porta ac a orci. " +
+		"Quisque mattis maximus porta. Sed congue, " +
+		"libero in ornare tincidunt, felis nunc tincidunt odio, " +
+		"eu pharetra nisi tortor non mauris. In nunc odio, " +
+		"vehicula eget dolor a, pretium fringilla lectus. " +
+		"Sed at placerat neque. Nulla pellentesque vestibulum nulla quis" +
+		" vulputate. Quisque ut tellus a orci vehicula facilisis. " +
+		"Aliquam pretium venenatis dui vitae rhoncus. " +
+		"Nunc ut lorem id arcu eleifend porta ac a orci. " +
+		"Quisque mattis maximus porta. Sed congue, " +
+		"libero in ornare tincidunt, felis nunc tincidunt odio, " +
+		"eu pharetra nisi tortor non mauris. In nunc odio, " +
+		"vehicula eget dolor a, pretium fringilla lectus. " +
+		"Sed at placerat neque. Nulla pellentesque vestibulum nulla quis end."
+
+	multipleSerializedMessages := NewSerializedChannelMessages(1, 5, longMessageToChannel)
+
+	expectedMessages := []string{
+		"Beginning pretium venenatis dui vitae rhoncus. " +
+			"Nunc ut lorem id arcu eleifend porta ac a orci. " +
+			"Quisque mattis maximus porta. Sed congue, " +
+			"libero in ornare tincidunt, felis nunc tincidunt odio, " +
+			"eu pharetra nisi tortor non mauris. In nunc odio, " +
+			"vehicula eget dolor a, pretium fringilla lectus. " +
+			"Sed at placerat neque. Nulla pe",
+		"llentesque vestibulum nulla quis vulputate. " +
+			"Quisque ut tellus a orci vehicula facilisis. " +
+			"Aliquam pretium venenatis dui vitae rhoncus. " +
+			"Nunc ut lorem id arcu eleifend porta ac a orci. " +
+			"Quisque mattis maximus porta. Sed congue, " +
+			"libero in ornare tincidunt, felis nunc tincidunt odio, " +
+			"eu pharetra nisi tortor non mauris. In nunc odio, " +
+			"vehicula eget dolor a, pretium fringilla lectus. " +
+			"Sed at placerat neque. Nulla",
+		" pellentesque vestibulum nulla quis vulputate. " +
+			"Quisque ut tellus a orci vehicula facilisis." +
+			"Nunc ut lorem id arcu eleifend porta ac a orci. " +
+			"Quisque mattis maximus porta. Sed congue, " +
+			"libero in ornare tincidunt, felis nunc tincidunt odio, " +
+			"eu pharetra nisi tortor non mauris. In nunc odio, " +
+			"vehicula eget dolor a, pretium fringilla lectus. " +
+			"Sed at placerat neque. " +
+			"Nulla pellentesque vestibulum nulla quis vulputa",
+		"te. Quisque ut tellus a orci vehicula facilisis. " +
+			"Aliquam pretium venenatis dui vitae rhoncus. " +
+			"Nunc ut lorem id arcu eleifend porta ac a orci. " +
+			"Quisque mattis maximus porta. Sed congue, " +
+			"libero in ornare tincidunt, felis nunc tincidunt odio, " +
+			"eu pharetra nisi tortor non mauris. In nunc odio, " +
+			"vehicula eget dolor a, pretium fringilla lectus. " +
+			"Sed at placerat neque. " +
+			"Nulla pellentesque vestibulum nulla quis end.",
 	}
-	if len(messages) > 1 {
-		// the message was too long for one buffer. this is the condition that
-		// automating the message generation will ameliorate
-		t.Errorf("Too many messages: %v messages generated", len(messages))
+	// if there isn't too much metadata embedded in the channel messages,
+	// you can expect this number of submessages to be needed.
+	expectedNumberOfMessages := uint64(len(longMessageToChannel)) / format.
+		DATA_LEN + 1
+
+	if expectedNumberOfMessages != uint64(len(multipleSerializedMessages)) {
+		t.Errorf("Got a different number of messages than expected. Got: %v," +
+			" expected %v.")
 	}
-	t.Logf("First message string: %v", messages[0].GetPayload())
+	for i := range multipleSerializedMessages {
+		result := ParseChannelMessage(multipleSerializedMessages[i])
+		if result.Message != expectedMessages[i] {
+			t.Errorf("Got a different sub-message than expected at %v. " +
+				"Got: %v..., expected %v...", i, result.Message[:10],
+					expectedMessages[i][:10])
+		}
+	}
 }

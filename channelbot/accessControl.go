e9344b3d9e724485682b75f751f5e5e152e50a81
Spencer Brown <spencer@privategrity.com>
2018-03-30 15:01:09 -0700

    Remove 10 from hardcoded channel users to make it easier to test manually

--- channelbot/accessControl.go
+++ channelbot/accessControl.go
@@ -1,7 +1,7 @@
 package channelbot
 
 // everyone's in every channel for now
-var subscribers = []uint64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+var subscribers = []uint64{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 func AddSubscriber(newSubscriber uint64) {
 	subscribers = append(subscribers, newSubscriber)

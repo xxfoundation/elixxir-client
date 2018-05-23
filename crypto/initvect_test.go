3b6a912f34e28420c713168cad3c454320937826
Rick Carback <rick@privategrity.com>
2018-05-23 19:41:55 +0000

    Move initvect code to crypto module

---
+++ crypto/initvect_test.go
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2018 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package crypto
+
+import (
+	"gitlab.com/privategrity/crypto/cyclic"
+	"testing"
+)
+
+// Smoke test for MakeInitVect
+func TestMakeInitVect(t *testing.T) {
+	InitCrypto()
+	tests := 100
+	min := cyclic.NewInt(2)
+	max := cyclic.NewIntFromString("7FFFFFFFFFFFFFFFFF", 16)
+	for i := 0; i < tests; i++ {
+		rand := MakeInitVect(cyclic.NewInt(0))
+		if rand.Cmp(min) < 0 || rand.Cmp(max) >= 0 {
+			t.Error("MakeInitVector is out of range.")
+		}
+	}
+}

e4742f84613af29c0acece9df337c3ccb9cb902b
Rick Carback <rick@privategrity.com>
2018-05-25 22:29:54 +0000

    Add Search functionality

--- bots/userDiscovery.go
+++ bots/userDiscovery.go
@@ -13,6 +13,8 @@ import (
 	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/privategrity/client/io"
 	"gitlab.com/privategrity/crypto/hash"
+	"strconv"
+	"strings"
 )
 
 // UdbID is the ID of the user discovery bot, which is always 13
@@ -41,10 +43,75 @@ func Register(valueType, value string, publicKey []byte) error {
 	return nil
 }
 
-// Returns a list of users matching the search criteria, or nil if
-// none were found.
-func Search(valueType, value string) []string {
-	return nil
+// Search returns a userID and public key based on the search criteria
+func Search(valueType, value string) (map[uint64][]byte, error) {
+	response := sendCommand(udbID, fmt.Sprintf("SEARCH %s %s", valueType, value))
+	empty := fmt.Sprintf("SEARCH %s NOTFOUND", value)
+	if response == empty {
+		return nil, nil
+	}
+	// While search returns more than 1 result, we only process the first
+	cMixUID, keyFP := parseSearch(response)
+	if cMixUID == 0 {
+		return nil, fmt.Errorf("%s", keyFP)
+	}
+
+	// Get the full key and decode it
+	responses := sendCommandMulti(2, udbID, fmt.Sprintf("GETKEY %s", keyFP))
+	publicKey := make([]byte, 256)
+	for i := 0; i < 2; i++ {
+		idx, keymat := parseGetKey(responses[i])
+		for j := range keymat {
+			publicKey[j+idx] = keymat[j]
+		}
+
+	}
+
+	actualFP := fingerprint(publicKey)
+	if keyFP != actualFP {
+		return nil, fmt.Errorf("Fingerprint for %s did not match %s!", keyFP,
+			actualFP)
+	}
+
+	retval := make(map[uint64][]byte)
+	retval[cMixUID] = publicKey
+
+	return retval, nil
+}
+
+func parseSearch(msg string) (uint64, string) {
+	resParts := strings.Split(msg, " ")
+	if len(resParts) != 5 {
+		return 0, fmt.Sprintf("Invalid response from search: %s", msg)
+	}
+
+	cMixUID, err := strconv.ParseUint(resParts[3], 10, 64)
+	if err != nil {
+		return 0, fmt.Sprintf("Couldn't parse search cMix UID: %s", msg)
+	}
+
+	return cMixUID, resParts[4]
+}
+
+func parseGetKey(msg string) (int, []byte) {
+	resParts := strings.Split(msg, " ")
+	if len(resParts) != 4 {
+		jww.WARN.Printf("Invalid response from GETKEY: %s", msg)
+		return -1, nil
+	}
+
+	idx, err := strconv.ParseInt(resParts[2], 10, 32)
+	if err != nil {
+		jww.WARN.Printf("Couldn't parse GETKEY Index: %s", msg)
+		return -1, nil
+	}
+	keymat, err := base64.StdEncoding.DecodeString(resParts[3])
+	if err != nil || len(keymat) != 128 {
+		jww.WARN.Printf("Couldn't decode GETKEY keymat: %s", msg)
+		return -1, nil
+	}
+
+	return int(idx), keymat
 }
 
 func pushKey(udbID uint64, keyFP string, publicKey []byte) error {
@@ -90,4 +157,23 @@ func sendCommand(botID uint64, command string) string {
 	response := <-listener
 	jww.ERROR.Printf(response.GetPayload())
 	return response.GetPayload()
+}
+
+func sendCommandMulti(responseCnt int, botID uint64, command string) []string {
+	listener := io.Messaging.Listen(botID)
+	defer io.Messaging.StopListening(listener)
+	err := io.Messaging.SendMessage(botID, command)
+
+	responses := make([]string, 0)
+	if err != nil {
+		responses = append(responses, err.Error())
+		return responses
+	}
+
+	for i := 0; i < responseCnt; i++ {
+		response := <-listener
+		jww.ERROR.Printf(response.GetPayload())
+		responses = append(responses, response.GetPayload())
+	}
+	return responses
 }

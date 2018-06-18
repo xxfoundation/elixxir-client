aa62871dc31e5bd67ea6522ee9fd1e2a36645f3c
Spencer Brown <spencer@privategrity.com>
2018-06-18 14:31:05 -0700

    Reintegrate user ID type for mock messaging

--- bindings/client_test.go
+++ bindings/client_test.go
@@ -34,14 +34,14 @@ type dummyMessaging struct {
 }
 
 // SendMessage to the server
-func (d *dummyMessaging) SendMessage(recipientID uint64, message string) error {
+func (d *dummyMessaging) SendMessage(recipientID globals.UserID, message string) error {
 	jww.INFO.Printf("Sending: %s", message)
 	lastmsg = message
 	return nil
 }
 
 // Listen for messages from a given sender
-func (d *dummyMessaging) Listen(senderID uint64) chan *format.Message {
+func (d *dummyMessaging) Listen(senderID globals.UserID) chan *format.Message {
 	return d.listener
 }
 

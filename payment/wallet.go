b20e392f289368af2bdb00bbb405b9f76e51cd40
Spencer Brown <spencer@privategrity.com>
2018-07-18 16:42:13 -0700

    Add listener for invoice messages

--- payment/wallet.go
+++ payment/wallet.go
@@ -10,6 +10,8 @@ import (
 	"gitlab.com/privategrity/client/parse"
 	"time"
 	"github.com/golang/protobuf/proto"
+	"gitlab.com/privategrity/crypto/coin"
+	jww "github.com/spf13/jwalterweatherman"
 )
 
 const CoinStorageTag string = "CoinStorage"
@@ -53,6 +55,7 @@ func NewWallet() (*Wallet, error) {
 	return &Wallet{coinStorage: cs, outboundRequests: obr, inboundRequests: ibr, pendingTransactions: pt}, nil
 }
 
+// FIXME Limit this to one part message (requires message ID revamp for accuracy)
 func (t *Transaction) FormatInvoice() (*parse.Message, error) {
 	invoice := parse.PaymentInvoice{
 		Time:         time.Now().Unix(),
@@ -73,7 +76,49 @@ func (t *Transaction) FormatInvoice() (*parse.Message, error) {
 		TypedBody: typedBody,
 		Sender:    t.Sender,
 		Receiver:  t.Recipient,
-		// TODO populate nonce and panic if outgoing message doesn't have one
+		// TODO populate nonce and panic if any outgoing message has none
 		Nonce: nil,
 	}, nil
+}
+
+func (w *Wallet) HearInboundRequest(msg *parse.Message) {
+	var invoice parse.PaymentInvoice
+
+	// Don't humor people who send malformed messages
+	if err := proto.Unmarshal(msg.Body, &invoice); err != nil {
+		jww.WARN.Printf("Got error unmarshaling inbound invoice: %v", err.Error())
+		return
+	}
+
+	if !coin.IsCompound(invoice.CreatedCoins) {
+		jww.WARN.Printf("Got an invoice with an incorrect coin type")
+		return
+	}
+
+	// Convert the message to a compound
+	var compound coin.Compound
+	copy(compound[:], invoice.CreatedCoins)
+
+	transaction := &Transaction{
+		Create:      coin.ConstructSleeve(nil, &compound),
+		Destroy:     nil,
+		Change:      NilSleeve,
+		Sender:      msg.Sender,
+		Recipient:   msg.Receiver,
+		Description: invoice.Memo,
+		Timestamp:   time.Unix(invoice.Time, 0),
+		Value:       compound.Value(),
+	}
+
+	w.inboundRequests.Add(msg.Hash(), transaction)
+}
+
+// This listener hears incoming invoices and puts them on the list
+// TODO hook the wallet up to the listeners somewhere
+func (w *Wallet) Hear(msg *parse.Message, isHeardElsewhere bool) {
+	switch msg.Type {
+	case parse.Type_PAYMENT_INVOICE:
+		w.HearInboundRequest(msg)
+		break
+	}
 }

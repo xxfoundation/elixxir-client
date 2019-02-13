b22c8ec5d2d31a0e410bcadb27d28a227b16501e
willcarter <will@privategrity.com>
2019-02-13 11:24:52 -0800

    Added new users and re-added UDB and Payment Boty

--- user/user.go
+++ user/user.go
@@ -18,8 +18,10 @@ var Users = newRegistry()
 
 const NUM_DEMO_USERS = 40
 
-var DemoUserNicks = []string{"David", "Jim", "Ben", "Rick", "Spencer", "Jake",
-	"Mario", "Will", "Allan", "Jono", "", "", "UDB", "", "", "", "Payments"}
+var DemoUserNicks = []string{"David", "Payments", "UDB", "Jim", "Ben", "Steph",
+	"Rick", "Jake", "Spencer", "Stephanie", "Mario", "Jono", "Amanda",
+	"Margaux", "Kevin", "Bruno", "Konstantino", "Bernardo", "Tigran",
+	"Kate", "Will", "Katie", "Bryan"}
 var DemoChannelNames = []string{"#General", "#Engineering", "#Lunch",
 	"#Random"}
 

2735b3803b2acd72ca68ab1841dd0fd24ae5d4d4
josh <josh@elixxir.io>
2020-02-10 15:40:39 -0800

    Remove conditional, instead have it poll for ndf directly

--- io/updateNdf.go
+++ io/updateNdf.go
@@ -29,11 +29,10 @@ func PollNdf(currentDef *ndf.NetworkDefinition, comms *client.Comms) (*ndf.Netwo
 	//Send the hash to registration
 
 	response, err := comms.RequestNdf(regHost, msg)
-	if err != nil {
-		for err != nil && strings.Contains(err.Error(), ndf.NO_NDF) {
-			globals.Log.INFO.Printf("Failed to get an ndf, possibly not ready yet. Retying now...")
-			response, err = comms.RequestNdf(regHost, msg)
-		}
+	// Retry until we no longer receive an error from
+	for err != nil && strings.Contains(err.Error(), ndf.NO_NDF) {
+		globals.Log.INFO.Printf("Failed to get an ndf, possibly not ready yet. Retying now...")
+		response, err = comms.RequestNdf(regHost, msg)
 	}
 
 	//If there was no error and the response is nil, client's ndf is up-to-date

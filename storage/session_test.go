dc3e4159781358384ae3d5816611b65069a053ee
jbhusson <jonah@elixxir.io>
2020-08-04 13:12:16 -0400

    re-enable session smoke test

--- storage/session_test.go
+++ storage/session_test.go
@@ -1,8 +1,10 @@
 package storage
 
 import (
+	"bytes"
 	"os"
 	"testing"
+	"time"
 )
 
 // Smoke test for session object init/set/get methods
@@ -16,29 +18,29 @@ func TestSession_Smoke(t *testing.T) {
 		t.Log(s)
 		t.Errorf("failed to init: %+v", err)
 	}
-	/*
-		ts, err := time.Now().MarshalText()
-		if err != nil {
-			t.Errorf("Failed to martial time for object")
-		}
-		err = s.Set("testkey", &VersionedObject{
-			Version:   0,
-			Timestamp: ts,
-			Data:      []byte("test"),
-		})
-		if err != nil {
-			t.Errorf("Failed to set: %+v", err)
-		}
-		o, err := s.Get("testkey")
-		if err != nil {
-			t.Errorf("Failed to get key")
-		}
-		if o == nil {
-			t.Errorf("Got nil return from get")
-		}
-		t.Log(o)
-		if bytes.Compare(o.Data, []byte("test")) != 0 {
-			t.Errorf("Failed to get data")
-		}
-	*/
+
+	ts, err := time.Now().MarshalText()
+	if err != nil {
+		t.Errorf("Failed to martial time for object")
+	}
+	err = s.Set("testkey", &VersionedObject{
+		Version:   0,
+		Timestamp: ts,
+		Data:      []byte("test"),
+	})
+	if err != nil {
+		t.Errorf("Failed to set: %+v", err)
+	}
+	o, err := s.Get("testkey")
+	if err != nil {
+		t.Errorf("Failed to get key")
+	}
+	if o == nil {
+		t.Errorf("Got nil return from get")
+	}
+
+	if bytes.Compare(o.Data, []byte("test")) != 0 {
+		t.Errorf("Failed to get data")
+	}
+
 }

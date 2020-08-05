b77cc2949d2ee3d88c967c9e87069f4e6188820c
jbhusson <jonah@elixxir.io>
2020-08-04 20:12:51 -0400

    add comments & copyright header to session code

--- storage/session.go
+++ storage/session.go
@@ -1,11 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+// Session object definition
+
 package storage
 
 import "gitlab.com/elixxir/ekv"
 
+// Session object, backed by encrypted filestore
 type Session struct {
 	kv *VersionedKV
 }
 
+// Initialize a new Session object
 func Init(baseDir, password string) (*Session, error) {
 	fs, err := ekv.NewFilestore(baseDir, password)
 	var s *Session
@@ -18,10 +28,12 @@ func Init(baseDir, password string) (*Session, error) {
 	return s, err
 }
 
+// Get an object from the session
 func (s *Session) Get(key string) (*VersionedObject, error) {
 	return s.kv.Get(key)
 }
 
+// Set a value in the session
 func (s *Session) Set(key string, object *VersionedObject) error {
 	return s.kv.Set(key, object)
 }

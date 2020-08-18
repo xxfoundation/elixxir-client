88cbb3908d76a1daf6f97c3f0c569f563d1078fc
Sydney Anne Erickson <sydney@elixxir.io>
2020-08-18 10:51:25 -0700

    Add params.go and status.go tests for Key

---
+++ key/params_test.go
@@ -0,0 +1,25 @@
+package key
+
+// Testing file for the params.go functions
+
+import "testing"
+
+// Test that the GetDefaultParams function returns the right default data
+func Test_GetDefaultParams(t *testing.T) {
+	p := GetDefaultParams()
+	if p.MinKeys != minKeys {
+		t.Errorf("MinKeys mismatch\r\tGot: %d\r\tExpected: %d", p.MinKeys, minKeys)
+	}
+	if p.MaxKeys != maxKeys {
+		t.Errorf("MinKeys mismatch\r\tGot: %d\r\tExpected: %d", p.MaxKeys, maxKeys)
+	}
+	if p.NumRekeys != numReKeys {
+		t.Errorf("MinKeys mismatch\r\tGot: %d\r\tExpected: %d", p.NumRekeys, numReKeys)
+	}
+	if p.TTLScalar != ttlScalar {
+		t.Errorf("MinKeys mismatch\r\tGot: %d\r\tExpected: %d", p.TTLScalar, ttlScalar)
+	}
+	if p.MinNumKeys != threshold {
+		t.Errorf("MinKeys mismatch\r\tGot: %d\r\tExpected: %d", p.MinNumKeys, threshold)
+	}
+}

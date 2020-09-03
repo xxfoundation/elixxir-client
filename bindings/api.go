1f07400d7435cab86a68bf696996b8f6ed8005ab
Richard T. Carback III <rick.carback@gmail.com>
2020-09-03 23:28:27 +0000

    Beginnings of new API, starting with definitions in bindings

---
+++ bindings/api.go
@@ -0,0 +1,43 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2019 Privategrity Corporation                                   /
+//                                                                             /
+// All rights reserved.                                                        /
+////////////////////////////////////////////////////////////////////////////////
+
+package bindings
+
+import (
+	"gitlab.com/elixxir/client/api"
+)
+
+// NewClient connects and registers to the network using a json encoded
+// network information string and then creates a new client at the specified
+// storageDir using the specified password. This function will fail
+// when:
+//   - network information cannot be read or the client cannot connect
+//     to the network and register within the defined timeout.
+//   - storageDir does not exist and cannot be created
+//   - It cannot create, read, or write files inside storageDir
+//   - Client files already exist inside storageDir.
+//   - cryptographic functionality is unavailable (e.g. random number
+//     generation)
+// The password is passed as a byte array so that it can be cleared from
+// memory and stored as securely as possible using the memguard library.
+// NewClient will block until the client has completed registration with
+// the network permissioning server.
+func NewClient(network, storageDir string, password []byte) (Client, error) {
+	// TODO: This should wrap the bindings ClientImpl, when available.
+	return api.NewClient(network, storageDir, password)
+}
+
+// LoadClient will load an existing client from the storageDir
+// using the password. This will fail if the client doesn't exist or
+// the password is incorrect.
+// The password is passed as a byte array so that it can be cleared from
+// memory and stored as securely as possible using the memguard library.
+// LoadClient does not block on network connection, and instead loads and
+// starts subprocesses to perform network operations.
+func LoadClient(storageDir string, password []byte) (Client, error) {
+	// TODO: This should wrap the bindings ClientImpl, when available.
+	return api.LoadClient(storageDir, password)
+}
--- bindings/interfaces.go
+++ bindings/interfaces.go
@@ -11,14 +11,133 @@ import (
 	"gitlab.com/elixxir/primitives/switchboard"
 )
 
-// Message used for binding
+// Client is defined inside the api package. At minimum, it implements all of
+// functionality defined here. A Client handles all network connectivity, key
+// generation, and storage for a given cryptographic identity on the cmix
+// network.
+type Client interface {
+
+	// ----- Reception -----
+
+	// RegisterListener records and installs a listener for messages
+	// matching specific uid, msgType, and/or username
+	RegisterListener(uid []byte, msgType int, username string,
+		listener Listener)
+
+	// ----- Transmission -----
+
+	// SendE2E sends an end-to-end payload to the provided recipient with
+	// the provided msgType. Returns the list of rounds in which parts of
+	// the message were sent or an error if it fails.
+	SendE2E(payload, recipient []byte, msgType int) (RoundList, error)
+	// SendUnsafe sends an unencrypted payload to the provided recipient
+	// with the provided msgType. Returns the list of rounds in which parts
+	// of the message were sent or an error if it fails.
+	// NOTE: Do not use this function unless you know what you are doing.
+	// This function always produces an error message in client logging.
+	SendUnsafe(payload, recipient []byte, msgType int) (RoundList, error)
+	// SendCMIX sends a "raw" CMIX message payload to the provided
+	// recipient. Note that both SendE2E and SendUnsafe call SendCMIX.
+	// Returns the round ID of the round the payload was sent or an error
+	// if it fails.
+	SendCMIX(payload, recipient []byte) (int, error)
+
+	// ----- Notifications -----
+
+	// RegisterForNotifications allows a client to register for push
+	// notifications.
+	// Note that clients are not required to register for push notifications
+	// especially as these rely on third parties (i.e., Firebase *cough*
+	// *cough* google's palantir *cough*) that may represent a security
+	// risk to the user.
+	RegisterForNotifications(token []byte) error
+	// UnregisterForNotifications turns of notifications for this client
+	UnregisterForNotifications() error
+
+	// ----- Registration -----
+
+	// Returns true if the cryptographic identity has been registered with
+	// the CMIX user discovery agent.
+	// Note that clients do not need to perform this step if they use
+	// out of band methods to exchange cryptographic identities
+	// (e.g., QR codes), but failing to be registered precludes usage
+	// of the user discovery mechanism (this may be preferred by user).
+	IsRegistered() bool
+
+	// RegisterIdentity registers an arbitrary username with the user
+	// discovery protocol. Returns an error when it cannot connect or
+	// the username is already registered.
+	RegisterIdentity(username string) error
+	// RegisterEmail makes the users email searchable after confirmation.
+	// It returns a registration confirmation token to be used with
+	// ConfirmRegistration or an error on failure.
+	RegisterEmail(email string) ([]byte, error)
+	// RegisterPhone makes the users phone searchable after confirmation.
+	// It returns a registration confirmation token to be used with
+	// ConfirmRegistration or an error on failure.
+	RegisterPhone(phone string) ([]byte, error)
+	// ConfirmRegistration sends the user discovery agent a confirmation
+	// token (from Register Email/Phone) and code (string sent via Email
+	// or SMS to confirm ownership) to confirm ownership.
+	ConfirmRegistration(token, code []byte) error
+
+	// ----- Contacts -----
+
+	// GetUser returns the current user Identity for this client. This
+	// can be serialized into a byte stream for out-of-band sharing.
+	GetUser() (Contact, error)
+	// MakeContact creates a contact from a byte stream (i.e., unmarshal's a
+	// Contact object), allowing out-of-band import of identities.
+	MakeContact(contactBytes []byte) (Contact, error)
+	// GetContact returns a Contact object for the given user id, or
+	// an error
+	GetContact(uid []byte) (Contact, error)
+
+	// ----- User Discovery -----
+
+	// Search accepts a "separator" separated list of search elements with
+	// an associated list of searchTypes. It returns a ContactList which
+	// allows you to iterate over the found contact objects.
+	Search(data, separator string, searchTypes []byte) ContactList
+	// SearchWithHandler is a non-blocking search that also registers
+	// a callback interface for user disovery events.
+	SearchWithHandler(data, separator string, searchTypes []byte,
+		hdlr UserDiscoveryHandler)
+
+	// ----- Key Exchange -----
+
+	// CreateAuthenticatedChannel creates a 1-way authenticated channel
+	// so this user can send messages to the desired recipient Contact.
+	// To receive confirmation from the remote user, clients must
+	// register a listener to do that.
+	CreateAuthenticatedChannel(recipient Contact, payload []byte) error
+	// RegierAuthEventsHandler registers a callback interface for channel
+	// authentication events.
+	RegisterAuthEventsHandler(hdlr AuthEventHandler)
+
+	// ----- Network -----
+
+	// StartNetworkRunner kicks off the longrunning network client threads
+	// and returns an object for checking state and stopping those threads.
+	// Call this when returning from sleep and close when going back to
+	// sleep.
+	StartNetworkRunner() NetworkRunner
+
+	// RegisterRoundEventsHandler registers a callback interface for round
+	// events.
+	RegisterRoundEventsHandler(hdlr RoundEventHandler)
+}
+
+// Message is a message received from the cMix network in the clear
+// or that has been decrypted using established E2E keys.
 type Message interface {
-	// Returns the message's sender ID
+	// Returns the message's sender ID, if available
 	GetSender() []byte
-	// Returns the message payload
-	// Parse this with protobuf/whatever according to the type of the message
+	// Returns the message payload/contents
+	// Parse this with protobuf/whatever according to the message type
 	GetPayload() []byte
 	// Returns the message's recipient ID
+	// This is usually your userID but could be an ephemeral/group ID
 	GetRecipient() []byte
 	// Returns the message's type
 	GetMessageType() int32
@@ -28,88 +147,77 @@ type Message interface {
 	GetTimestampNano() int64
 }
 
-// Copy of the storage interface.
-// It is identical to the interface used in Globals,
-// and a results the types can be passed freely between the two
-type Storage interface {
-	// Give a Location for storage.  Does not need to be implemented if unused.
-	SetLocation(string, string) error
-	// Returns the Location for storage.
-	// Does not need to be implemented if unused.
-	GetLocation() string
-	// Stores the passed byte slice to location A
-	SaveA([]byte) error
-	// Returns the stored byte slice stored in location A
-	LoadA() []byte
-	// Stores the passed byte slice to location B
-	SaveB([]byte) error
-	// Returns the stored byte slice stored in location B
-	LoadB() []byte
-	// Returns whether the storage has even been written to.
-	// if something exists in A or B
-	IsEmpty() bool
+// RoundEvent contains event information for a given round.
+// TODO: This is a half-baked interface and will be filled out later.
+type RoundEvent interface {
+	// GetID returns the round ID for this round.
+	GetID() int
+	// GetStatus returns the status of this round.
+	GetStatus() int
 }
 
-// Translate a bindings storage to a client storage
-type storageProxy struct {
-	boundStorage Storage
+// ContactList contains a list of contacts
+type ContactList interface {
+	// GetLen returns the number of contacts in the list
+	GetLen() int
+	// GetContact returns the contact at index i
+	GetContact(i int) Contact
 }
 
-// Translate a bindings message to a parse message
+// Contact contains the contacts information. Note that this object
+// is a copy of the contact at the time it was generated, so api users
+// cannot rely on this object being updated once it has been received.
+type Contact interface {
+	// GetID returns the user ID for this user.
+	GetID() []byte
+	// GetPublicKey returns the publickey bytes for this user.
+	GetPublicKey() []byte
+	// GetSalt returns the salt used to initiate an authenticated channel
+	GetSalt() []byte
+	// IsAuthenticated returns true if an authenticated channel exists for
+	// this user so we can begin to send messages.
+	IsAuthenticated() bool
+	// IsConfirmed returns true if the user has confirmed the authenticated
+	// channel on their end.
+	IsConfirmed() bool
+	// Marshal creates a serialized representation of a contact for
+	// out-of-band contact exchange.
+	Marshal() ([]byte, error)
+}
+
+// ----- Callback interfaces -----
+
+// Listener provides a callback to hear a message
 // An object implementing this interface can be called back when the client
 // gets a message of the type that the registerer specified at registration
 // time.
 type Listener interface {
-	// This does not include the generic interfaces seen in the go implementation
-	// Those are used internally on the backend and cause errors if we try to port them
-	Hear(msg Message, isHeardElsewhere bool)
+	// Hear is called to receive a message in the UI
+	Hear(msg Message)
 }
 
-// Translate a bindings listener to a switchboard listener
-// Note to users of this package from other languages: Symbols that start with
-// lowercase are unexported from the package and meant for internal use only.
-type listenerProxy struct {
-	proxy Listener
+// AuthEventHandler handles authentication requests initiated by
+// CreateAuthenticatedChannel
+type AuthEventHandler interface {
+	// HandleConfirmation handles AuthEvents received after
+	// the client has called CreateAuthenticatedChannel for
+	// the provided contact. Payload is typically empty but
+	// may include a small introductory message.
+	HandleConfirmation(contact Contact, payload []byte)
+	// HandleRequest handles AuthEvents received before
+	// the client has called CreateAuthenticatedChannel for
+	// the provided contact. It should prompt the user to accept
+	// the channel creation "request" and, if approved,
+	// call CreateAuthenticatedChannel for this Contact.
+	HandleRequest(contact Contact, payload []byte)
 }
 
-func (lp *listenerProxy) Hear(msg switchboard.Item, isHeardElsewhere bool, i ...interface{}) {
-	msgInterface := &parse.BindingsMessageProxy{Proxy: msg.(*parse.Message)}
-	lp.proxy.Hear(msgInterface, isHeardElsewhere)
+// RoundEventHandler handles round events happening on the cMix network.
+type RoundEventHandler interface {
+	HandleEvent(re RoundEvent)
 }
 
-// Interface used to receive a callback on searching for a user
-type SearchCallback interface {
-	Callback(userID, pubKey []byte, err error)
-}
-
-type searchCallbackProxy struct {
-	proxy SearchCallback
-}
-
-func (scp *searchCallbackProxy) Callback(userID, pubKey []byte, err error) {
-	scp.proxy.Callback(userID, pubKey, err)
-}
-
-// Interface used to receive a callback on searching for a user's nickname
-type NickLookupCallback interface {
-	Callback(nick string, err error)
-}
-
-type nickCallbackProxy struct {
-	proxy NickLookupCallback
-}
-
-// interface used to receive the result of a nickname request
-func (ncp *nickCallbackProxy) Callback(nick string, err error) {
-	ncp.proxy.Callback(nick, err)
-}
-
-// interface used to receive a ui friendly description of the current status of
-// registration
-type ConnectionStatusCallback interface {
-	Callback(status int, TimeoutSeconds int)
-}
-
-type OperationProgressCallback interface {
-	Callback(int)
+// UserDiscoveryHandler handles search results against the user discovery agent.
+type UserDiscoveryHandler interface {
+	HandleSearchResults(results ContactList)
 }

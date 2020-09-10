7f9ae590caa0b15481ffaf20993baa76953bb4d5
Richard T. Carback III <rick.carback@gmail.com>
2020-09-10 17:14:33 +0000

    remove deprecated user code

--- user/regState.go
+++
@@ -1,8 +0,0 @@
-package user
-
-const (
-	NotStarted            uint32 = iota // Set on session creation
-	KeyGenComplete               = 1000 // Set upon generation of session information
-	PermissioningComplete        = 2000 // Set upon completion of RegisterWithPermissioning
-	UDBComplete                  = 3000 // Set upon completion of RegisterWithUdb
-)

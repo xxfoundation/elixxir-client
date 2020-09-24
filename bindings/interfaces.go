45a228e491869e3ba0a7437e3dcec0f243bcad1a
Benjamin Wenger <ben@elixxir.ioo>
2020-09-24 07:25:26 -0700

    fixed bindings

--- bindings/interfaces.go
+++ bindings/interfaces.go
@@ -8,6 +8,7 @@ package bindings
 
 import (
 	"gitlab.com/elixxir/client/api"
+	"gitlab.com/xx_network/primitives/id"
 )
 
 // Client is defined inside the api package. At minimum, it implements all of
@@ -143,7 +144,7 @@ type ContactList interface {
 // time.
 type Listener interface {
 	// Hear is called to receive a message in the UI
-	Hear(msg api.Message)
+	Hear(msg Message)
 }
 
 // AuthEventHandler handles authentication requests initiated by
@@ -187,4 +188,29 @@ type RoundEventHandler interface {
 // UserDiscoveryHandler handles search results against the user discovery agent.
 type UserDiscoveryHandler interface {
 	HandleSearchResults(results ContactList)
+}
+
+// Message is a message received from the cMix network in the clear
+// or that has been decrypted using established E2E keys.
+type Message interface {
+	// Returns the message's sender ID, if available
+	GetSender() id.ID
+	GetSenderBytes() []byte
+
+	// Returns the message payload/contents
+	// Parse this with protobuf/whatever according to the message type
+	GetPayload() []byte
+
+	// Returns the message's recipient ID
+	// This is usually your userID but could be an ephemeral/group ID
+	GetRecipient() id.ID
+	GetRecipientBytes() []byte
+
+	// Returns the message's type
+	GetMessageType() int32
+
+	// Returns the message's timestamp in seconds since unix epoc
+	GetTimestamp() int64
+	// Returns the message's timestamp in ns since unix epoc
+	GetTimestampNano() int64
 }

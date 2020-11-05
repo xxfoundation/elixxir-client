6e4471372cb259772b5b2d8d0cdc5e3ef54a4a92
Benjamin Wenger <ben@elixxir.ioo>
2020-11-05 14:25:52 -0800

    made skeleton for remove fact

---
+++ ud/manager.go
@@ -0,0 +1,13 @@
+package ud
+
+import (
+	"gitlab.com/elixxir/comms/client"
+	"gitlab.com/xx_network/comms/connect"
+)
+
+type Manager struct{
+	comms *client.Comms
+	host *connect.Host
+	privKey *
+}
+
---
+++ ud/remove.go
@@ -0,0 +1,35 @@
+package ud
+
+import (
+	"gitlab.com/elixxir/client/interfaces/contact"
+	"gitlab.com/elixxir/comms/client"
+	"gitlab.com/xx_network/comms/connect"
+	"gitlab.com/xx_network/comms/messages"
+	"gitlab.com/xx_network/crypto/signature/rsa"
+)
+
+
+type removeFactComms interface{
+	SendDeleteMessage(host *connect.Host, message *messages.AuthenticatedMessage)
+}
+
+func (m *Manager)RemoveFact(fact contact.Fact)error{
+	return m.removeFact(fact,m.comms)
+}
+
+func (m *Manager)removeFact(fact contact.Fact, SendDeleteMessage removeFactComms)error {
+	//digest the fact
+	fact.Stringify()
+	//sign the fact
+	rsa.Sign()
+
+	//constuct the message
+
+
+	//send the message
+
+	//return the error
+
+
+
+}

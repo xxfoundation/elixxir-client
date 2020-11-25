261f2cdf7d07133a8a8ed5fdff9f2cf0e4127bbd
Jono Wenger <jono@elixxir.io>
2020-11-25 10:52:23 -0800

    Implement user discovery user registration

--- go.mod
+++ go.mod
@@ -18,13 +18,14 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.1
 	gitlab.com/elixxir/bloomfilter v0.0.0-20200930191214-10e9ac31b228
-	gitlab.com/elixxir/comms v0.0.4-0.20201120005227-ec9177071abb
-	gitlab.com/elixxir/crypto v0.0.5-0.20201118204646-9b23991834c6
+	gitlab.com/elixxir/comms v0.0.4-0.20201124200043-8031463a4843
+	gitlab.com/elixxir/crypto v0.0.5-0.20201124195819-54be458a0dac
 	gitlab.com/elixxir/ekv v0.1.3
 	gitlab.com/elixxir/primitives v0.0.3-0.20201116174806-97f190989704
 	gitlab.com/xx_network/comms v0.0.4-0.20201119231004-a67d08045535
-	gitlab.com/xx_network/crypto v0.0.4
+	gitlab.com/xx_network/crypto v0.0.5-0.20201124194022-366c10b1bce0
 	gitlab.com/xx_network/primitives v0.0.3-0.20201116234927-44e42fc91e7c
+	gitlab.com/xx_network/ring v0.0.3-0.20201120004140-b0e268db06d1 // indirect
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 	google.golang.org/protobuf v1.25.0
 	gopkg.in/ini.v1 v1.61.0 // indirect
--- ud/register.go
+++ ud/register.go
@@ -1,25 +1,75 @@
 package ud
 
 import (
+	"github.com/pkg/errors"
 	pb "gitlab.com/elixxir/comms/mixmessages"
-	"gitlab.com/xx_network/primitives/id"
+	"gitlab.com/elixxir/crypto/factID"
+	"gitlab.com/elixxir/crypto/hash"
+	"gitlab.com/elixxir/primitives/fact"
+	"gitlab.com/xx_network/comms/connect"
+	"gitlab.com/xx_network/comms/messages"
+	"gitlab.com/xx_network/crypto/signature/rsa"
 )
 
-func (m *Manager) Register(myID *id.ID, username string) error {
+type registerUserComms interface {
+	SendRegisterUser(*connect.Host, *pb.UDBUserRegistration) (*messages.Ack, error)
+}
 
+// Register registers a user with user discovery.
+func (m *Manager) Register(username string) error {
+	return m.register(username, m.comms)
+}
+
+// register registers a user with user discovery with a specified comm for
+// easier testing.
+func (m *Manager) register(username string, comm registerUserComms) error {
+	var err error
+	user := m.storage.User()
+	cryptoUser := m.storage.User().GetCryptographicIdentity()
+	rng := m.rng.GetStream()
+
+	// Construct the user registration message
 	msg := &pb.UDBUserRegistration{
-		PermissioningSignature: nil,
-		RSAPublicPem:           "",
-		IdentityRegistration:   nil,
-		IdentitySignature:      nil,
-		Frs:                    nil,
-		UID:                    myID.Bytes(),
-		XXX_NoUnkeyedLiteral:   struct{}{},
-		XXX_unrecognized:       nil,
-		XXX_sizecache:          0,
+		PermissioningSignature: user.GetRegistrationValidationSignature(),
+		RSAPublicPem:           string(rsa.CreatePublicKeyPem(cryptoUser.GetRSA().GetPublic())),
+		IdentityRegistration: &pb.Identity{
+			Username: username,
+			DhPubKey: m.storage.E2e().GetDHPublicKey().Bytes(),
+			Salt:     cryptoUser.GetSalt(),
+		},
+		UID: cryptoUser.GetUserID().Marshal(),
 	}
 
-	_, _ = m.comms.SendRegisterUser(m.host, msg)
+	// Sign the identity data and add to user registration message
+	identityDigest := msg.IdentityRegistration.Digest()
+	msg.IdentitySignature, err = rsa.Sign(rng, cryptoUser.GetRSA(),
+		hash.CMixHash, identityDigest, nil)
+	if err != nil {
+		return errors.Errorf("Failed to sign user's IdentityRegistration: %+v", err)
+	}
 
-	return nil
+	// Create new username fact
+	usernameFact, err := fact.NewFact(fact.Username, username)
+	if err != nil {
+		return errors.Errorf("Failed to create new username fact: %+v", err)
+	}
+
+	// Hash and sign fact
+	hashedFact := factID.Fingerprint(usernameFact)
+	signedFact, err := rsa.Sign(rng, cryptoUser.GetRSA(), hash.CMixHash, hashedFact, nil)
+
+	// Add username fact register request to the user registration message
+	msg.Frs = &pb.FactRegisterRequest{
+		UID: cryptoUser.GetUserID().Marshal(),
+		Fact: &pb.Fact{
+			Fact:     username,
+			FactType: 0,
+		},
+		FactSig: signedFact,
+	}
+
+	// Register user with user discovery
+	_, err = comm.SendRegisterUser(m.host, msg)
+
+	return err
 }

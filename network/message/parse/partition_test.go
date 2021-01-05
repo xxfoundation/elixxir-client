474699c94170f3aa091dc6d3240a5708f545d303
Sydney Anne Erickson <sydney@elixxir.io>
2021-01-05 13:24:00 -0800

    Add comments for testing for message parsing

--- network/message/parse/partition_test.go
+++ network/message/parse/partition_test.go
@@ -1,3 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                          //
+//                                                                           //
+// Use of this source code is governed by a license that can be found in the //
+// LICENSE file                                                              //
+///////////////////////////////////////////////////////////////////////////////
+
 package parse
 
 import (
@@ -14,7 +21,7 @@ var ipsumTestStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cra
 	"interdum suscipit. Etiam vitae dui posuere, congue mi a, convallis odio. In commodo risus at lorem volutpat " +
 	"placerat. In cursus magna purus, suscipit dictum lorem aliquam non. Praesent efficitur."
 
-// Test that NewPartitioner outputs a correctly formatted Partitioner
+// Test that NewPartitioner outputs a correctly made Partitioner
 func TestNewPartitioner(t *testing.T) {
 	storeSession := storage.InitTestingSession(t)
 	p := NewPartitioner(4096, storeSession)
@@ -54,6 +61,7 @@ func TestNewPartitioner(t *testing.T) {
 	}
 }
 
+// Test that no error is returned running Partition
 func TestPartitioner_Partition(t *testing.T) {
 	storeSession := storage.InitTestingSession(t)
 	p := NewPartitioner(len(ipsumTestStr), storeSession)
@@ -65,11 +73,28 @@ func TestPartitioner_Partition(t *testing.T) {
 	}
 }
 
+// Test that HandlePartition can handle a message part
 func TestPartitioner_HandlePartition(t *testing.T) {
 	storeSession := storage.InitTestingSession(t)
 	p := NewPartitioner(len(ipsumTestStr), storeSession)
 
 	m := newMessagePart(1107, 1, []byte(ipsumTestStr))
+
+	_, _ = p.HandlePartition(
+		&id.DummyUser,
+		message.None,
+		m.Bytes(),
+		[]byte{'t', 'e', 's', 't', 'i', 'n', 'g',
+			's', 't', 'r', 'i', 'n', 'g'},
+	)
+}
+
+// Test that HandlePartition can handle a first message part
+func TestPartitioner_HandleFirstPartition(t *testing.T) {
+	storeSession := storage.InitTestingSession(t)
+	p := NewPartitioner(len(ipsumTestStr), storeSession)
+
+	m := newFirstMessagePart(message.Text, 1107, 1, time.Now(), []byte(ipsumTestStr))
 
 	_, _ = p.HandlePartition(
 		&id.DummyUser,

ce273f56f0952c53160e95e8592db99982133e6e
Jono Wenger <jono@elixxir.io>
2021-02-01 15:59:54 -0800

    Fix tests

--- storage/reception/identity.go
+++ storage/reception/identity.go
@@ -13,7 +13,7 @@ import (
 const identityStorageKey = "IdentityStorage"
 const identityStorageVersion = 0
 
-type Identity struct{
+type Identity struct {
 	//identity
 	EphId  ephemeral.Id
 	Source *id.ID
@@ -34,25 +34,24 @@ type Identity struct{
 	Ephemeral bool
 }
 
-func loadIdentity(kv *versioned.KV)(Identity, error){
+func loadIdentity(kv *versioned.KV) (Identity, error) {
 	obj, err := kv.Get(identityStorageKey)
-	if err!=nil{
+	if err != nil {
 		return Identity{}, errors.WithMessage(err, "Failed to load Identity")
 	}
 
 	r := Identity{}
 	err = json.Unmarshal(obj.Data, &r)
-	if err!=nil{
+	if err != nil {
 		return Identity{}, errors.WithMessage(err, "Failed to unmarshal Identity")
 	}
 	return r, nil
 }
 
-
-func (i Identity)store(kv *versioned.KV)error{
+func (i Identity) store(kv *versioned.KV) error {
 	//marshal the registration
 	regStr, err := json.Marshal(&i)
-	if err!=nil{
+	if err != nil {
 		return errors.WithMessage(err, "Failed to marshal Identity")
 	}
 
@@ -65,22 +64,33 @@ func (i Identity)store(kv *versioned.KV)error{
 
 	//store the data
 	err = kv.Set(identityStorageKey, obj)
-	if err!=nil{
+	if err != nil {
 		return errors.WithMessage(err, "Failed to store Identity")
 	}
 
 	return nil
 }
 
-func (i Identity)delete(kv *versioned.KV)error{
+func (i Identity) delete(kv *versioned.KV) error {
 	return kv.Delete(identityStorageKey)
 }
 
-func (i Identity)calculateKrSize()int{
-	return int(i.EndValid.Sub(i.StartValid).Seconds()+1)*maxRoundsPerSecond
+func (i Identity) calculateKrSize() int {
+	return int(i.EndValid.Sub(i.StartValid).Seconds()+1) * maxRoundsPerSecond
 }
 
-func (i *Identity)String()string{
+func (i *Identity) String() string {
 	return strconv.FormatInt(i.EphId.Int64(), 16) + " " + i.Source.String()
 }
 
+func (i Identity) Equal(b Identity) bool {
+	return i.EphId == b.EphId &&
+		i.Source.Cmp(b.Source) &&
+		i.End.Equal(b.End) &&
+		i.ExtraChecks == b.ExtraChecks &&
+		i.StartValid.Equal(b.StartValid) &&
+		i.EndValid.Equal(b.EndValid) &&
+		i.RequestMask == b.RequestMask &&
+		i.Ephemeral == b.Ephemeral
+
+}
--- storage/reception/identity_test.go
+++ storage/reception/identity_test.go
@@ -6,7 +6,6 @@ import (
 	"gitlab.com/xx_network/primitives/id"
 	"gitlab.com/xx_network/primitives/id/ephemeral"
 	"math/rand"
-	"reflect"
 	"testing"
 	"time"
 )
@@ -21,20 +20,20 @@ func TestIdentity_EncodeDecode(t *testing.T) {
 		ExtraChecks: 12,
 		StartValid:  time.Now().Round(0),
 		EndValid:    time.Now().Round(0),
-		RequestMask: 2*time.Hour,
+		RequestMask: 2 * time.Hour,
 		Ephemeral:   false,
 	}
 	err := r.store(kv)
-	if err!=nil{
+	if err != nil {
 		t.Errorf("Failed to store: %s", err)
 	}
 
 	rLoad, err := loadIdentity(kv)
-	if err!=nil{
+	if err != nil {
 		t.Errorf("Failed to load: %s", err)
 	}
 
-	if !reflect.DeepEqual(r, rLoad){
+	if !r.Equal(rLoad) {
 		t.Errorf("The two registrations are not the same\n saved:  %+v\n loaded: %+v", r, rLoad)
 	}
 }
@@ -68,11 +67,10 @@ func TestIdentity_Delete(t *testing.T) {
 	}
 }
 
-
 func TestIdentity_String(t *testing.T) {
 	rng := rand.New(rand.NewSource(42))
 
-	timestamp  := time.Date(2009, 11, 17, 20,
+	timestamp := time.Date(2009, 11, 17, 20,
 		34, 58, 651387237, time.UTC)
 
 	received, _ := generateFakeIdentity(rng, 15, timestamp)
@@ -80,25 +78,25 @@ func TestIdentity_String(t *testing.T) {
 	expected := "-1763 U4x/lrFkvxuXu59LtHLon1sUhPJSCcnZND6SugndnVID"
 
 	s := received.String()
-	if s != expected{
-		t.Errorf("String did not return the correct value: " +
+	if s != expected {
+		t.Errorf("String did not return the correct value: "+
 			"\n\t Expected: %s\n\t Received: %s", expected, s)
 	}
 }
 
-func TestIdentity_CalculateKrSize(t *testing.T){
-	deltas := []time.Duration{0, 2*time.Second, 2*time.Hour, 36*time.Hour,
-		time.Duration(rand.Uint32())*time.Millisecond}
+func TestIdentity_CalculateKrSize(t *testing.T) {
+	deltas := []time.Duration{0, 2 * time.Second, 2 * time.Hour, 36 * time.Hour,
+		time.Duration(rand.Uint32()) * time.Millisecond}
 	for _, d := range deltas {
-		expected := int(d.Seconds()+1)*maxRoundsPerSecond
+		expected := int(d.Seconds()+1) * maxRoundsPerSecond
 		now := time.Now()
 		id := Identity{
-			StartValid:  now,
-			EndValid:    now.Add(d),
+			StartValid: now,
+			EndValid:   now.Add(d),
 		}
 
 		krSize := id.calculateKrSize()
-		if krSize != expected{
+		if krSize != expected {
 			t.Errorf("kr size not correct! expected: %v, recieved: %v",
 				expected, krSize)
 		}

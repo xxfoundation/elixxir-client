799ede3edb7a44f1f0439db92a71c1d5ea6a36ba
Benjamin Wenger <ben@elixxir.ioo>
2021-02-15 10:24:18 -0800

    fixed the processing of timeouts to be more accurate for sending to new rounds

--- network/message/sendCmix.go
+++ network/message/sendCmix.go
@@ -75,7 +75,7 @@ func sendCmixHelper(msg format.Message, recipient *id.ID, param params.CMIX, ins
 		roundCutoffTime.Add(sendTimeBuffer)
 		now := time.Now()
 
-		if roundCutoffTime.After(now) {
+		if now.After(roundCutoffTime) {
 			jww.WARN.Printf("Round %d received which has already started" +
 				" realtime: \n\t started: %s \n\t now: %s", bestRound.ID,
 				roundCutoffTime, now)

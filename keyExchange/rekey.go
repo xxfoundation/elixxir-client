5dbb17582a8df68b440ee1cb8e8b4d2cc7760b22
Benjamin Wenger <ben@elixxir.ioo>
2021-02-24 16:36:40 -0800

    made it so that newly create send sessions are of the "sending" negotiation status instead of "unconfirmed" so they do not get triggered a second time

--- keyExchange/rekey.go
+++ keyExchange/rekey.go
@@ -129,7 +129,6 @@ func negotiate(instance *network.Instance, sendE2E interfaces.SendE2E,
 	// transmit, the partner cannot read the result. Log the error and set
 	// the session as unconfirmed so it will re-trigger the negotiation
 	if !success {
-		session.SetNegotiationStatus(e2e.Unconfirmed)
 		return errors.Errorf("Key Negotiation for %s failed to "+
 			"transmit %v/%v paritions: %v round failures, %v timeouts",
 			session, numRoundFail+numTimeOut, len(rounds), numRoundFail,

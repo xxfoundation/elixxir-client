de433167ecb63a075dcfe177bb85fe8444d3ebe4
josh <josh@elixxir.io>
2021-05-03 15:06:35 -0700

    Export fields in UncheckedRound

--- storage/rounds/unchecked.go
+++ storage/rounds/unchecked.go
@@ -22,19 +22,19 @@ const (
 // These rounds are stored for retry of message retrieval
 type UncheckedRound struct {
 	//
-	rid id.Round
+	Rid id.Round
 	roundIdentity
 	// Timestamp in which round has been stored
-	ts time.Time
+	Ts time.Time
 	// Number of times a round has been checked
-	numTries uint
+	NumTries uint
 }
 
 // Round identity information used in message retrieval
 // Derived from reception.Identity
 type roundIdentity struct {
-	epdId  ephemeral.Id
-	source *id.ID
+	EpdId  ephemeral.Id
+	Source *id.ID
 }
 
 // Storage object saving rounds to retry for message retrieval
@@ -80,13 +80,13 @@ func (s *UncheckedRoundStore) AddRound(rid id.Round, ephID ephemeral.Id, source 
 	s.mux.Lock()
 	defer s.mux.Unlock()
 	newUncheckedRound := UncheckedRound{
-		rid: rid,
+		Rid: rid,
 		roundIdentity: roundIdentity{
-			epdId:  ephID,
-			source: source,
+			EpdId:  ephID,
+			Source: source,
 		},
-		ts:       netTime.Now(),
-		numTries: 0,
+		Ts:       netTime.Now(),
+		NumTries: 0,
 	}
 
 	s.list[rid] = newUncheckedRound
@@ -95,11 +95,18 @@ func (s *UncheckedRoundStore) AddRound(rid id.Round, ephID ephemeral.Id, source 
 }
 
 // Retrieves an UncheckedRound from the map, if it exists
-func (s *UncheckedRoundStore) Get(rid id.Round) (UncheckedRound, bool) {
+func (s *UncheckedRoundStore) GetRound(rid id.Round) (UncheckedRound, bool) {
 	s.mux.RLock()
 	defer s.mux.RUnlock()
 	rnd, exists := s.list[rid]
 	return rnd, exists
+}
+
+// Retrieves the list of rounds
+func (s *UncheckedRoundStore) GetList() map[id.Round]UncheckedRound {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	return s.list
 }
 
 // Increments the amount of checks performed on this stored round
@@ -111,7 +118,7 @@ func (s *UncheckedRoundStore) IncrementCheck(rid id.Round) {
 		return
 	}
 
-	rnd.numTries++
+	rnd.NumTries++
 }
 
 func (s *UncheckedRoundStore) save() error {
@@ -140,21 +147,21 @@ func (s *UncheckedRoundStore) serializeRounds() ([]byte, error) {
 	for _, rnd := range s.list {
 		// Write the round ID
 		b := make([]byte, 8)
-		binary.LittleEndian.PutUint64(b, uint64(rnd.rid))
+		binary.LittleEndian.PutUint64(b, uint64(rnd.Rid))
 		buf.Write(b)
 
 		// Write the round identity info
-		buf.Write(rnd.roundIdentity.epdId[:])
-		if rnd.source != nil {
-			buf.Write(rnd.roundIdentity.source.Marshal())
+		buf.Write(rnd.roundIdentity.EpdId[:])
+		if rnd.Source != nil {
+			buf.Write(rnd.roundIdentity.Source.Marshal())
 		} else {
 			buf.Write(make([]byte, id.ArrIDLen))
 		}
 
 		// Write the time stamp bytes
-		tsBytes, err := rnd.ts.MarshalBinary()
+		tsBytes, err := rnd.Ts.MarshalBinary()
 		if err != nil {
-			return nil, errors.WithMessagef(err, "Could not marshal timestamp for round %d", rnd.rid)
+			return nil, errors.WithMessagef(err, "Could not marshal timestamp for round %d", rnd.Rid)
 		}
 		b = make([]byte, 8)
 		binary.LittleEndian.PutUint64(b, uint64(len(tsBytes)))
@@ -163,7 +170,7 @@ func (s *UncheckedRoundStore) serializeRounds() ([]byte, error) {
 
 		// Write the number of tries for this round
 		b = make([]byte, 8)
-		binary.LittleEndian.PutUint64(b, uint64(rnd.numTries))
+		binary.LittleEndian.PutUint64(b, uint64(rnd.NumTries))
 		buf.Write(b)
 	}
 
@@ -181,27 +188,27 @@ func deserializeRounds(data []byte) (map[id.Round]UncheckedRound, error) {
 	for i := 0; i < int(length); i++ {
 		rnd := UncheckedRound{}
 		rid, _ := binary.Varint(buff.Next(8))
-		rnd.rid = id.Round(rid)
+		rnd.Rid = id.Round(rid)
 
 		// Deserialize the round identity information
-		copy(rnd.epdId[:], buff.Next(8))
+		copy(rnd.EpdId[:], buff.Next(8))
 
 		sourceId, err := id.Unmarshal(buff.Next(id.ArrIDLen))
 		if err != nil {
 			return nil, errors.WithMessage(err, "Failed to unmarshal round identity.source")
 		}
 
-		rnd.source = sourceId
+		rnd.Source = sourceId
 
 		// Deserialize the timestamp bytes
 		timestampLen, _ := binary.Varint(buff.Next(8))
 		tsByes := buff.Next(int(timestampLen))
-		if err = rnd.ts.UnmarshalBinary(tsByes); err != nil {
+		if err = rnd.Ts.UnmarshalBinary(tsByes); err != nil {
 			return nil, errors.WithMessage(err, "Failed to unmarshal round timestamp")
 		}
 
 		numTries, _ := binary.Varint(buff.Next(8))
-		rnd.numTries = uint(numTries)
+		rnd.NumTries = uint(numTries)
 
 		roundMap[id.Round(rid)] = rnd
 

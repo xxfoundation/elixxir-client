aaca8da30819c4604a4a1a3a52bad1eaae156086
Richard T. Carback III <rick.carback@gmail.com>
2021-12-03 17:49:17 +0000

    Remove studdering from sidh interfaces lib.

--- interfaces/sidh/sidh.go
+++ interfaces/sidh/sidh.go
@@ -2,10 +2,10 @@ package interfaces
 
 import "github.com/cloudflare/circl/dh/sidh"
 
-const SidHKeyId = sidh.Fp503
-var SidHPubKeyByteSize = sidh.NewPublicKey(sidh.Fp503,
+const KeyId = sidh.Fp503
+var PubKeyByteSize = sidh.NewPublicKey(sidh.Fp503,
 	sidh.KeyVariantSidhA).Size()
-var SidHPubKeyBitSize = SidHPubKeyByteSize * 8
-var SidHPrivKeyByteSize = sidh.NewPrivateKey(sidh.Fp503,
+var PubKeyBitSize = PubKeyByteSize * 8
+var PrivKeyByteSize = sidh.NewPrivateKey(sidh.Fp503,
 	sidh.KeyVariantSidhA).Size()
-var SidHPrivKeyBitSize = SidHPrivKeyByteSize * 8
+var PrivKeyBitSize = PrivKeyByteSize * 8

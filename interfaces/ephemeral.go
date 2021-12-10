ee08ef51dd3ee2b902fef396691a0e44c8fcdb3a
jbhusson <jonah@elixxir.io>
2021-12-10 15:55:23 -0500

    Comments

--- interfaces/ephemeral.go
+++ interfaces/ephemeral.go
@@ -1,3 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2020 xx network SEZC                                           //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file                                                               //
+////////////////////////////////////////////////////////////////////////////////
+
 package interfaces
 
+// DefaultExtraChecks is the default value for ExtraChecks on reception.Identity
 const DefaultExtraChecks = 10

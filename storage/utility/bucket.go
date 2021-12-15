7f45ec4f0193dca446837cf13e161e0c1c6a8271
josh <josh@elixxir.io>
2021-12-15 12:16:42 -0800

    Add params to bucketStore

--- storage/utility/bucket.go
+++ storage/utility/bucket.go
@@ -18,18 +18,17 @@ import (
 
 const (
 	bucketStorePrefix  = "bucketStore"
-	buckerStoreKey     = "bucketStoreKey"
+	bucketStoreKey     = "bucketStoreKey"
 	bucketStoreVersion = 0
 )
 
 // BucketStore stores a leaky bucket into storage. The bucket
-// is saved in a JSON-able format, where the key is used as an identifier
-// of the bucket for ekv purposes.
+// is saved in a JSON-able format.
 type BucketStore struct {
 	bucket *rateLimiting.Bucket
-
-	kv  *versioned.KV
-	mux sync.Mutex
+	params *rateLimiting.MapParams
+	kv     *versioned.KV
+	mux    sync.RWMutex
 }
 
 // NewBucketStore creates a new, empty BucketStore and saves it to storage.
@@ -40,68 +39,57 @@ func NewBucketStore(params *rateLimiting.BucketParams, kv *versioned.KV) (*Bucke
 	bs := &BucketStore{
 		bucket: rateLimiting.CreateBucketFromParams(params, nil),
 		kv:     kv.Prefix(bucketStorePrefix),
-		mux:    sync.Mutex{},
+		mux:    sync.RWMutex{},
 	}
 
 	return bs, bs.save()
-}
-
-// Add adds the specified number of tokens to the bucket. If an add is
-// unsuccessful, an error is returned. Else the bucket is saved to storage.
-func (s *BucketStore) Add(tokens uint32) error {
-	s.mux.Lock()
-	defer s.mux.Unlock()
-
-	success, _ := s.bucket.Add(tokens)
-	if err := s.save(); err != nil {
-		return errors.WithMessagef(err, "Failed to save")
-	}
-
-	if !success {
-		return errors.Errorf("Failed to add tokens %d "+
-			"to bucket", tokens)
-	}
-
-	return nil
 }
 
 // AddWithExternalParams adds the specified number of tokens to the bucket
 // given external bucket parameters rather than the params specified in
 // the bucket. If an add is unsuccessful, an error is returned.
 // Else the bucket is saved to storage.
-func (s *BucketStore) AddWithExternalParams(tokens,
-	capacity, leakedTokens uint32,
-	duration time.Duration) error {
+func (s *BucketStore) AddWithExternalParams(tokens uint32) error {
 	s.mux.Lock()
 	defer s.mux.Unlock()
 
-	success, _ := s.bucket.
-		AddWithExternalParams(tokens, capacity, leakedTokens, duration)
+	success, _ := s.bucket.AddWithExternalParams(tokens,
+		s.params.Capacity, s.params.LeakedTokens,
+		s.params.LeakDuration)
 	if err := s.save(); err != nil {
 		return errors.WithMessagef(err, "Failed to save")
 	}
 
 	if !success {
-		return errors.Errorf("Failed to AddWithExternalParams "+
-			"(tokens %d, capacity %d, leakedTokens %d) to bucket",
-			tokens, capacity, leakedTokens)
+		return errors.Errorf("Failed to add to bucket")
 	}
 
 	return nil
+}
+
+func (s *BucketStore) UpdateParams(capacity, leakedTokens uint32,
+	leakDuration time.Duration) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+
+	s.params = &rateLimiting.MapParams{
+		Capacity:     capacity,
+		LeakedTokens: leakedTokens,
+		LeakDuration: leakDuration,
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // Storage Functions                                                          //
 ////////////////////////////////////////////////////////////////////////////////
 
-// LoadBucketStore is a storage operation which loads a bucket from storage
-// given the key identifier.
+// LoadBucketStore is a storage operation which loads a bucket from storage.
 func LoadBucketStore(params *rateLimiting.BucketParams,
 	kv *versioned.KV) (*BucketStore, error) {
 	bs := &BucketStore{
 		bucket: rateLimiting.CreateBucketFromParams(params, nil),
 		kv:     kv.Prefix(bucketStorePrefix),
-		mux:    sync.Mutex{},
+		mux:    sync.RWMutex{},
 	}
 
 	return bs, bs.load()
@@ -122,14 +110,14 @@ func (s *BucketStore) save() error {
 		Data:      data,
 	}
 
-	return s.kv.Set(buckerStoreKey, bucketStoreVersion, &obj)
+	return s.kv.Set(bucketStoreKey, bucketStoreVersion, &obj)
 }
 
 // load is a helper function which extracts the bucket data from storage
 // and loads it back into BucketStore.
 func (s *BucketStore) load() error {
 	// Load the versioned object
-	vo, err := s.kv.Get(buckerStoreKey, bucketStoreVersion)
+	vo, err := s.kv.Get(bucketStoreKey, bucketStoreVersion)
 	if err != nil {
 		return err
 	}
--- storage/utility/bucket_test.go
+++ storage/utility/bucket_test.go
@@ -13,7 +13,6 @@ import (
 	"gitlab.com/xx_network/primitives/rateLimiting"
 	"reflect"
 	"testing"
-	"time"
 )
 
 // NewBucketStore happy path.
@@ -34,7 +33,7 @@ func TestNewBucketStore(t *testing.T) {
 	}
 
 	// Load new bucket from storage
-	vo, err := bs.kv.Get(buckerStoreKey, bucketStoreVersion)
+	vo, err := bs.kv.Get(bucketStoreKey, bucketStoreVersion)
 	if err != nil {
 		t.Fatalf("Failed to get bucket from EKV: %v", err)
 	}
@@ -73,13 +72,13 @@ func TestBucketStore_AddWithExternalParams(t *testing.T) {
 	}
 
 	// Modify internal state of bucket
-	err = bs.AddWithExternalParams(10, 11, 12, 14*time.Second)
+	err = bs.AddWithExternalParams(10, 11, 12)
 	if err != nil {
 		t.Fatalf("AddWithExternalParams error: %v", err)
 	}
 
 	// Load stored bucket
-	vo, err := bs.kv.Get(buckerStoreKey, bucketStoreVersion)
+	vo, err := bs.kv.Get(bucketStoreKey, bucketStoreVersion)
 	if err != nil {
 		t.Fatalf("Failed to get bucket from EKV: %v", err)
 	}
@@ -127,49 +126,5 @@ func TestLoadBucketStore(t *testing.T) {
 		t.Fatalf("Loaded bucket store and created bucket did not match."+
 			"\n\tExpected: %v"+
 			"\n\tReceived: %v ", bs, receivedBs)
-	}
-}
-
-// BucketStore.Add happy path.
-func TestBucketStore_Add(t *testing.T) {
-	// Create initializers
-	params := &rateLimiting.BucketParams{
-		Capacity:   10,
-		Remaining:  11,
-		LeakRate:   12,
-		LastUpdate: time.Now().UnixNano(),
-	}
-	kv := versioned.NewKV(make(ekv.Memstore))
-
-	// Create bucket
-	bs, err := NewBucketStore(params, kv)
-	if err != nil {
-		t.Fatalf("Error received creating BucketStore: %v", err)
-	}
-
-	// Modify internal state of bucket
-	err = bs.Add(1)
-	if err != nil {
-		t.Fatalf("AddWithExternalParams error: %v", err)
-	}
-
-	// Load stored bucket
-	vo, err := bs.kv.Get(buckerStoreKey, bucketStoreVersion)
-	if err != nil {
-		t.Fatalf("Failed to get bucket from EKV: %v", err)
-	}
-
-	// Unmarshal stored data into separate bucket
-	receivedBs := rateLimiting.CreateBucketFromParams(params, nil)
-	err = receivedBs.UnmarshalJSON(vo.Data)
-	if err != nil {
-		t.Fatalf("UnmarshalJSON error: %v", err)
-	}
-
-	// Ensure bucket in RAM and bucket in storage are consistent
-	if !reflect.DeepEqual(receivedBs, bs.bucket) {
-		t.Fatalf("Loaded bucket and created bucket did not match."+
-			"\n\tExpected: %v"+
-			"\n\tReceived: %v ", bs.bucket, receivedBs)
 	}
 }

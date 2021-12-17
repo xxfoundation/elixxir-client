6a3167125cc0940aef15319bdbfaf4dfc78468ce
jbhusson <jonah@elixxir.io>
2021-12-17 13:20:18 -0500

    Add getprimagesb64

--- bindings/preimage.go
+++ bindings/preimage.go
@@ -1,6 +1,7 @@
 package bindings
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"github.com/pkg/errors"
 	"gitlab.com/xx_network/primitives/id"
@@ -26,6 +27,24 @@ func (c *Client) GetPreimages(identity []byte) (string, error) {
 
 	iid := &id.ID{}
 	copy(iid[:], identity)
+
+	list, exist := c.api.GetStorage().GetEdge().Get(iid)
+	if !exist {
+		return "", errors.Errorf("Could not find a preimage list for %s", iid)
+	}
+
+	marshaled, err := json.Marshal(&list)
+
+	return string(marshaled), err
+}
+
+func (c *Client) GetPreimagesB64(identity string) (string, error) {
+	iid := &id.ID{}
+	decoded, err := base64.StdEncoding.DecodeString(identity)
+	if err != nil {
+		return "", err
+	}
+	copy(iid[:], decoded)
 
 	list, exist := c.api.GetStorage().GetEdge().Get(iid)
 	if !exist {

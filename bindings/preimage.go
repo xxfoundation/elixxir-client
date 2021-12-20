35b9bab9bb82def5b0618202366ee5e9e3fb38f5
Jake Taylor <jake@elixxir.io>
2021-12-20 14:18:47 -0600

    finalized logging

--- bindings/preimage.go
+++ bindings/preimage.go
@@ -1,9 +1,7 @@
 package bindings
 
 import (
-	"encoding/base64"
 	"encoding/json"
-	"github.com/pkg/errors"
 	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/xx_network/primitives/id"
 	"reflect"
@@ -27,53 +25,22 @@ func (c *Client) RegisterPreimageCallback(identity []byte, pin PreimageNotificat
 }
 
 func (c *Client) GetPreimages(identity []byte) []byte {
-	jww.FATAL.Printf("TEST")
 	iid := &id.ID{}
 	copy(iid[:], identity)
 
 	list, exist := c.api.GetStorage().GetEdge().Get(iid)
 	if !exist {
+		jww.ERROR.Printf("Preimage for %s does not exist", iid.String())
 		return []byte{}
 	}
 
 	marshaled, err := json.Marshal(&list)
 	if err != nil {
-		jww.FATAL.Printf("TESTTEST: %+v", err)
+		jww.ERROR.Printf("Error marshaling preimages: %s", err.Error())
 		return []byte{}
 	}
-	jww.FATAL.Printf("CAT %d %v %d", reflect.TypeOf(marshaled).Align(), unsafe.Sizeof(marshaled), len(marshaled))
+
+	jww.DEBUG.Printf("Preimages size: %v %v %d",
+		reflect.TypeOf(marshaled).Align(), unsafe.Sizeof(marshaled), len(marshaled))
 	return marshaled
-}
-
-func (c *Client) GetPreimagesB64(identity string) (string, error) {
-	iid := &id.ID{}
-	decoded, err := base64.StdEncoding.DecodeString(identity)
-	if err != nil {
-		return "", err
-	}
-	copy(iid[:], decoded)
-
-	list, exist := c.api.GetStorage().GetEdge().Get(iid)
-	if !exist {
-		return "", errors.Errorf("Could not find a preimage list for %s", iid)
-	}
-
-	marshaled, err := json.Marshal(&list)
-
-	return string(marshaled), err
-}
-
-// hack on getPreimages so it works on iOS per https://github.com/golang/go/issues/46893
-func (c *Client) GetPreimagesHack(dummy string, identity []byte) (string, error) {
-
-	iid := &id.ID{}
-	copy(iid[:], identity)
-
-	list, exist := c.api.GetStorage().GetEdge().Get(iid)
-	if !exist {
-		return "", errors.Errorf("Could not find a preimage list for %s", iid)
-	}
-
-	marshaled, err := json.Marshal(&list)
-	return string(marshaled), err
 }

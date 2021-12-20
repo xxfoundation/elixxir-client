b234cffcaa654eefd63a48d63f017e93f1d01ab4
Jake Taylor <jake@elixxir.io>
2021-12-20 14:54:03 -0600

    return str

--- bindings/preimage.go
+++ bindings/preimage.go
@@ -24,23 +24,23 @@ func (c *Client) RegisterPreimageCallback(identity []byte, pin PreimageNotificat
 	c.api.GetStorage().GetEdge().AddUpdateCallback(iid, cb)
 }
 
-func (c *Client) GetPreimages(identity []byte) []byte {
+func (c *Client) GetPreimages(identity []byte) string {
 	iid := &id.ID{}
 	copy(iid[:], identity)
 
 	list, exist := c.api.GetStorage().GetEdge().Get(iid)
 	if !exist {
 		jww.ERROR.Printf("Preimage for %s does not exist", iid.String())
-		return []byte{}
+		return ""
 	}
 
 	marshaled, err := json.Marshal(&list)
 	if err != nil {
 		jww.ERROR.Printf("Error marshaling preimages: %s", err.Error())
-		return []byte{}
+		return ""
 	}
 
 	jww.DEBUG.Printf("Preimages size: %v %v %d",
 		reflect.TypeOf(marshaled).Align(), unsafe.Sizeof(marshaled), len(marshaled))
-	return marshaled
+	return string(marshaled)
 }

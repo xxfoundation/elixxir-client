ddf42e953330d0b4f4adf70ffd1ba18df07ad1b3
josh <josh@elixxir.io>
2022-02-04 11:28:10 -0800

    Add unconfirmed facts to ud Store

--- storage/ud/store.go
+++ storage/ud/store.go
@@ -1,285 +1,232 @@
-///////////////////////////////////////////////////////////////////////////////
-// Copyright © 2020 xx network SEZC                                          //
-//                                                                           //
-// Use of this source code is governed by a license that can be found in the //
-// LICENSE file                                                              //
-///////////////////////////////////////////////////////////////////////////////
-
 package ud
+
+// This file handles the storage operations on facts.
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/pkg/errors"
 	"gitlab.com/elixxir/client/storage/versioned"
 	"gitlab.com/elixxir/primitives/fact"
 	"gitlab.com/xx_network/primitives/netTime"
-	"sync"
 )
 
 // Storage constants
 const (
-	version = 0
-	prefix  = "udStorePrefix"
-	key     = "udStoreKey"
+	version            = 0
+	prefix             = "udStorePrefix"
+	unconfirmedFactKey = "unconfirmedFactKey"
+	confirmedFactKey   = "confirmedFactKey"
 )
 
-// Error constants
-const (
-	factTypeExistsErr          = "Fact %v cannot be added as fact type %s has already been stored. Cancelling backup operation!"
-	unrecognizedFactErr        = "Fact %v is not of expected type (%s). Cancelling backup operation!"
-	unrecognizedFactInStoreErr = "Fact %s with type %s loaded from memory is invalid"
-)
-
-// Store is the storage object for the higher level ud.Manager object.
-// This storage implementation is written for client side.
-type Store struct {
-	// registeredFacts contains only 2 registered facts: an email and a phone number.
-	// These are definitely indexed, as defined above.
-	registeredFacts map[fact.Fact]struct{}
-	kv              *versioned.KV
-	mux             sync.RWMutex
+// unconfirmedFactDisk is an object used to store the data of an unconfirmed fact.
+// It combines the key (confirmationId) and fact data (stringifiedFact) into a
+// single JSON-able object.
+type unconfirmedFactDisk struct {
+	confirmationId  string
+	stringifiedFact string
 }
 
-// NewStore creates a new, empty Store object.
-func NewStore(kv *versioned.KV) (*Store, error) {
-	kv = kv.Prefix(prefix)
-
-	s := &Store{
-		registeredFacts: make(map[fact.Fact]struct{}, 0),
-		kv:              kv,
-	}
-
-	return s, s.save()
-}
-
-// LoadStore loads the Store object from the provided versioned.KV.
-func LoadStore(kv *versioned.KV) (*Store, error) {
-	kv = kv.Prefix(prefix)
-
-	obj, err := kv.Get(key, version)
-	if err != nil {
-		return nil, err
-	}
-
-	s := &Store{
-		registeredFacts: make(map[fact.Fact]struct{}, 0),
-		kv:              kv,
-	}
-
-	err = s.unmarshal(obj.Data)
-	if err != nil {
-		return nil, errors.WithMessage(err, "Failed to unmarshal store")
-	}
-
-	return s, nil
-
-}
-
-// BackUpMissingFacts adds a registered fact to the Store object. It can take in both an
-// email and a phone number. One or the other may be an empty string, however both is considered
-// an error. It checks for each whether that fact type already exists in the structure. If a fact
-// type already exists, an error is returned.
-// ************************************************************************
-// NOTE: This is done since BackUpMissingFacts is exposed to the
-// bindings layer. This prevents front end from using this as the method
-// to store facts on their end, which is not its intended use case. It's intended use
-// case is to store already registered facts, prior to the creation of this function.
-// We handle storage of newly registered internally using Store.StoreFact.
-// ************************************************************************
-// Any other fact.FactType is not accepted and returns an error and nothing is backed up.
-// If you attempt to back up a fact type that has already been backed up,
-// an error will be returned and nothing will be backed up.
-// Otherwise, it adds the fact and returns whether the Store saved successfully.
-func (s *Store) BackUpMissingFacts(email, phone fact.Fact) error {
-	s.mux.Lock()
-	defer s.mux.Unlock()
-
-	if isFactZero(email) && isFactZero(phone) {
-		return errors.New("Cannot backup missing facts: Both email and phone facts are empty!")
-	}
-
-	modifiedEmail, modifiedPhone := false, false
-
-	// Handle email if it is not zero (empty string)
-	if !isFactZero(email) {
-		// check if fact is expected type
-		if email.T != fact.Email {
-			return errors.New(fmt.Sprintf("BackUpMissingFacts expects input in the order (email, phone). "+
-				"Email (%s) is non-empty but not an email. Cancelling backup operation", email.Fact))
-		}
-
-		// Check if fact type is already in map. See docstring NOTE for explanation
-		if isFactTypeInMap(fact.Email, s.registeredFacts) {
-			// If an email exists in memory, return an error
-			return errors.Errorf(factTypeExistsErr, email, fact.Email)
-		} else {
-			modifiedEmail = true
-		}
-	}
-
-	if !isFactZero(phone) {
-		// check if fact is expected type
-		if phone.T != fact.Phone {
-			return errors.New(fmt.Sprintf("BackUpMissingFacts expects input in the order (email, phone). "+
-				"Phone (%s) is non-empty but not an phone. Cancelling backup operation", phone.Fact))
-		}
-
-		// Check if fact type is already in map. See docstring NOTE for explanation
-		if isFactTypeInMap(fact.Phone, s.registeredFacts) {
-			// If a phone exists in memory, return an error
-			return errors.Errorf(factTypeExistsErr, phone, fact.Phone)
-		} else {
-			modifiedPhone = true
-		}
-	}
-
-	if modifiedPhone || modifiedEmail {
-		if modifiedEmail {
-			s.registeredFacts[email] = struct{}{}
-		}
-
-		if modifiedPhone {
-			s.registeredFacts[phone] = struct{}{}
-		}
-
-		return s.save()
-	}
-
-	return nil
-
-}
-
-// StoreFact is our internal use function which will add the fact to
-// memory and save to storage. THIS IS FOR REGISTERED FACTS ONLY.
-func (s *Store) StoreFact(f fact.Fact) error {
-	s.mux.Lock()
-	defer s.mux.Unlock()
-
-	s.registeredFacts[f] = struct{}{}
-	return s.save()
-}
-
-// DeleteFact is our internal use function which will delete the fact to
-// memory and save to storage. An error is returned if the fact does not exist in memory.
-func (s *Store) DeleteFact(f fact.Fact) error {
-	s.mux.Lock()
-	defer s.mux.Unlock()
-
-	if _, exists := s.registeredFacts[f]; !exists {
-		return errors.Errorf("Fact %v does not exist in store", f)
-	}
-
-	delete(s.registeredFacts, f)
-	return s.save()
-}
-
-// GetStringifiedFacts returns a list of stringified facts from the Store's
-// registeredFacts map.
-func (s *Store) GetStringifiedFacts() []string {
-	s.mux.RLock()
-	defer s.mux.RUnlock()
-
-	return s.serializeFacts()
-}
-
-// GetFacts returns a list of fact.Fact objects that exist within the
-// Store's registeredFacts map.
-func (s *Store) GetFacts() []fact.Fact {
-	s.mux.RLock()
-	defer s.mux.RUnlock()
-
-	// Flatten the facts into a slice
-	facts := make([]fact.Fact, 0, len(s.registeredFacts))
-	for f := range s.registeredFacts {
-		facts = append(facts, f)
-	}
-
-	return facts
-}
+/////////////////////////////////////////////////////////////////
+// SAVE FUNCTIONS
+/////////////////////////////////////////////////////////////////
 
 // save serializes the state within Store into byte data and stores
 // that data into storage via the EKV.
 func (s *Store) save() error {
-	now := netTime.Now()
 
-	data, err := s.marshal()
+	err := s.saveUnconfirmedFacts()
+	if err != nil {
+		return errors.WithMessage(err, "Failed to save unconfirmed facts")
+	}
+
+	err = s.saveConfirmedFacts()
+	if err != nil {
+		return errors.WithMessage(err, "Failed to save confirmed facts")
+	}
+
+	return nil
+}
+
+// saveConfirmedFacts saves all the data within Store.confirmedFacts into storage.
+func (s *Store) saveConfirmedFacts() error {
+
+	data, err := s.marshalConfirmedFacts()
 	if err != nil {
 		return err
 	}
 
+	// Construct versioned object
+	now := netTime.Now()
 	obj := versioned.Object{
 		Version:   version,
 		Timestamp: now,
 		Data:      data,
 	}
 
-	return s.kv.Set(key, version, &obj)
+	// Save to storage
+	return s.kv.Set(confirmedFactKey, version, &obj)
 }
 
-// marshal serializes the state within Store into byte data.
-func (s *Store) marshal() ([]byte, error) {
-	return json.Marshal(s.serializeFacts())
-}
-
-// unmarshal deserializes byte data into Store's state.
-func (s *Store) unmarshal(data []byte) error {
-	var fStrings []string
-
-	err := json.Unmarshal(data, &fStrings)
+// saveUnconfirmedFacts saves all data within Store.unconfirmedFacts into storage.
+func (s *Store) saveUnconfirmedFacts() error {
+	data, err := s.marshalUnconfirmedFacts()
 	if err != nil {
 		return err
 	}
 
-	return s.deserializeFacts(fStrings)
-}
-
-// serializeFacts is a helper function which serializes Store's registeredFacts
-// map into a list of strings. Each string in the list represents
-// a fact.Fact that has been Stringified.
-func (s *Store) serializeFacts() []string {
-	fStrings := make([]string, 0, len(s.registeredFacts))
-	for f := range s.registeredFacts {
-		fStrings = append(fStrings, f.Stringify())
+	// Construct versioned object
+	now := netTime.Now()
+	obj := versioned.Object{
+		Version:   version,
+		Timestamp: now,
+		Data:      data,
 	}
 
-	return fStrings
+	// Save to storage
+	return s.kv.Set(unconfirmedFactKey, version, &obj)
+
 }
 
-// deserializeFacts takes a list of stringified fact.Fact's and un-stringifies
-// them into fact.Fact objects. These objects are them placed into Store's
-// registeredFacts map.
-func (s *Store) deserializeFacts(fStrings []string) error {
-	facts := make(map[fact.Fact]struct{}, 0)
-	for _, fStr := range fStrings {
-		f, err := fact.UnstringifyFact(fStr)
-		if err != nil {
-			return errors.WithMessage(err, "Failed to load due to "+
-				"malformed fact")
-		}
+/////////////////////////////////////////////////////////////////
+// LOAD FUNCTIONS
+/////////////////////////////////////////////////////////////////
 
-		facts[f] = struct{}{}
+// LoadStore loads the Store object from the provided versioned.KV.
+func LoadStore(kv *versioned.KV) (*Store, error) {
+	kv = kv.Prefix(prefix)
+
+	s := &Store{
+		confirmedFacts:   make(map[fact.Fact]struct{}, 0),
+		unconfirmedFacts: make(map[string]fact.Fact, 0),
+		kv:               kv,
 	}
 
-	s.registeredFacts = facts
+	return s, s.load()
+
+}
+
+// load is a helper function which loads all data stored in storage from
+// the save operation.
+func (s *Store) load() error {
+	err := s.loadConfirmedFacts()
+	if err != nil {
+		return errors.WithMessage(err, "Failed to load confirmed facts")
+	}
+
+	err = s.loadUnconfirmedFacts()
+	if err != nil {
+		return errors.WithMessage(err, "Failed to load unconfirmed facts")
+	}
 
 	return nil
 }
 
-// fixme: consider this being a method on the fact.Fact object?
-// isFactZero tests whether a fact has been uninitialized.
-func isFactZero(f fact.Fact) bool {
-	return f.T == fact.Username && f.Fact == ""
-}
-
-// isFactTypeInMap is a helper function which determines whether a fact type exists within
-// the data structure.
-func isFactTypeInMap(factType fact.FactType, facts map[fact.Fact]struct{}) bool {
-	for f := range facts {
-		if f.T == factType {
-			return true
-		}
+// loadConfirmedFacts loads all confirmed facts from storage.
+// It is the inverse operation of saveConfirmedFacts.
+func (s *Store) loadConfirmedFacts() error {
+	// Pull data from storage
+	obj, err := s.kv.Get(confirmedFactKey, version)
+	if err != nil {
+		return err
 	}
 
-	return false
+	// Place the map in memory
+	s.confirmedFacts, err = s.unmarshalConfirmedFacts(obj.Data)
+
+	return nil
+}
+
+// loadUnconfirmedFacts loads all unconfirmed facts from storage.
+// It is the inverse operation of saveUnconfirmedFacts.
+func (s *Store) loadUnconfirmedFacts() error {
+	// Pull data from storage
+	obj, err := s.kv.Get(unconfirmedFactKey, version)
+	if err != nil {
+		return err
+	}
+
+	// Place the map in memory
+	s.unconfirmedFacts, err = s.unmarshalUnconfirmedFacts(obj.Data)
+
+	return nil
+}
+
+/////////////////////////////////////////////////////////////////
+// MARSHAL/UNMARSHAL FUNCTIONS
+/////////////////////////////////////////////////////////////////
+
+// marshalConfirmedFacts is a marshaller which serializes the data
+//// in the confirmedFacts map into a JSON.
+func (s *Store) marshalConfirmedFacts() ([]byte, error) {
+	// Flatten confirmed facts to a list
+	fStrings := s.serializeConfirmedFacts()
+
+	// Marshal to JSON
+	return json.Marshal(&fStrings)
+}
+
+// marshalUnconfirmedFacts is a marshaller which serializes the data
+// in the unconfirmedFacts map into a JSON.
+func (s *Store) marshalUnconfirmedFacts() ([]byte, error) {
+	// Flatten unconfirmed facts to a list
+	ufdList := make([]unconfirmedFactDisk, 0, len(s.unconfirmedFacts))
+	for confirmationId, f := range s.unconfirmedFacts {
+		ufd := unconfirmedFactDisk{
+			confirmationId:  confirmationId,
+			stringifiedFact: f.Stringify(),
+		}
+		ufdList = append(ufdList, ufd)
+	}
+
+	return json.Marshal(&ufdList)
+}
+
+// unmarshalConfirmedFacts is a function which deserializes the data from storage
+// into a structure matching the confirmedFacts map.
+func (s *Store) unmarshalConfirmedFacts(data []byte) (map[fact.Fact]struct{}, error) {
+	// Unmarshal into list
+	var fStrings []string
+	err := json.Unmarshal(data, &fStrings)
+	if err != nil {
+		return nil, err
+	}
+
+	// Deserialize the list into a map
+	confirmedFacts := make(map[fact.Fact]struct{}, 0)
+	for _, fStr := range fStrings {
+		f, err := fact.UnstringifyFact(fStr)
+		if err != nil {
+			return nil, errors.WithMessage(err, "Failed to load due to "+
+				"malformed fact")
+		}
+
+		confirmedFacts[f] = struct{}{}
+	}
+
+	return confirmedFacts, nil
+}
+
+// unmarshalUnconfirmedFacts is a function which deserializes the data from storage
+// into a structure matching the unconfirmedFacts map.
+func (s *Store) unmarshalUnconfirmedFacts(data []byte) (map[string]fact.Fact, error) {
+	// Unmarshal into list
+	var ufdList []unconfirmedFactDisk
+	err := json.Unmarshal(data, &ufdList)
+	if err != nil {
+		return nil, err
+	}
+
+	// Deserialize the list into a map
+	unconfirmedFacts := make(map[string]fact.Fact, 0)
+	for _, ufd := range ufdList {
+		f, err := fact.UnstringifyFact(ufd.stringifiedFact)
+		if err != nil {
+			return nil, errors.WithMessage(err, "Failed to load due to "+
+				"malformed fact")
+		}
+
+		unconfirmedFacts[ufd.confirmationId] = f
+	}
+
+	return unconfirmedFacts, nil
 }
--- storage/ud/store_test.go
+++ storage/ud/store_test.go
@@ -1,49 +1,12 @@
-///////////////////////////////////////////////////////////////////////////////
-// Copyright © 2020 xx network SEZC                                          //
-//                                                                           //
-// Use of this source code is governed by a license that can be found in the //
-// LICENSE file                                                              //
-///////////////////////////////////////////////////////////////////////////////
-
 package ud
 
 import (
 	"bytes"
 	"gitlab.com/elixxir/client/storage/versioned"
 	"gitlab.com/elixxir/ekv"
-	"gitlab.com/elixxir/primitives/fact"
 	"reflect"
-	"sort"
 	"testing"
 )
-
-func TestNewStore(t *testing.T) {
-
-	kv := versioned.NewKV(make(ekv.Memstore))
-	expectedStore := &Store{
-		kv: kv.Prefix(prefix),
-	}
-	expectedData, err := expectedStore.marshal()
-	if err != nil {
-		t.Fatalf("marshal() produced an error: %v", err)
-	}
-
-	_, err = NewStore(kv)
-	if err != nil {
-		t.Errorf("NewStore() produced an error: %v", err)
-	}
-
-	data, err := expectedStore.kv.Get(key, version)
-	if err != nil {
-		t.Errorf("Get() error when getting Store from KV: %v", err)
-	}
-
-	if !bytes.Equal(expectedData, data.Data) {
-		t.Errorf("NewStore() returned incorrect Store."+
-			"\nexpected: %+v\nreceived: %+v", expectedData,
-			data.Data)
-	}
-}
 
 func TestLoadStore(t *testing.T) {
 	kv := versioned.NewKV(make(ekv.Memstore))
@@ -67,7 +30,7 @@ func TestLoadStore(t *testing.T) {
 
 }
 
-func TestStore_StoreFact(t *testing.T) {
+func TestStore_MarshalUnmarshal_ConfirmedFacts(t *testing.T) {
 	kv := versioned.NewKV(make(ekv.Memstore))
 
 	expectedStore, err := NewStore(kv)
@@ -75,24 +38,25 @@ func TestStore_StoreFact(t *testing.T) {
 		t.Errorf("NewStore() produced an error: %v", err)
 	}
 
-	expected := fact.Fact{
-		Fact: "josh",
-		T:    fact.Username,
-	}
-
-	err = expectedStore.StoreFact(expected)
+	data, err := expectedStore.kv.Get(confirmedFactKey, version)
 	if err != nil {
-		t.Fatalf("StoreFact() produced an error: %v", err)
+		t.Errorf("Get() error when getting Store from KV: %v", err)
 	}
 
-	_, exists := expectedStore.registeredFacts[expected]
-	if !exists {
-		t.Fatalf("Fact %s does not exist in map", expected)
+	expectedData, err := expectedStore.marshalConfirmedFacts()
+	if err != nil {
+		t.Fatalf("marshalConfirmedFact error: %+v", err)
+	}
+
+	if !bytes.Equal(expectedData, data.Data) {
+		t.Errorf("NewStore() returned incorrect Store."+
+			"\nexpected: %+v\nreceived: %+v", expectedData,
+			data.Data)
 	}
 
 }
 
-func TestStore_DeleteFact(t *testing.T) {
+func TestStore_MarshalUnmarshal_UnconfirmedFacts(t *testing.T) {
 	kv := versioned.NewKV(make(ekv.Memstore))
 
 	expectedStore, err := NewStore(kv)
@@ -100,199 +64,25 @@ func TestStore_DeleteFact(t *testing.T) {
 		t.Errorf("NewStore() produced an error: %v", err)
 	}
 
-	expected := fact.Fact{
-		Fact: "josh",
-		T:    fact.Username,
-	}
-
-	err = expectedStore.StoreFact(expected)
+	data, err := expectedStore.kv.Get(confirmedFactKey, version)
 	if err != nil {
-		t.Fatalf("StoreFact() produced an error: %v", err)
+		t.Errorf("Get() error when getting Store from KV: %v", err)
 	}
 
-	_, exists := expectedStore.registeredFacts[expected]
-	if !exists {
-		t.Fatalf("Fact %s does not exist in map", expected)
-	}
-
-	err = expectedStore.DeleteFact(expected)
+	data, err = expectedStore.kv.Get(unconfirmedFactKey, version)
 	if err != nil {
-		t.Fatalf("DeleteFact() produced an error: %v", err)
+		t.Errorf("Get() error when getting Store from KV: %v", err)
 	}
 
-	err = expectedStore.DeleteFact(expected)
-	if err == nil {
-		t.Fatalf("DeleteFact should produce an error when deleting a fact not in store")
-	}
-
-}
-
-func TestStore_BackUpMissingFacts(t *testing.T) {
-	kv := versioned.NewKV(make(ekv.Memstore))
-
-	expectedStore, err := NewStore(kv)
+	expectedData, err := expectedStore.marshalUnconfirmedFacts()
 	if err != nil {
-		t.Errorf("NewStore() produced an error: %v", err)
+		t.Fatalf("marshalConfirmedFact error: %+v", err)
 	}
 
-	email := fact.Fact{
-		Fact: "josh@elixxir.io",
-		T:    fact.Email,
-	}
-
-	phone := fact.Fact{
-		Fact: "6175555678",
-		T:    fact.Phone,
-	}
-
-	err = expectedStore.BackUpMissingFacts(email, phone)
-	if err != nil {
-		t.Fatalf("BackUpMissingFacts() produced an error: %v", err)
-	}
-
-	_, exists := expectedStore.registeredFacts[email]
-	if !exists {
-		t.Fatalf("Fact %v not found in store.", email)
-	}
-
-	_, exists = expectedStore.registeredFacts[phone]
-	if !exists {
-		t.Fatalf("Fact %v not found in store.", phone)
-	}
-
-}
-
-func TestStore_BackUpMissingFacts_DuplicateFactType(t *testing.T) {
-	kv := versioned.NewKV(make(ekv.Memstore))
-
-	expectedStore, err := NewStore(kv)
-	if err != nil {
-		t.Errorf("NewStore() produced an error: %v", err)
-	}
-
-	email := fact.Fact{
-		Fact: "josh@elixxir.io",
-		T:    fact.Email,
-	}
-
-	phone := fact.Fact{
-		Fact: "6175555678",
-		T:    fact.Phone,
-	}
-
-	err = expectedStore.BackUpMissingFacts(email, phone)
-	if err != nil {
-		t.Fatalf("BackUpMissingFacts() produced an error: %v", err)
-	}
-
-	err = expectedStore.BackUpMissingFacts(email, fact.Fact{})
-	if err == nil {
-		t.Fatalf("BackUpMissingFacts() should not allow backing up an "+
-			"email when an email has already been backed up: %v", err)
-	}
-
-	err = expectedStore.BackUpMissingFacts(fact.Fact{}, phone)
-	if err == nil {
-		t.Fatalf("BackUpMissingFacts() should not allow backing up a "+
-			"phone number when a phone number has already been backed up: %v", err)
-	}
-
-}
-
-func TestStore_GetFacts(t *testing.T) {
-	kv := versioned.NewKV(make(ekv.Memstore))
-
-	testStore, err := NewStore(kv)
-	if err != nil {
-		t.Errorf("NewStore() produced an error: %v", err)
-	}
-
-	emailFact := fact.Fact{
-		Fact: "josh@elixxir.io",
-		T:    fact.Email,
-	}
-
-	emptyFact := fact.Fact{}
-
-	err = testStore.BackUpMissingFacts(emailFact, emptyFact)
-	if err != nil {
-		t.Fatalf("Faild to add fact %v: %v", emailFact, err)
-	}
-
-	phoneFact := fact.Fact{
-		Fact: "6175555212",
-		T:    fact.Phone,
-	}
-
-	err = testStore.BackUpMissingFacts(emptyFact, phoneFact)
-	if err != nil {
-		t.Fatalf("Faild to add fact %v: %v", phoneFact, err)
-	}
-
-	expectedFacts := []fact.Fact{emailFact, phoneFact}
-
-	receivedFacts := testStore.GetFacts()
-
-	sort.SliceStable(receivedFacts, func(i, j int) bool {
-		return receivedFacts[i].Fact > receivedFacts[j].Fact
-	})
-
-	sort.SliceStable(expectedFacts, func(i, j int) bool {
-		return expectedFacts[i].Fact > expectedFacts[j].Fact
-	})
-
-	if !reflect.DeepEqual(expectedFacts, receivedFacts) {
-		t.Fatalf("GetFacts() did not return expected fact list."+
-			"\nExpected: %v"+
-			"\nReceived: %v", expectedFacts, receivedFacts)
-	}
-}
-
-func TestStore_GetFactStrings(t *testing.T) {
-	kv := versioned.NewKV(make(ekv.Memstore))
-
-	testStore, err := NewStore(kv)
-	if err != nil {
-		t.Errorf("NewStore() produced an error: %v", err)
-	}
-
-	emailFact := fact.Fact{
-		Fact: "josh@elixxir.io",
-		T:    fact.Email,
-	}
-
-	emptyFact := fact.Fact{}
-
-	err = testStore.BackUpMissingFacts(emailFact, emptyFact)
-	if err != nil {
-		t.Fatalf("Faild to add fact %v: %v", emailFact, err)
-	}
-
-	phoneFact := fact.Fact{
-		Fact: "6175555212",
-		T:    fact.Phone,
-	}
-
-	err = testStore.BackUpMissingFacts(emptyFact, phoneFact)
-	if err != nil {
-		t.Fatalf("Faild to add fact %v: %v", phoneFact, err)
-	}
-
-	expectedFacts := []string{emailFact.Stringify(), phoneFact.Stringify()}
-
-	receivedFacts := testStore.GetStringifiedFacts()
-	sort.SliceStable(receivedFacts, func(i, j int) bool {
-		return receivedFacts[i] > receivedFacts[j]
-	})
-
-	sort.SliceStable(expectedFacts, func(i, j int) bool {
-		return expectedFacts[i] > expectedFacts[j]
-	})
-
-	if !reflect.DeepEqual(expectedFacts, receivedFacts) {
-		t.Fatalf("GetStringifiedFacts() did not return expected fact list."+
-			"\nExpected: %v"+
-			"\nReceived: %v", expectedFacts, receivedFacts)
+	if !bytes.Equal(expectedData, data.Data) {
+		t.Errorf("NewStore() returned incorrect Store."+
+			"\nexpected: %+v\nreceived: %+v", expectedData,
+			data.Data)
 	}
 
 }

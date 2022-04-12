18063513d23dced71af2389efa0691893b44f074
Richard T. Carback III <rick.carback@gmail.com>
2022-04-12 20:45:02 +0000

    Move proto user struct into user struct

--- interfaces/user/user.go
+++ interfaces/user/user.go
@@ -16,6 +16,28 @@ import (
 	"gitlab.com/xx_network/primitives/id"
 )
 
+type Proto struct {
+	//General Identity
+	TransmissionID   *id.ID
+	TransmissionSalt []byte
+	TransmissionRSA  *rsa.PrivateKey
+	ReceptionID      *id.ID
+	ReceptionSalt    []byte
+	ReceptionRSA     *rsa.PrivateKey
+	Precanned        bool
+	// Timestamp in which user has registered with the network
+	RegistrationTimestamp int64
+
+	RegCode string
+
+	TransmissionRegValidationSig []byte
+	ReceptionRegValidationSig    []byte
+
+	//e2e Identity
+	E2eDhPrivateKey *cyclic.Int
+	E2eDhPublicKey  *cyclic.Int
+}
+
 type Info struct {
 	//General Identity
 	TransmissionID   *id.ID

f169bdc479e33ec4ebd293c761bd7006714f979e
Benjamin Wenger <ben@elixxir.ioo>
2022-05-26 09:31:49 -0700

    added missing functionality

---
+++ bindings/secrets_test.go
@@ -0,0 +1,24 @@
+package bindings
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGenerateSecret(t *testing.T) {
+	secret1 := GenerateSecret(32)
+	secret2 := GenerateSecret(32)
+
+	if bytes.Compare(secret1, secret2) == 0 {
+		t.Errorf("GenerateSecret: Not generating entropy")
+	}
+
+	// This runs after the test function and errors out if no panic was
+	// raised.
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("GenerateSecret: Low entropy was permitted")
+		}
+	}()
+	GenerateSecret(31)
+}

d5692991ce9e6e80371534d7b61c43d82f15cfe1
jbhusson <jonah@elixxir.io>
2022-06-28 17:43:00 -0400

    Potential fix for deadlock

--- auth/callbacks.go
+++ auth/callbacks.go
@@ -42,6 +42,9 @@ func (p *partnerCallbacks) DeletePartnerCallback(partnerId *id.ID) {
 
 // getPartnerCallback returns the Callbacks for the given partnerId
 func (p *partnerCallbacks) getPartnerCallback(partnerId *id.ID) Callbacks {
+	p.RLock()
+	defer p.RUnlock()
+
 	return p.callbacks[*partnerId]
 }
 

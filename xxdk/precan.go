6b7c5e1db5f86b9764155607f03f76efdb2e703c
Jake Taylor <jake@elixxir.io>
2022-07-12 15:17:42 -0500

    refactor to unify login function

--- xxdk/precan.go
+++ xxdk/precan.go
@@ -13,48 +13,13 @@ import (
 	"gitlab.com/elixxir/client/e2e/ratchet/partner/session"
 	util "gitlab.com/elixxir/client/storage/utility"
 	"gitlab.com/elixxir/crypto/contact"
-	"gitlab.com/elixxir/crypto/cyclic"
-	"gitlab.com/elixxir/crypto/diffieHellman"
-	"gitlab.com/elixxir/primitives/fact"
 	"math/rand"
 
 	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/client/storage"
-	"gitlab.com/elixxir/client/storage/user"
 	"gitlab.com/elixxir/crypto/fastRNG"
 	"gitlab.com/xx_network/crypto/csprng"
-	"gitlab.com/xx_network/crypto/signature/rsa"
-	"gitlab.com/xx_network/primitives/id"
 )
-
-// CreatePrecannedUser creates a precanned user
-func CreatePrecannedUser(precannedID uint, rng csprng.Source) user.Info {
-
-	// Salt, UID, etc gen
-	salt := make([]byte, SaltSize)
-
-	userID := id.ID{}
-	binary.BigEndian.PutUint64(userID[:], uint64(precannedID))
-	userID.SetType(id.User)
-
-	// NOTE: not used... RSA Keygen (4096 bit defaults)
-	rsaKey, err := rsa.GenerateKey(rng, rsa.DefaultRSABitLen)
-	if err != nil {
-		jww.FATAL.Panicf(err.Error())
-	}
-
-	return user.Info{
-		TransmissionID:   &userID,
-		TransmissionSalt: salt,
-		ReceptionID:      &userID,
-		ReceptionSalt:    salt,
-		Precanned:        true,
-		E2eDhPrivateKey:  nil,
-		E2eDhPublicKey:   nil,
-		TransmissionRSA:  rsaKey,
-		ReceptionRSA:     rsaKey,
-	}
-}
 
 // NewPrecannedClient creates an insecure user with predetermined keys
 // with nodes It creates client storage, generates keys, connects, and
@@ -62,7 +27,7 @@ func CreatePrecannedUser(precannedID uint, rng csprng.Source) user.Info {
 // username/identity, but merely creates a new cryptographic identity
 // for adding such information at a later date.
 func NewPrecannedClient(precannedID uint, defJSON, storageDir string,
-	password []byte) (ReceptionIdentity, error) {
+	password []byte) error {
 	jww.INFO.Printf("NewPrecannedClient()")
 	rngStreamGen := fastRNG.NewStreamGenerator(12, 1024,
 		csprng.NewSystemRNG)
@@ -70,49 +35,32 @@ func NewPrecannedClient(precannedID uint, defJSON, storageDir string,
 
 	def, err := ParseNDF(defJSON)
 	if err != nil {
-		return ReceptionIdentity{}, err
+		return err
 	}
 	cmixGrp, e2eGrp := DecodeGroups(def)
 
-	dhPrivKey := generatePrecanDHKeypair(precannedID, e2eGrp)
-
-	userInfo := CreatePrecannedUser(precannedID, rngStream)
-	identity, err := buildReceptionIdentity(userInfo.ReceptionID, userInfo.ReceptionSalt,
-		userInfo.ReceptionRSA, e2eGrp, dhPrivKey)
-	if err != nil {
-		return ReceptionIdentity{}, err
-	}
-
+	userInfo := createPrecannedUser(precannedID, rngStream, e2eGrp)
 	store, err := CheckVersionAndSetupStorage(def, storageDir, password,
 		userInfo, cmixGrp, e2eGrp, "")
 	if err != nil {
-		return ReceptionIdentity{}, err
+		return err
 	}
 
 	// Mark the precanned user as finished with permissioning and registered
 	// with the network.
 	err = store.ForwardRegistrationStatus(storage.PermissioningComplete)
 	if err != nil {
-		return ReceptionIdentity{}, err
+		return err
 	}
 
-	return identity, err
+	return err
 }
 
-func generatePrecanDHKeypair(precannedID uint, e2eGrp *cyclic.Group) *cyclic.Int {
-	// DH Keygen
-	prng := rand.New(rand.NewSource(int64(precannedID)))
-	prime := e2eGrp.GetPBytes()
-	keyLen := len(prime)
-	priv := diffieHellman.GeneratePrivateKey(keyLen, e2eGrp, prng)
-	return priv
-}
-
-// Create an insecure e2e relationship with a precanned user
+// MakePrecannedAuthenticatedChannel creates an insecure e2e relationship with a precanned user
 func (m *E2e) MakePrecannedAuthenticatedChannel(precannedID uint) (
 	contact.Contact, error) {
 
-	precan := m.MakePrecannedContact(precannedID)
+	precan := m.GetReceptionIdentity().GetContact()
 
 	myID := binary.BigEndian.Uint64(m.GetStorage().GetReceptionID()[:])
 	// Pick a variant based on if their ID is bigger than mine.
@@ -125,19 +73,25 @@ func (m *E2e) MakePrecannedAuthenticatedChannel(precannedID uint) (
 	prng1 := rand.New(rand.NewSource(int64(precannedID)))
 	theirSIDHPrivKey := util.NewSIDHPrivateKey(theirVariant)
 	theirSIDHPubKey := util.NewSIDHPublicKey(theirVariant)
-	theirSIDHPrivKey.Generate(prng1)
+	err := theirSIDHPrivKey.Generate(prng1)
+	if err != nil {
+		return contact.Contact{}, err
+	}
 	theirSIDHPrivKey.GeneratePublicKey(theirSIDHPubKey)
 
 	prng2 := rand.New(rand.NewSource(int64(myID)))
 	mySIDHPrivKey := util.NewSIDHPrivateKey(myVariant)
 	mySIDHPubKey := util.NewSIDHPublicKey(myVariant)
-	mySIDHPrivKey.Generate(prng2)
+	err = mySIDHPrivKey.Generate(prng2)
+	if err != nil {
+		return contact.Contact{}, err
+	}
 	mySIDHPrivKey.GeneratePublicKey(mySIDHPubKey)
 
 	// add the precanned user as a e2e contact
 	// FIXME: these params need to be threaded through...
 	sesParam := session.GetDefaultParams()
-	_, err := m.e2e.AddPartner(precan.ID, precan.DhPubKey,
+	_, err = m.e2e.AddPartner(precan.ID, precan.DhPubKey,
 		m.e2e.GetHistoricalDHPrivkey(), theirSIDHPubKey,
 		mySIDHPrivKey, sesParam, sesParam)
 
@@ -146,28 +100,4 @@ func (m *E2e) MakePrecannedAuthenticatedChannel(precannedID uint) (
 	m.GetCmix().CheckInProgressMessages()
 
 	return precan, err
-}
-
-// Create an insecure e2e contact object for a precanned user
-func (m *E2e) MakePrecannedContact(precannedID uint) contact.Contact {
-
-	e2eGrp := m.GetStorage().GetE2EGroup()
-
-	rng := m.GetRng().GetStream()
-	precanned := CreatePrecannedUser(precannedID, rng)
-	rng.Close()
-
-	precanned.E2eDhPrivateKey = generatePrecanDHKeypair(precannedID,
-		m.GetStorage().GetE2EGroup())
-
-	// compute their public e2e key
-	partnerPubKey := e2eGrp.ExpG(precanned.E2eDhPrivateKey,
-		e2eGrp.NewInt(1))
-
-	return contact.Contact{
-		ID:             precanned.ReceptionID,
-		DhPubKey:       partnerPubKey,
-		OwnershipProof: nil,
-		Facts:          make([]fact.Fact, 0),
-	}
 }

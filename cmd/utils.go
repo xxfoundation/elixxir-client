018a33d60ea8bcde0de79a489eff6215ba681d6c
joshemb <josh@elixxir.io>
2022-07-15 14:07:07 -0700

    Refactor flag usage in cmd

--- cmd/utils.go
+++ cmd/utils.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"github.com/spf13/cobra"
+	"gitlab.com/elixxir/client/e2e"
+	"gitlab.com/elixxir/client/xxdk"
 	"io/ioutil"
 	"strconv"
 	"strings"
@@ -13,6 +16,56 @@ import (
 )
 
 // todo: go through cmd package and organize utility functions
+
+// bindFlagHelper binds the key to a pflag.Flag used by Cobra and prints an
+// error if one occurs.
+func bindFlagHelper(key string, command *cobra.Command) {
+	err := viper.BindPFlag(key, command.Flags().Lookup(key))
+	if err != nil {
+		jww.ERROR.Printf("viper.BindPFlag failed for %q: %+v", key, err)
+	}
+}
+
+func verifySendSuccess(client *xxdk.E2e, paramsE2E e2e.Params,
+	roundIDs []id.Round, partnerId *id.ID, payload []byte) bool {
+	retryChan := make(chan struct{})
+	done := make(chan struct{}, 1)
+
+	// Construct the callback function which
+	// verifies successful message send or retries
+	f := func(allRoundsSucceeded, timedOut bool,
+		rounds map[id.Round]cmix.RoundResult) {
+		printRoundResults(
+			rounds, roundIDs, payload, partnerId)
+		if !allRoundsSucceeded {
+			retryChan <- struct{}{}
+		} else {
+			done <- struct{}{}
+		}
+	}
+
+	// Monitor rounds for results
+	err := client.GetCmix().GetRoundResults(
+		paramsE2E.CMIXParams.Timeout, f, roundIDs...)
+	if err != nil {
+		jww.DEBUG.Printf("Could not verify messages were sent " +
+			"successfully, resending messages...")
+		return false
+	}
+
+	select {
+	case <-retryChan:
+		// On a retry, go to the top of the loop
+		jww.DEBUG.Printf("Messages were not sent successfully," +
+			" resending messages...")
+		return true
+	case <-done:
+		// Close channels on verification success
+		close(done)
+		close(retryChan)
+		return false
+	}
+}
 
 func parsePassword(pwStr string) []byte {
 	if strings.HasPrefix(pwStr, "0x") {
@@ -68,7 +121,7 @@ func printRoundResults(rounds map[id.Round]cmix.RoundResult, roundIDs []id.Round
 }
 
 func writeContact(c contact.Contact) {
-	outfilePath := viper.GetString("writeContact")
+	outfilePath := viper.GetString(writeContactFlag)
 	if outfilePath == "" {
 		return
 	}
@@ -83,6 +136,7 @@ func readContact(inputFilePath string) contact.Contact {
 	if inputFilePath == "" {
 		return contact.Contact{}
 	}
+
 	data, err := ioutil.ReadFile(inputFilePath)
 	jww.INFO.Printf("Contact file size read in: %d", len(data))
 	if err != nil {

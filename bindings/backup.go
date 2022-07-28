d0096aa5b8f696e17529cf4e75d9f92ae8038016
joshemb <josh@elixxir.io>
2022-07-28 13:28:29 -0700

    Expose fields in BackupReport

--- bindings/backup.go
+++ bindings/backup.go
@@ -26,10 +26,10 @@ type Backup struct {
 // NewCmixFromBackup.
 type BackupReport struct {
 	// The JSON encoded list of E2E partner IDs
-	backupIdListJson []byte
+	BackupIdListJson []byte
 
 	// The backup parameters found within the backup file
-	backupParams []byte
+	BackupParams []byte
 }
 
 // UpdateBackupFunc contains a function callback that returns new backups.
@@ -73,8 +73,8 @@ func NewCmixFromBackup(ndfJSON, storageDir string, sessionPassword,
 
 	// Construct report
 	report := BackupReport{
-		backupIdListJson: backupIdListJson,
-		backupParams:     []byte(backupParamsStr),
+		BackupIdListJson: backupIdListJson,
+		BackupParams:     []byte(backupParamsStr),
 	}
 
 	// Marshal report

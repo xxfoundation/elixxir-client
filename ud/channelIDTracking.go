81af270f8d7b02221986762a68ba51bd181a3db1
David Stainton <dstainton@elixxir.io>
2022-08-29 11:44:08 -0400

    WIP

--- ud/channelIDTracking.go
+++ ud/channelIDTracking.go
@@ -28,28 +28,6 @@ const (
 var startChannelNameServiceOnce sync.Once
 var ErrChannelLeaseSignature = errors.New("failure to validate lease signature")
 
-// NameService is an interface which encapsulates
-// the user identity channel tracking service.
-type NameService interface {
-
-	// GetUsername returns the username.
-	GetUsername() string
-
-	// GetChannelValidationSignature returns the validation
-	// signature and the time it was signed.
-	GetChannelValidationSignature() (signature []byte, lease time.Time)
-
-	// GetChannelPubkey returns the user's public key.
-	GetChannelPubkey() ed25519.PublicKey
-
-	// SignChannelMessage returns the signature of the
-	// given message.
-	SignChannelMessage(message []byte) (signature []byte, err error)
-
-	// ValidateChannelMessage
-	ValidateChannelMessage(username string, lease time.Time, pubKey ed25519.PublicKey, authorIDSignature []byte) bool
-}
-
 // loadRegistrationDisk loads a registrationDisk from the kv
 // and returns the registrationDisk.
 func loadRegistrationDisk(kv *versioned.KV) (registrationDisk, error) {

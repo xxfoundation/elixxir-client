e476d19d5e9e2611c57f0f9d47bc0f426433eae6
David Stainton <dstainton@elixxir.io>
2022-08-30 18:16:02 -0400

    Add test TestMutateTimestampDeltaAverage

--- channels/mutateTimestamp_test.go
+++ channels/mutateTimestamp_test.go
@@ -1,7 +1,11 @@
 package channels
 
 import (
+	"crypto/rand"
+	"testing"
 	"time"
+
+	"gitlab.com/elixxir/crypto/channel"
 )
 
 // withinMutationWindow is a utility test function to check if a mutated
@@ -11,4 +15,31 @@ func withinMutationWindow(raw, mutated time.Time) bool {
 	upperBound := raw.Add(time.Duration(halfTenMsInNs))
 
 	return mutated.After(lowerBound) && mutated.Before(upperBound)
+}
+
+func abs(n int64) int64 {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
+func TestMutateTimestampDeltaAverage(t *testing.T) {
+	samples := 10000
+	t1 := time.Now()
+	sum := int64(0)
+
+	for i := 0; i < samples; i++ {
+		var msgID channel.MessageID
+		rand.Read(msgID[:])
+		t2 := mutateTimestamp(t1, msgID)
+		delta := t2.Sub(t1)
+		sum += abs(int64(delta))
+	}
+
+	avg := sum / int64(samples)
+	diff := abs(avg - 2502865)
+	if diff > 30000 {
+		t.Fatal()
+	}
 }

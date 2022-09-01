d3027a2baecdcd99cbb41e38ca6f3e9d2777f797
Benjamin Wenger <ben@elixxir.ioo>
2022-09-01 09:26:56 -0700

    improved variable names

--- channels/emoji.go
+++ channels/emoji.go
@@ -15,7 +15,7 @@ import (
 //based on emojis found at https://unicode.org/emoji/charts/full-emoji-list.html
 const findEmoji = `[\xA9\xAE\x{2000}-\x{3300}\x{1F000}-\x{1FBFF}]`
 
-var compiledRegex = regexp.MustCompile(findEmoji)
+var compiledFindEmoji = regexp.MustCompile(findEmoji)
 
 // ValidateReaction checks that the reaction only contains a single emoji.
 func ValidateReaction(reaction string) error {
@@ -29,7 +29,7 @@ func ValidateReaction(reaction string) error {
 	reader := bytes.NewReader([]byte(reaction))
 
 	// make sure it has emojis
-	if !compiledRegex.MatchReader(reader) {
+	if !compiledFindEmoji.MatchReader(reader) {
 		return InvalidReaction
 	}
 

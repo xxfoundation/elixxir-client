a2076a41b1dc18876ad5bc56357a8d33c127da1a
David Stainton <dstainton@elixxir.io>
2022-09-15 16:57:37 -0400

    WIP ratchet

--- e2e/ratchet/ratchet.go
+++ e2e/ratchet/ratchet.go
@@ -16,6 +16,7 @@ import (
 	"gitlab.com/elixxir/client/cmix/message"
 	"gitlab.com/elixxir/client/e2e/ratchet/partner"
 	"gitlab.com/elixxir/client/e2e/ratchet/partner/session"
+	"gitlab.com/elixxir/client/interfaces/nike"
 	util "gitlab.com/elixxir/client/storage/utility"
 	"gitlab.com/elixxir/client/storage/versioned"
 	"gitlab.com/elixxir/crypto/cyclic"
@@ -96,6 +97,45 @@ func New(kv *versioned.KV, myID *id.ID, privKey *cyclic.Int,
 // AddPartner adds a partner. Automatically creates both send and receive
 // sessions using the passed cryptographic data and per the parameters sent
 func (r *Ratchet) AddPartner(partnerID *id.ID,
+	partnerPubKey, myPrivKey *cyclic.Int, partnerCTIDHPubKey nike.PublicKey,
+	myCTIDHPrivKey nike.PrivateKey, sendParams,
+	receiveParams session.Params) (partner.Manager, error) {
+	r.mux.Lock()
+	defer r.mux.Unlock()
+
+	myID := r.myID
+
+	myPubKey := diffieHellman.GeneratePublicKey(myPrivKey, r.grp)
+	jww.INFO.Printf("Adding Partner %s:\n\tMy Public Key: %s"+
+		"\n\tPartner Public Key: %s to %s",
+		partnerID,
+		myPubKey.TextVerbose(16, 0),
+		partnerPubKey.TextVerbose(16, 0), myID)
+
+	mid := *partnerID
+
+	if _, ok := r.managers[mid]; ok {
+		return nil, errors.New("Cannot overwrite existing partner")
+	}
+	m := partner.NewManager(r.kv, r.myID, partnerID, myPrivKey,
+		partnerPubKey, myCTIDHPrivKey, partnerCTIDHPubKey,
+		sendParams, receiveParams, r.cyHandler, r.grp, r.rng)
+
+	r.managers[mid] = m
+	if err := r.save(); err != nil {
+		jww.FATAL.Printf("Failed to add Partner %s: Save of store failed: %s",
+			partnerID, err)
+	}
+
+	// Add services for the manager
+	r.add(m)
+
+	return m, nil
+}
+
+// AddPartner adds a partner. Automatically creates both send and receive
+// sessions using the passed cryptographic data and per the parameters sent
+func (r *Ratchet) AddPartnerLegacySIDH(partnerID *id.ID,
 	partnerPubKey, myPrivKey *cyclic.Int, partnerSIDHPubKey *sidh.PublicKey,
 	mySIDHPrivKey *sidh.PrivateKey, sendParams,
 	receiveParams session.Params) (partner.Manager, error) {

f2eb195165c716bba402d844acb27d411fb3a5ca
David Stainton <dstainton@elixxir.io>
2022-09-30 20:51:19 -0400

    Add DH NIKE

---
+++ nike/dh/dh.go
@@ -0,0 +1,133 @@
+package dh
+
+import (
+	"gitlab.com/elixxir/client/interfaces/nike"
+
+	"gitlab.com/xx_network/crypto/csprng"
+	"gitlab.com/xx_network/crypto/large"
+
+	"gitlab.com/elixxir/crypto/cyclic"
+	"gitlab.com/elixxir/crypto/diffieHellman"
+)
+
+const (
+	bitSize        = 4096
+	groupSize      = bitSize / 8
+	privateKeySize = groupSize
+	publicKeySize  = groupSize
+)
+
+var primeString = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1" +
+	"29024E088A67CC74020BBEA63B139B22514A08798E3404DD" +
+	"EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245" +
+	"E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED" +
+	"EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D" +
+	"C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F" +
+	"83655D23DCA3AD961C62F356208552BB9ED529077096966D" +
+	"670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B" +
+	"E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9" +
+	"DE2BCBF6955817183995497CEA956AE515D2261898FA0510" +
+	"15728E5A8AACAA68FFFFFFFFFFFFFFFF"
+
+type dhNIKE struct{}
+
+// DHNIKE is essentially a factory type that builds
+// various types related to the DiffieHellman NIKE interface implementation.
+var DHNIKE = &dhNIKE{}
+
+var _ nike.PrivateKey = (*privateKey)(nil)
+var _ nike.PublicKey = (*publicKey)(nil)
+var _ nike.Nike = (*dhNIKE)(nil)
+
+func (d *dhNIKE) PublicKeySize() int {
+	return publicKeySize
+}
+
+func (d *dhNIKE) PrivateKeySize() int {
+	return privateKeySize
+}
+
+// UnmarshalBinaryPublicKey unmarshals the public key bytes.
+func (d *dhNIKE) UnmarshalBinaryPublicKey(b []byte) (nike.PublicKey, error) {
+	pubKey := &publicKey{
+		publicKey: new(cyclic.Int),
+	}
+	err := pubKey.FromBytes(b)
+	if err != nil {
+		return nil, err
+	}
+	return pubKey, nil
+}
+
+// UnmarshalBinaryPrivateKey unmarshals the public key bytes.
+func (d *dhNIKE) UnmarshalBinaryPrivateKey(b []byte) (nike.PrivateKey, error) {
+	privKey := &privateKey{
+		nike:       d,
+		privateKey: new(cyclic.Int),
+	}
+	err := privKey.FromBytes(b)
+	if err != nil {
+		return nil, err
+	}
+	return privKey, nil
+}
+
+func (d *dhNIKE) group() *cyclic.Group {
+	p := large.NewInt(1)
+	p.SetString(primeString, 16)
+	g := large.NewInt(2)
+	return cyclic.NewGroup(p, g)
+}
+
+func (d *dhNIKE) NewKeypair() (nike.PrivateKey, nike.PublicKey) {
+	rng := csprng.NewSystemRNG()
+	group := d.group()
+	privKey := diffieHellman.GeneratePrivateKey(privateKeySize, group, rng)
+	pubKey := diffieHellman.GeneratePublicKey(privKey, group)
+	return &privateKey{
+			nike:       d,
+			privateKey: privKey,
+		}, &publicKey{
+			publicKey: pubKey,
+		}
+}
+
+type privateKey struct {
+	nike       *dhNIKE
+	privateKey *cyclic.Int
+}
+
+func (p *privateKey) DeriveSecret(pubKey nike.PublicKey) []byte {
+	c := diffieHellman.GenerateSessionKey(p.privateKey,
+		(pubKey.(*publicKey)).publicKey,
+		p.nike.group())
+	return c.Bytes()
+}
+
+func (p *privateKey) Reset() {
+	p.privateKey = nil
+}
+
+func (p *privateKey) Bytes() []byte {
+	return p.privateKey.BinaryEncode()
+}
+
+func (p *privateKey) FromBytes(data []byte) error {
+	return p.privateKey.BinaryDecode(data)
+}
+
+type publicKey struct {
+	publicKey *cyclic.Int
+}
+
+func (p *publicKey) Reset() {
+	p.publicKey = nil
+}
+
+func (p *publicKey) Bytes() []byte {
+	return p.publicKey.BinaryEncode()
+}
+
+func (p *publicKey) FromBytes(data []byte) error {
+	return p.publicKey.BinaryDecode(data)
+}
---
+++ nike/dh/dh_test.go
@@ -0,0 +1,59 @@
+package dh
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNike(t *testing.T) {
+	alicePrivateKey, alicePublicKey := DHNIKE.NewKeypair()
+	bobPrivateKey, bobPublicKey := DHNIKE.NewKeypair()
+
+	secret1 := alicePrivateKey.DeriveSecret(bobPublicKey)
+	secret2 := bobPrivateKey.DeriveSecret(alicePublicKey)
+
+	require.Equal(t, secret1, secret2)
+}
+
+func TestPrivateKeyMarshaling(t *testing.T) {
+	alicePrivateKey, _ := DHNIKE.NewKeypair()
+
+	alicePrivateKeyBytes := alicePrivateKey.Bytes()
+	alice2PrivateKey, _ := DHNIKE.NewKeypair()
+
+	err := alice2PrivateKey.FromBytes(alicePrivateKeyBytes)
+	require.NoError(t, err)
+
+	alice2PrivateKeyBytes := alice2PrivateKey.Bytes()
+
+	require.Equal(t, alice2PrivateKeyBytes, alicePrivateKeyBytes)
+
+	alice3PrivateKey, err := DHNIKE.UnmarshalBinaryPrivateKey(alice2PrivateKeyBytes)
+	require.NoError(t, err)
+
+	alice3PrivateKeyBytes := alice3PrivateKey.Bytes()
+
+	require.Equal(t, alice3PrivateKeyBytes, alice2PrivateKeyBytes)
+}
+
+func TestPublicKeyMarshaling(t *testing.T) {
+	_, alicePublicKey := DHNIKE.NewKeypair()
+
+	alicePublicKeyBytes := alicePublicKey.Bytes()
+	alice2PublicKey, _ := DHNIKE.NewKeypair()
+
+	err := alice2PublicKey.FromBytes(alicePublicKeyBytes)
+	require.NoError(t, err)
+
+	alice2PublicKeyBytes := alice2PublicKey.Bytes()
+
+	require.Equal(t, alice2PublicKeyBytes, alicePublicKeyBytes)
+
+	alice3PublicKey, err := DHNIKE.UnmarshalBinaryPublicKey(alice2PublicKeyBytes)
+	require.NoError(t, err)
+
+	alice3PublicKeyBytes := alice3PublicKey.Bytes()
+
+	require.Equal(t, alice3PublicKeyBytes, alice2PublicKeyBytes)
+}

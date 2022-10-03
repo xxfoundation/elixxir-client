d7d73f40df1388f63d6cd6a0ae29d0e941bdea13
David Stainton <dstainton@elixxir.io>
2022-10-03 14:19:51 -0400

    ratchet: mv tests, add send/receive encrypt/decrypt test

--- ratchet/symmetric_ratchet_test.go
+++ ratchet/symmetric_ratchet_test.go
@@ -63,3 +63,38 @@ func TestRatchetMarshal(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, r1, r2)
 }
+
+func TestReceiveSendRatchetEncryptDecrypt(t *testing.T) {
+	salt := make([]byte, 32)
+	_, err := rand.Read(salt)
+	require.NoError(t, err)
+
+	size := uint32(32)
+
+	alicePrivateKey, alicePublicKey := DefaultNIKE.NewKeypair()
+	bobPrivateKey, bobPublicKey := DefaultNIKE.NewKeypair()
+
+	aliceReceiveRatchet, _ := NewReceiveRatchet(alicePrivateKey, bobPublicKey, salt, size)
+	aliceSendRatchet := NewSendRatchet(alicePrivateKey, alicePublicKey, bobPublicKey, salt, size)
+
+	bobReceiveRatchet, _ := NewReceiveRatchet(bobPrivateKey, alicePublicKey, salt, size)
+	bobSendRatchet := NewSendRatchet(bobPrivateKey, bobPublicKey, alicePublicKey, salt, size)
+
+	msg1 := []byte("hi bob")
+
+	ciphertext1, err := aliceSendRatchet.Encrypt(msg1)
+	require.NoError(t, err)
+
+	plaintext1, err := bobReceiveRatchet.Decrypt(ciphertext1)
+	require.NoError(t, err)
+	require.Equal(t, plaintext1, msg1)
+
+	msg2 := []byte("acknowledged")
+
+	ciphertext2, err := bobSendRatchet.Encrypt(msg2)
+	require.NoError(t, err)
+
+	plaintext2, err := aliceReceiveRatchet.Decrypt(ciphertext2)
+	require.NoError(t, err)
+	require.Equal(t, plaintext2, msg2)
+}

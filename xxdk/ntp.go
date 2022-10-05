6aad5ef9fb2364ba6904d4f32cb3c107aff5107c
benjamin <ben@elixxir.io>
2022-10-05 15:23:16 -0700

    made ntp use many servers and rotate randomly

--- xxdk/ntp.go
+++ xxdk/ntp.go
@@ -5,6 +5,7 @@ import (
 	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/client/stoppable"
 	"gitlab.com/xx_network/primitives/netTime"
+	"math/rand"
 	"sync/atomic"
 	"time"
 )
@@ -14,7 +15,14 @@ const (
 	runDelta   = 5 * 60 * time.Second
 )
 
-var host = "0.north-america.pool.ntp.org"
+var hosts = []string{"0.north-america.pool.ntp.org", "time-a-g.nist.gov", "time-b-g.nist.gov",
+	"time-c-g.nist.gov", "time-d-g.nist.gov", "time-d-g.nist.gov", "time-e-g.nist.gov",
+	"time-e-g.nist.gov", "time-a-wwv.nist.gov", "time-b-wwv.nist.gov", "time-c-wwv.nist.gov",
+	"time-d-wwv.nist.gov", "time-d-wwv.nist.gov", "time-e-wwv.nist.gov", "time-e-wwv.nist.gov",
+	"time-a-b.nist.gov", "time-b-b.nist.gov", "time-c-b.nist.gov", "time-d-b.nist.gov", "time-d-b.nist.gov",
+	"time-e-b.nist.gov", "time-e-b.nist.gov", "time.nist.gov", "utcnist.colorado.edu", "utcnist2.colorado.edu",
+	"time1.google.com", "time2.google.com", "time3.google.com", "time4.google.com", "0.pool.ntp.org",
+	"1.pool.ntp.org", "2.pool.ntp.org", "3.pool.ntp.org", "time.windows.com", "time.apple.com", "time.euro.apple.com"}
 
 type NtpTime struct {
 	offsets       []time.Duration
@@ -35,6 +43,7 @@ func (nt *NtpTime) Start() (stoppable.Stoppable, error) {
 	stopper := stoppable.NewSingle("ntp")
 	go func() {
 		for true {
+			host := hosts[rand.Int()%len(hosts)]
 			offset := nt.sync(host)
 			if len(nt.offsets) == numOffsets {
 				jww.INFO.Printf("Updated ntp time with '%s' current offset "+
@@ -59,7 +68,13 @@ func (nt *NtpTime) Now() time.Time {
 
 func (nt *NtpTime) sync(host string) int64 {
 
-	response, _ := ntp.Query(host)
+	response, err := ntp.Query(host)
+	if err != nil {
+		oldAvg := atomic.LoadInt64(nt.averageOffset)
+		jww.WARN.Printf("skipping poll of ntp, current offset: %s, error : %s",
+			time.Duration(oldAvg), err)
+		return oldAvg
+	}
 
 	if len(nt.offsets) >= numOffsets {
 		nt.offsets = append(nt.offsets[1:], response.ClockOffset)

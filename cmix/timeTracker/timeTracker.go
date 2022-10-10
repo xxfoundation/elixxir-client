334c576c76316a225fec3904a0c717d5433abd05
David Stainton <dstainton@elixxir.io>
2022-10-10 16:40:28 -0400

    timeTracker: add corrections from code review

--- cmix/timeTracker/timeTracker.go
+++ cmix/timeTracker/timeTracker.go
@@ -1,3 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
+
+// package timeTracker tracks local clock skew relative to gateways.
 package timeTracker
 
 import (
@@ -7,17 +15,27 @@ import (
 	"gitlab.com/xx_network/primitives/id"
 )
 
+const maxHistogramSize = 50
+
 // TimeOffsetTracker tracks local clock skew relative to various
 // gateways.
 type TimeOffsetTracker interface {
+	// Add additional data to our aggregate clock skews.
 	Add(gwID *id.ID, startTime, rTs time.Time, rtt, gwD time.Duration)
+
+	// Aggregate returns the average of the last n offsets.
 	Aggregate() time.Duration
+}
+
+type gatewayDelays struct {
+	lock   sync.RWMutex
+	delays []time.Duration
 }
 
 type timeOffsetTracker struct {
 	numAvg int
 
-	gatewayClockDelays map[id.ID][]time.Duration
+	gatewayClockDelays *sync.Map // id.ID -> []time.Duration
 
 	lock    sync.RWMutex
 	offsets []time.Duration
@@ -26,34 +44,31 @@ type timeOffsetTracker struct {
 // New returns an implementation of TimeOffsetTracker.
 func New() TimeOffsetTracker {
 	t := &timeOffsetTracker{
-		numAvg: 50,
+		numAvg:             maxHistogramSize,
+		gatewayClockDelays: new(sync.Map),
 	}
 	return t
 }
 
-// Add additional data to our aggregate clock skews.
 func (t *timeOffsetTracker) Add(gwID *id.ID, startTime, rTs time.Time, rtt, gwD time.Duration) {
 	delay := rtt/2 - gwD
 
+	delays, _ := t.gatewayClockDelays.LoadOrStore(*gwID, []time.Duration{})
+
+	gwdelays := delays.(*gatewayDelays)
+
+	gwdelays.lock.Lock()
+	pushDurationRing(delay, gwdelays.delays, t.numAvg)
+	gwDelay := average(gwdelays.delays)
+	gwdelays.lock.Unlock()
+
+	offset := startTime.Sub(rTs.Add(-gwDelay))
+
 	t.lock.Lock()
 	defer t.lock.Unlock()
-
-	_, ok := t.gatewayClockDelays[*gwID]
-	if !ok {
-		t.gatewayClockDelays[*gwID] = []time.Duration{}
-	}
-
-	pushDurationRing(delay, t.gatewayClockDelays[*gwID], t.numAvg)
-	gwDelay := average(t.gatewayClockDelays[*gwID])
-
-	delayTime1 := rTs.Add(-gwDelay)
-	offsetTime := startTime.Add(-rTs.Sub(delayTime1))
-	offset := offsetTime.Sub(startTime)
-
 	pushDurationRing(offset, t.offsets, t.numAvg)
 }
 
-// Aggregate returns the average of the last n offsets.
 func (t *timeOffsetTracker) Aggregate() time.Duration {
 	t.lock.RLock()
 	defer t.lock.RUnlock()

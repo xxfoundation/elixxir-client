b3bd9b68ffbd2b3d73ee8f60ed9478c0e15dad24
benjamin <ben@elixxir.io>
2022-10-11 13:04:16 -0700

    improved comments and made NowMs round properly

--- cmix/localTime.go
+++ cmix/localTime.go
@@ -1,10 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
+
 package cmix
 
 import "time"
 
+// describes a local time object which gets time
+// from the local clock in milliseconds
 type localTime struct{}
 
 func (localTime) NowMs() int64 {
 	t := time.Now()
-	return t.UnixNano() / int64(time.Millisecond)
+	return (t.UnixNano() + int64(time.Millisecond)/2) / int64(time.Millisecond)
 }

7327b5fed169a199b0b57fe67966b08e65ba130f
benjamin <ben@elixxir.io>
2022-10-20 14:26:42 -0700

    increased number of paraelle registrations to 20

--- cmix/params.go
+++ cmix/params.go
@@ -99,7 +99,7 @@ func GetDefaultParams() Params {
 		MaxCheckedRounds:          500,
 		RegNodesBufferLen:         1000,
 		NetworkHealthTimeout:      15 * time.Second,
-		ParallelNodeRegistrations: 5,
+		ParallelNodeRegistrations: 20,
 		KnownRoundsThreshold:      1500, // 5 rounds/sec * 60 sec/min * 5 min
 		FastPolling:               true,
 		VerboseRoundTracking:      false,

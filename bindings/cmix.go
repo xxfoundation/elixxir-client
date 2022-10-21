36729d5a2fc1dce070513f99649287446250e27f
Sydney Anne Erickson <sydney@elixxir.io>
2022-10-21 10:24:46 -0700

    move cmix and identity in bindings back

--- bindings/cmix.go
+++ bindings/cmix.go
@@ -5,7 +5,7 @@
 // LICENSE file.                                                              //
 ////////////////////////////////////////////////////////////////////////////////
 
-package bindings2
+package bindings
 
 import (
 	"sync"

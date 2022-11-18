832097f7dd10352e1e162845d586b683df7073fa
Richard T. Carback III <rick.carback@gmail.com>
2022-11-18 22:06:24 +0000

    Fix tests

--- channels/dm.go
+++ channels/dm.go
@@ -14,11 +14,13 @@ import (
 	"github.com/pkg/errors"
 	jww "github.com/spf13/jwalterweatherman"
 	"gitlab.com/elixxir/client/v4/storage/versioned"
+	cryptoChannel "gitlab.com/elixxir/crypto/channel"
 	"gitlab.com/elixxir/crypto/hash"
 	"gitlab.com/xx_network/primitives/id"
 	"gitlab.com/xx_network/primitives/id/ephemeral"
 	"gitlab.com/xx_network/primitives/netTime"
 	"golang.org/x/crypto/blake2b"
+	"google.golang.org/protobuf/proto"
 
 	"gitlab.com/elixxir/client/v4/cmix"
 	"gitlab.com/elixxir/client/v4/cmix/identity/receptionID"
@@ -34,8 +36,7 @@ import (
 const (
 	dmStoreKey              = "dmToken-%s"
 	dmStoreVersion          = 0
-	DirectMessage           = "direct_message"
-	directMessageServiceTag = "direct_message"
+	directMessageServiceTag = "direct_message_v0"
 )
 
 func DeriveReceptionID(publicKey nike.PublicKey, idToken []byte) *id.ID {
@@ -46,6 +47,7 @@ func DeriveReceptionID(publicKey nike.PublicKey, idToken []byte) *id.ID {
 	h.Write(publicKey.Bytes())
 	h.Write(idToken)
 	idBytes := h.Sum(nil)
+	idBytes = append(idBytes, byte(id.User))
 	receptionID, err := id.Unmarshal(idBytes)
 	if err != nil {
 		panic(err)
@@ -54,82 +56,224 @@ func DeriveReceptionID(publicKey nike.PublicKey, idToken []byte) *id.ID {
 }
 
 type dmClient struct {
-	receptionID        *id.ID
-	partnerReceptionID *id.ID
-	privateKey         nike.PrivateKey
-	partnerPubKey      nike.PublicKey
+	receptionID *id.ID
+	privateKey  nike.PrivateKey
+	publicKey   nike.PublicKey
+	myToken     []byte
 
+	nm  *nicknameManager
 	net Client
 	rng *fastRNG.StreamGenerator
 }
 
-func NewDMClient(privateEdwardsKey ed25519.PrivateKey,
-	partnerPublicKey ed25519.PublicKey,
-	myIdToken, partnerIdToken []byte,
+func NewDMClient(privateKey nike.PrivateKey,
+	myIdToken []byte, nickMgr *nicknameManager,
 	net Client,
 	rng *fastRNG.StreamGenerator) *dmClient {
 
-	privateKey := ecdh.ECDHNIKE.NewEmptyPrivateKey()
-	privateKey.(*ecdh.PrivateKey).FromEdwards(privateEdwardsKey)
 	publicKey := ecdh.ECDHNIKE.DerivePublicKey(privateKey)
 
-	partnerPubKey := ecdh.ECDHNIKE.NewEmptyPublicKey()
-	partnerPubKey.(*ecdh.PublicKey).FromEdwards(partnerPublicKey)
-
-	partnerReceptionID := DeriveReceptionID(partnerPubKey, partnerIdToken)
 	receptionID := DeriveReceptionID(publicKey, myIdToken)
 
+	// TODO: do we do the reception registration here or do we do
+	// it outside of this function?
+	// similarly, do we take a full cMix client or do we
+	// just take a sender function? I think I favor the latter...
+
 	return &dmClient{
-		receptionID:        receptionID,
-		partnerReceptionID: partnerReceptionID,
-		privateKey:         privateKey,
-		partnerPubKey:      partnerPubKey,
-		net:                net,
-		rng:                rng,
+		receptionID: receptionID,
+		privateKey:  privateKey,
+		publicKey:   publicKey,
+		nm:          nickMgr,
+		net:         net,
+		rng:         rng,
 	}
 }
 
-func (dc *dmClient) SendMessage(plaintext []byte, cMixParams cmix.CMIXParams) (rounds.Round, ephemeral.Id, error) {
+// GetDMNIKEPublicKey converts a public key from a signing key to a NIKE
+// (key exchange compatible) version of the the public key.
+func GetDMNIKEPublicKey(publicEdwardsKey *ed25519.PublicKey) nike.PublicKey {
+	publicKey := ecdh.ECDHNIKE.NewEmptyPublicKey()
+	publicKey.(*ecdh.PublicKey).FromEdwards(*publicEdwardsKey)
+	return publicKey
+}
 
-	assembler := func(rid id.Round) ([]byte, error) {
-		return plaintext, nil
+// GetDMNIKEPrivateKey converts a private key from a signing key to a NIKE
+// (key exchange compatible) version of the the private key.
+func GetDMNIKEPrivateKey(privateEdwardsKey *ed25519.PrivateKey) nike.PrivateKey {
+	privateKey := ecdh.ECDHNIKE.NewEmptyPrivateKey()
+	privateKey.(*ecdh.PrivateKey).FromEdwards(*privateEdwardsKey)
+	return privateKey
+}
+
+func (dc *dmClient) Send(partnerToken []byte, partnerPubKey nike.PublicKey,
+	messageType MessageType, msg []byte, params cmix.CMIXParams) (
+	cryptoChannel.MessageID, rounds.Round, ephemeral.Id, error) {
+
+	if params.DebugTag == cmix.DefaultDebugTag {
+		params.DebugTag = directMessageServiceTag
 	}
 
+	partnerID := DeriveReceptionID(partnerPubKey, partnerToken)
+
+	// Note: We log sends on exit, and append what happened to the message
+	// this cuts down on clutter in the log.
+	sendPrint := fmt.Sprintf("[%s] Sending dm to %s type %d at %s",
+		params.DebugTag, partnerID, messageType, netTime.Now())
+	defer jww.INFO.Println(sendPrint)
+
+	nickname, _ := dc.nm.GetNickname(partnerID)
+
+	var msgId cryptoChannel.MessageID
+
+	// Generate random nonce to be used for message ID
+	// generation. This makes it so two identical messages sent on
+	// the same round have different message IDs.
+	rng := dc.rng.GetStream()
+	msgNonce := make([]byte, messageNonceSize)
+	n, err := rng.Read(msgNonce)
+	rng.Close()
+	if err != nil {
+		sendPrint += fmt.Sprintf(", failed to generate nonce: %+v", err)
+		return cryptoChannel.MessageID{}, rounds.Round{},
+			ephemeral.Id{},
+			errors.Errorf("Failed to generate nonce: %+v", err)
+	} else if n != messageNonceSize {
+		sendPrint += fmt.Sprintf(
+			", got %d bytes for %d-byte nonce", n, messageNonceSize)
+		return cryptoChannel.MessageID{}, rounds.Round{},
+			ephemeral.Id{},
+			errors.Errorf(
+				"Generated %d bytes for %d-byte nonce", n,
+				messageNonceSize)
+	}
+
+	directMessage := &DirectMessage{
+		Lease:          0,
+		ECCPublicKey:   dc.publicKey.Bytes(),
+		DMToken:        dc.myToken,
+		PayloadType:    uint32(messageType),
+		Payload:        msg,
+		Nickname:       nickname,
+		Nonce:          msgNonce,
+		LocalTimestamp: netTime.Now().UnixNano(),
+	}
+
+	// Send to Partner
 	assemble := func(rid id.Round) (fp format.Fingerprint,
-		service message.Service, encryptedPayload, mac []byte, err error) {
-		payload, err := assembler(rid)
+		service message.Service, encryptedPayload, mac []byte,
+		err error) {
+
+		directMessage.RoundID = uint64(rid)
+
+		// Serialize the message
+		dmSerial, err := proto.Marshal(directMessage)
 		if err != nil {
-			return format.Fingerprint{}, message.Service{}, nil,
-				nil, err
+			return
 		}
+
+		// Make the messageID
+		msgId = cryptoChannel.MakeMessageID(dmSerial, partnerID)
+
+		// NOTE: When sending you use the partner id
+		//       When self sending you use your own id
+		//       Receiver figures out what to do based on msg content
+		service = message.Service{
+			Identifier: partnerID.Bytes(),
+			Tag:        directMessageServiceTag,
+		}
+
+		// As we don't use the mac or fp fields, we can extend
+		// our payload size
+		// (-2 to eliminate the first byte of mac and fp)
+		payloadLen := (dc.net.GetMaxMessageLength() +
+			format.MacLen + format.KeyFPLen - 2)
+
+		ciphertext := dm.Cipher.Encrypt(dmSerial, partnerPubKey,
+			payloadLen)
+
+		fpBytes, encryptedPayload, mac, err := dc.createCMIXFields(
+			ciphertext)
+
+		fp = format.NewFingerprint(fpBytes)
+
+		return fp, service, encryptedPayload, mac, err
+	}
+	partnerRnd, partnerEphID, err := dc.net.SendWithAssembler(partnerID,
+		assemble,
+		params)
+	if err != nil {
+		sendPrint += fmt.Sprintf(", err on partner send: %+v", err)
+	}
+	sendPrint += fmt.Sprintf(", partner send eph %s rnd %s id %s",
+		partnerEphID, partnerRnd.ID, msgId)
+
+	// SELF Send, NOTE: This is the send that returns the message ID
+	// for tracking. We can't track the receipt of the DM because we
+	// never pick it up.
+	selfAssemble := func(rid id.Round) (fp format.Fingerprint,
+		service message.Service, encryptedPayload, mac []byte,
+		err error) {
+
+		directMessage.RoundID = uint64(rid)
+		// NOTE: Very important to overwrite these fields
+		// for self sending!
+		directMessage.ECCPublicKey = partnerPubKey.Bytes()
+		directMessage.DMToken = partnerToken
+
+		// Serialize the message
+		dmSerial, err := proto.Marshal(directMessage)
+		if err != nil {
+			return
+		}
+
+		// Make the messageID
+		msgId = cryptoChannel.MakeMessageID(dmSerial, partnerID)
+
+		// NOTE: When sending you use the partner id
+		//       When self sending you use your own id
+		//       Receiver figures out what to do based on msg content
 		service = message.Service{
 			Identifier: dc.receptionID.Bytes(),
 			Tag:        directMessageServiceTag,
 		}
 
-		if cMixParams.DebugTag == cmix.DefaultDebugTag {
-			cMixParams.DebugTag = directMessageServiceTag
+		if params.DebugTag == cmix.DefaultDebugTag {
+			params.DebugTag = directMessageServiceTag
 		}
 
-		// Create payload sized for sending over cmix
-		sizedPayload := make([]byte, dc.net.GetMaxMessageLength())
-		// Read random data into sized payload
-		_, err = dc.rng.GetStream().Read(sizedPayload)
+		// As we don't use the mac or fp fields, we can extend
+		// our payload size
+		// (-2 to eliminate the first byte of mac and fp)
+		payloadLen := (dc.net.GetMaxMessageLength() +
+			format.MacLen + format.KeyFPLen - 2)
+
+		// FIXME: Why does this one return an error when the
+		// other doesn't!?
+		ciphertext, err := dm.Cipher.EncryptSelf(dmSerial, dc.privateKey,
+			payloadLen)
 		if err != nil {
-			return format.Fingerprint{}, message.Service{}, nil,
-				nil, errors.WithMessage(err, "Failed to add "+
-					"random data to sized broadcast")
+			return
 		}
-		encryptedPayload = dm.Cipher.Encrypt(payload, dc.partnerPubKey,
-			dc.net.GetMaxMessageLength())
-		copy(sizedPayload[:len(encryptedPayload)], encryptedPayload)
 
-		return
+		fpBytes, encryptedPayload, mac, err := dc.createCMIXFields(
+			ciphertext)
+
+		fp = format.NewFingerprint(fpBytes)
+
+		return fp, service, encryptedPayload, mac, err
 	}
+	myRnd, myEphID, err := dc.net.SendWithAssembler(dc.receptionID,
+		selfAssemble,
+		params)
+	if err != nil {
+		sendPrint += fmt.Sprintf(", err on self send: %+v", err)
+	}
+	sendPrint += fmt.Sprintf(", self send eph %s rnd %s id %s",
+		myEphID, myRnd.ID, msgId)
 
-	return dc.net.SendWithAssembler(dc.partnerReceptionID,
-		assemble,
-		cMixParams)
+	return msgId, myRnd, myEphID, err
+
 }
 
 // RegisterListener registers a listener for broadcast messages.
@@ -159,7 +303,8 @@ type processor struct {
 	cb ListenerFunc
 }
 
-// String returns a string identifying the symmetricProcessor for debugging purposes.
+// String returns a string identifying the symmetricProcessor for
+// debugging purposes.
 func (p *processor) String() string {
 	return "directMessage-"
 }
@@ -168,7 +313,17 @@ func (p *processor) String() string {
 func (p *processor) Process(msg format.Message,
 	receptionID receptionID.EphemeralIdentity, round rounds.Round) {
 
-	payload, err := dm.Cipher.Decrypt(msg.GetContents(), p.c.privateKey)
+	ciphertext := reconstructCiphertext(msg)
+
+	var payload []byte
+	var err error
+	if dm.Cipher.IsSelfEncrypted(ciphertext, p.c.privateKey) {
+		payload, err = dm.Cipher.DecryptSelf(ciphertext,
+			p.c.privateKey)
+	} else {
+		payload, err = dm.Cipher.Decrypt(ciphertext,
+			p.c.privateKey)
+	}
 	if err != nil {
 		jww.ERROR.Printf("failed to decrypt direct message: %s", err)
 		return
@@ -227,4 +382,63 @@ func hashPrivateKey(privKey *ed25519.PrivateKey) []byte {
 
 	h.Write(privKey.Seed())
 	return h.Sum(nil)
+}
+
+// Helper function that splits up the ciphertext into the appropriate cmix
+// packet subfields
+func (dc *dmClient) createCMIXFields(ciphertext []byte) (fpBytes,
+	encryptedPayload, mac []byte, err error) {
+
+	rng := dc.rng.GetStream()
+	defer rng.Close()
+
+	fpBytes = make([]byte, format.KeyFPLen)
+	mac = make([]byte, format.MacLen)
+	encryptedPayload = make([]byte, dc.net.GetMaxMessageLength())
+
+	// The first byte of mac and fp are random
+	prefixBytes := make([]byte, 2)
+	n, err := rng.Read(prefixBytes)
+	if err != nil || n != len(prefixBytes) {
+		err = fmt.Errorf("rng read failure: %+v", err)
+		return nil, nil, nil, err
+	}
+	fpBytes[0] = prefixBytes[0]
+	mac[0] = prefixBytes[1]
+
+	// ciphertext[0:FPLen-1] == fp[1:FPLen]
+	start := 0
+	end := format.KeyFPLen
+	copy(fpBytes[1:format.KeyFPLen], ciphertext[start:end])
+	// ciphertext[FPLen-1:FPLen+MacLen-2] == mac[1:MacLen]
+	start = end
+	end = start + format.MacLen - 1
+	copy(mac[1:format.MacLen], ciphertext[start:end])
+	// ciphertext[FPLen+MacLen-2:] == encryptedPayload
+	start = end
+	end = start + len(encryptedPayload)
+	copy(encryptedPayload, ciphertext[start:end])
+
+	// Fill anything left w/ random data
+	numLeft := end - start - len(encryptedPayload)
+	if numLeft > 0 {
+		jww.WARN.Printf("[DM] small ciphertext, added %d bytes",
+			numLeft)
+		n, err := rng.Read(encryptedPayload[end-start:])
+		if err != nil || n != numLeft {
+			err = fmt.Errorf("rng read failure: %+v", err)
+			return nil, nil, nil, err
+		}
+	}
+
+	return fpBytes, encryptedPayload, mac, nil
+}
+
+// This helper does the opposite of "createCMIXFields" above
+func reconstructCiphertext(msg format.Message) []byte {
+	fp := msg.GetKeyFP()
+	res := fp[1:]
+	res = append(res, msg.GetMac()[1:]...)
+	res = append(res, msg.GetContents()...)
+	return res
 }

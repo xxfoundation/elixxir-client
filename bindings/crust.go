1b998c23c6dab4498c4361b0a0fac583675c0e6d
joshemb <josh@elixxir.io>
2022-12-02 11:22:43 -0800

    Add documentation for upload backup report

--- bindings/crust.go
+++ bindings/crust.go
@@ -14,6 +14,27 @@ import (
 	"gitlab.com/xx_network/primitives/utils"
 )
 
+// UploadSuccessReport is the report received when calling UploadBackup.
+// Example JSON:
+//   {
+//     "requestid":"f98fa83e-209c-42b1-be05-011e11643162-1670008805297",
+//     "status":"queued",
+//     "created":"2022-12-02T19:20:05Z",
+//     "pin":{
+//        "cid":"QmUe8UmQ5iEKHQNp9z5HN7fXmcf666zUuPb6N24oiGq31G",
+//        "name":"LoremIpsum.txt",
+//        "origins":[
+//        ]
+//     },
+//     "delegates":[
+//        "/ip4/183.131.193.198/tcp/14001/p2p/12D3KooWMcAHcs97R49PLZjGUKDbP1fr9iijeepod8fkktHTLCgN"
+//     ],
+//     "info":{}
+//  }
+type UploadSuccessReport struct {
+	crust.UploadSuccessReport
+}
+
 // UploadBackup will upload the file provided to the distributed file server.
 // This will return a UploadSuccessReport, which provides data on the status of
 // the upload. The file may be recovered using RecoverBackup.
@@ -44,7 +65,9 @@ func UploadBackup(filePath string, udManager *UserDiscovery,
 		return nil, err
 	}
 
-	return json.Marshal(uploadSuccessReport)
+	report := UploadSuccessReport{*uploadSuccessReport}
+
+	return json.Marshal(report)
 }
 
 // RecoverBackup retrieves the backup file uploaded to the distributed file

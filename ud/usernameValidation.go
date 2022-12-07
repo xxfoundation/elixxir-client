b2b676b27d595f08579dabbc8080b28580455482
joshemb <josh@elixxir.io>
2022-12-06 22:16:19 -0800

    [CRUST] Avoid querying network for UD signature

--- ud/usernameValidation.go
+++ ud/usernameValidation.go
@@ -10,10 +10,17 @@ package ud
 import (
 	"github.com/pkg/errors"
 	jww "github.com/spf13/jwalterweatherman"
+	"gitlab.com/elixxir/client/storage/versioned"
 	pb "gitlab.com/elixxir/comms/mixmessages"
 	"gitlab.com/elixxir/crypto/partnerships/crust"
 	"gitlab.com/xx_network/comms/connect"
 	"gitlab.com/xx_network/crypto/signature/rsa"
+	"gitlab.com/xx_network/primitives/netTime"
+)
+
+const (
+	usernameValidationStore   = "usernameValidationStore"
+	usernameValidationVersion = 0
 )
 
 // userValidationComms is a sub-interface of the Comms interface for
@@ -36,22 +43,46 @@ func (m *Manager) GetUsernameValidationSignature() ([]byte, error) {
 	defer m.usernameValidationMux.Unlock()
 	var err error
 
-	// If validation signature is not present, request it from
-	// UD
 	if m.usernameValidationSignature == nil {
-		m.usernameValidationSignature, err = m.getUsernameValidationSignature(m.comms)
+		// If validation signature is not present in memory
+		// load it from storage or request it from the network
+		m.usernameValidationSignature, err = m.loadOrGetUsernameValidation()
 		if err != nil {
-			return nil, errors.Errorf("Failed to retrieve signature from "+
-				"UD: %v", err)
+			return nil, err
 		}
 	}
 
 	return m.usernameValidationSignature, nil
 }
 
+// loadOrGetUsernameValidation is a helper function which will lazily load the
+// username validation signature from storage. If it does not exist in storage,
+// then it will query the signature from the UD service.
+func (m *Manager) loadOrGetUsernameValidation() ([]byte, error) {
+	var validationSignature []byte
+
+	// Attempt storage retrieval
+	obj, err := m.storage.GetKV().Get(usernameValidationStore, usernameValidationVersion)
+	if err != nil {
+		// If validation signature is not present in storage,
+		// request the username from the network
+		validationSignature,
+			err = m.queryUsernameValidationSignature(m.comms)
+		if err != nil {
+			return nil, errors.Errorf("Failed to retrieve signature from "+
+				"UD: %v", err)
+		}
+	} else {
+		// Put stored data in the object data
+		validationSignature = obj.Data
+	}
+
+	return validationSignature, nil
+}
+
 // getUsernameValidationSignature is the helper function which queries
 // the signature from the UD service.
-func (m *Manager) getUsernameValidationSignature(
+func (m *Manager) queryUsernameValidationSignature(
 	comms userValidationComms) ([]byte, error) {
 
 	// Construct request for username validation
@@ -74,7 +105,7 @@ func (m *Manager) getUsernameValidationSignature(
 		return nil, err
 	}
 
-	jww.INFO.Printf("[CRUST] Retrieved username validation from UD...")
+	jww.INFO.Printf("[CRUST] Retrieved username validation from UD.")
 
 	publicKey, err := rsa.LoadPublicKeyFromPem(response.ReceptionPublicKeyPem)
 	if err != nil {
@@ -85,6 +116,18 @@ func (m *Manager) getUsernameValidationSignature(
 	err = crust.VerifyVerificationSignature(host.GetPubKey(),
 		crust.HashUsername(response.Username),
 		publicKey, response.Signature)
+	if err != nil {
+		return nil, err
+	}
+
+	// Store request
+	err = m.storage.GetKV().Set(usernameValidationStore,
+		usernameValidationVersion,
+		&versioned.Object{
+			Version:   usernameValidationVersion,
+			Timestamp: netTime.Now(),
+			Data:      response.Signature,
+		})
 	if err != nil {
 		return nil, err
 	}

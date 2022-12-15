4e32637af707deb05dd53dc037bb6d11f786ac0d
benjamin <ben@elixxir.io>
2022-12-15 13:02:51 -0800

    fixed the js ip handling

--- registration/connect_js.go
+++ registration/connect_js.go
@@ -8,24 +8,26 @@ const defaultIP = "35.157.32.59"
 // getAddress returns the correct connection info. For non webassembly,
 // it is a simple pass through. For webassembly, it does not
 // return the cert
-func getConnectionInfo(regAddr, certificate string) (addr string, cert []byte, err error) {
+func getConnectionInfo2(regAddr, certificate string) (addr string, cert []byte, err error) {
 
 	var ip string
 
-	if regAddr == defaultURl {
-		ip = defaultIP
-	} else {
-		var err error
-		ip, err = net.LookupIP(regAddr)
-		if err != nil {
-			return "", nil, err
-		}
-	}
-
-	_, gwPort, err := net.SplitHostPort(gwAddr)
+	regUrl, gwPort, err := net.SplitHostPort(regAddr)
 	if err != nil {
 		err = errors.WithMessage(err, "Failed to find port on provided URL")
 		return "", nil, err
+	}
+
+	if regUrl == defaultURl {
+		ip = defaultIP
+	} else {
+		var err error
+		ipObj, err := net.LookupIP(regUrl)
+		if err != nil {
+			return "", nil, err
+		}
+
+		ip = ipObj[0].String()
 	}
 
 	addr = ip + ":" + gwPort

950aa5ee9b9c6eb43177c80f2606db56797711cc
benjamin <ben@elixxir.io>
2022-12-15 13:04:20 -0800

    fixed the js ip handling

--- registration/connect_js.go
+++ registration/connect_js.go
@@ -12,20 +12,21 @@ func getConnectionInfo(regAddr, certificate string) (addr string, cert []byte, e
 
 	var ip string
 
-	if regAddr == defaultURl {
-		ip = defaultIP
-	} else {
-		var err error
-		ip, err = net.LookupIP(regAddr)
-		if err != nil {
-			return "", nil, err
-		}
-	}
-
-	_, gwPort, err := net.SplitHostPort(gwAddr)
+	regUrl, gwPort, err := net.SplitHostPort(regAddr)
 	if err != nil {
 		err = errors.WithMessage(err, "Failed to find port on provided URL")
 		return "", nil, err
+	}
+
+	if regUrl == defaultURl {
+		ip = defaultIP
+	} else {
+		ipObj, err := net.LookupIP(regUrl)
+		if err != nil {
+			return "", nil, err
+		}
+
+		ip = ipObj[0].String()
 	}
 
 	addr = ip + ":" + gwPort

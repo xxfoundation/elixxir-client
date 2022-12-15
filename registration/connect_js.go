0810c27d01eecd0e48e385710b32ab7115afc0e8
benjamin <ben@elixxir.io>
2022-12-15 12:33:33 -0800

    implemented https support, has deps issues

--- registration/connect_js.go
+++ registration/connect_js.go
@@ -8,7 +8,7 @@ const defaultIP = "35.157.32.59"
 // getAddress returns the correct connection info. For non webassembly,
 // it is a simple pass through. For webassembly, it does not
 // return the cert
-func getConnectionInfo(regAddr, certificate string) (addr string, cert []byte, error) {
+func getConnectionInfo(regAddr, certificate string) (addr string, cert []byte, err error) {
 
 	var ip string
 

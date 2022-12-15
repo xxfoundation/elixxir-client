4e319d6428768020f6ff6f717bacbb32fca349f9
benjamin <ben@elixxir.io>
2022-12-15 13:57:26 -0800

    fixed registar's js address handling

--- registration/connect_js.go
+++ registration/connect_js.go
@@ -1,39 +1,17 @@
 package registration
 
-import (
-	"github.com/pkg/errors"
-	"net"
-)
+import "strings"
 
-const defaultURl = "registration.mainnet.cmix.rip"
-const defaultIP = "35.157.32.59"
+const toReplace = "registration"
+const replaceWith = "registrar"
 
 // getAddress returns the correct connection info. For non webassembly,
 // it is a simple pass through. For webassembly, it does not
 // return the cert
 func getConnectionInfo(regAddr, certificate string) (addr string, cert []byte, err error) {
+	addr = strings.Replace(regAddr, toReplace, replaceWith)
 
-	var ip string
-
-	regUrl, gwPort, err := net.SplitHostPort(regAddr)
-	if err != nil {
-		err = errors.WithMessage(err, "Failed to find port on provided URL")
-		return "", nil, err
-	}
-
-	if regUrl == defaultURl {
-		ip = defaultIP
-	} else {
-		ipObj, err := net.LookupIP(regUrl)
-		if err != nil {
-			return "", nil, err
-		}
-
-		ip = ipObj[0].String()
-	}
-
-	addr = ip + ":" + gwPort
-	cert = nil
+	cert = []byte(certificate)
 
 	return addr, cert, nil
 }

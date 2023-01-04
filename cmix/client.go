c71481456c4ddb53539652735a1d1deab9a9bde5
benjamin <ben@elixxir.io>
2023-01-04 13:04:02 -0800

    fixed missing comms

--- cmix/client.go
+++ cmix/client.go
@@ -181,7 +181,7 @@ func (c *client) initialize(ndfile *ndf.NetworkDefinition) error {
 	// Enable optimized HostPool initialization
 	poolParams.MaxPings = 50
 	sender, err := gateway.NewSender(poolParams, c.rng, ndfile, c.comms,
-		c.session, nodeChan)
+		c.session, c.comms, nodeChan)
 	if err != nil {
 		return err
 	}

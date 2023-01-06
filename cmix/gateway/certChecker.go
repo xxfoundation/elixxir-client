caa7c150a93039f0c1a972b01a9bcbfb4c61cadf
jbhusson <jonah@elixxir.io>
2023-01-06 17:20:24 -0500

    Disable checking against actual remote cert until fixed, update downstream to use https/options

--- cmix/gateway/certChecker.go
+++ cmix/gateway/certChecker.go
@@ -67,12 +67,18 @@ func (cc *certChecker) CheckRemoteCertificate(gwHost *connect.Host) error {
 	remoteCertSignature := gwTlsCertResp.GetSignature()
 	declaredFingerprint := blake2b.Sum256(gwTlsCertResp.GetCertificate())
 
-	// Get remote certificate used for connection from the host object
-	actualRemoteCert, err := gwHost.GetRemoteCertificate()
-	if err != nil {
-		return err
-	}
-	actualFingerprint := blake2b.Sum256(actualRemoteCert.Raw)
+	// FIXME: http package does not set the tls certificate when compiled for js/wasm
+	// This is because the js implementation uses fetch in javascript, which does not get this information
+	// See https://cs.opensource.google/go/go/+/refs/tags/go1.19.4:src/net/http/roundtrip_js.go
+	// Potential fix (for firefox only) is acessing request security information via webRequest
+	// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest#accessing_security_information
+	//// Get remote certificate used for connection from the host object
+	//actualRemoteCert, err := gwHost.GetRemoteCertificate()
+	//if err != nil {
+	//	return err
+	//}
+	rawActualRemoteCert := gwTlsCertResp.Certificate         //actualRemoteCert.Raw
+	actualFingerprint := blake2b.Sum256(rawActualRemoteCert) //actualRemoteCert.Raw)
 
 	// If the fingerprints of the used & declared certs do not match, return an error
 	if actualFingerprint != declaredFingerprint {
@@ -90,7 +96,7 @@ func (cc *certChecker) CheckRemoteCertificate(gwHost *connect.Host) error {
 	}
 
 	// Verify received signature
-	err = verifyRemoteCertificate(actualRemoteCert.Raw, remoteCertSignature, gwHost)
+	err = verifyRemoteCertificate(rawActualRemoteCert, remoteCertSignature, gwHost)
 	if err != nil {
 		return err
 	}

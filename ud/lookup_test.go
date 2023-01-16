b9874960a7141503d2b3532d5553133f9811a87d
Richard T. Carback III <rick.carback@gmail.com>
2023-01-16 21:03:29 +0000

    Bump timeout more

--- ud/lookup_test.go
+++ ud/lookup_test.go
@@ -82,7 +82,7 @@ func TestManager_Lookup(t *testing.T) {
 	t.Logf("%v", r.Bytes())
 	t.Logf("%s", s)
 
-	timeout := 1000 * time.Millisecond
+	timeout := 2000 * time.Millisecond
 	p := single.RequestParams{
 		Timeout:             timeout,
 		MaxResponseMessages: 1,

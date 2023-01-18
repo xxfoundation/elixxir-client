d297582411c57d65a53752e3002b96e0348d30e5
joshemb <josh@elixxir.io>
2023-01-17 17:00:25 -0800

    WIP: Add tests for Emoji.Set

---
+++ emoji/emojiSet_test.go
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file                                                               //
+////////////////////////////////////////////////////////////////////////////////
+
+package emoji
+
+import "testing"
+
+func TestSet_SanitizeFrontendEmojiList(t *testing.T) {
+
+}

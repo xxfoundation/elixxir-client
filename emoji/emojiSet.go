bdf2e4c9bbe0e9d366f1555eda99cacfd927c25f
joshemb <josh@elixxir.io>
2023-01-18 11:24:35 -0800

    Finalize emoji set logic

--- emoji/emojiSet.go
+++ emoji/emojiSet.go
@@ -19,28 +19,28 @@ import (
 type Set struct {
 	// replacementMap contains a list of emoji code-points in the front end list
 	// that must be replaced to adhere to backend recognized code-points.
-	replacementMap map[string]string
+	replacementMap map[codepoint]codepoint
 
-	// backendEmojiList contains a list of all Unicode codepoints for the emojis
-	// within the gomoji.emojiMap. This allows for quick lookup when comparing
-	// against the frontend list of emojis.
-	backendEmojiList map[string]struct{}
+	// supportedEmojis contains a list of all Unicode codepoints for the emojis
+	// that are supported. This allows for quick lookup when comparing against
+	// the frontend list of emojis.
+	supportedEmojis map[codepoint]struct{}
 }
 
 // NewSet constructs a Set.
 func NewSet() *Set {
 	return &Set{
-		replacementMap: map[string]string{
+		replacementMap: map[codepoint]codepoint{
 			"❤️": "❤", // In Unicode: "\u2764\ufe0f":"\u2764"
 		},
-		backendEmojiList: emojiListToMap(gomoji.AllEmojis()),
+		supportedEmojis: emojiListToMap(gomoji.AllEmojis()),
 	}
 }
 
 // SanitizeFrontendEmojiList will sanitize the list of emojis that front end
 // supports. It will be sanitized by modifying the list to determine the union
 // of supported emojis between front end (EmojiMart) and backend (gomoji.emojiMap).
-func (es *Set) SanitizeFrontendEmojiList(frontendEmojiSetJson []byte) ([]byte, error) {
+func (s *Set) SanitizeFrontendEmojiList(frontendEmojiSetJson []byte) ([]byte, error) {
 
 	// Unmarshal front end's JSON
 	var frontEndEmojiSet emojiMartData
@@ -49,57 +49,95 @@ func (es *Set) SanitizeFrontendEmojiList(frontendEmojiSetJson []byte) ([]byte, e
 		return nil, errors.Errorf("Failed to unmarshal front end emoji JSON: %+v", err)
 	}
 
-	// Go through all emojis front end supports and determine
-	// what can be supported on the backend
-	var emojisToRemove []string
-	for char, Emoji := range frontEndEmojiSet.Emojis {
+	// Find all incompatible emojis in the front end set
+	emojisToRemove := s.findIncompatibleEmojis(&frontEndEmojiSet)
+
+	// Remove all incompatible emojis from the set
+	removeIncompatibleEmojis(&frontEndEmojiSet, emojisToRemove)
+
+	return json.Marshal(frontEndEmojiSet)
+}
+
+// findIncompatibleEmojis returns a list of emojis in the emojiMartData that are
+// not supported by the Set. Also, any emojiMartData emoji codepoints that are
+// incompatible and have replacements (as defined in Set) are replaced.
+func (s *Set) findIncompatibleEmojis(set *emojiMartData) (emojisToRemove []emojiID) {
+	// Iterate over all emojis in the emojiMartData.Emojis list
+	for char, Emoji := range set.Emojis {
 		var newSkins []skin
 		for _, Skin := range Emoji.Skins {
-			// Determine if we must replace or remove an emoji
-			replacement, replace := es.replace(Skin.Unified)
+			// Determine if the emoji's codepoint should be replaced or removed
+			replacement, replace := s.replace(Skin.Unified)
 			if replace {
 				newSkins = append(newSkins, skin{replacement, Skin.Native})
-			} else if !es.remove(Skin.Unified) {
+			} else if !s.remove(Skin.Unified) {
 				newSkins = append(newSkins, Skin)
 			}
 		}
 
 		if len(newSkins) > 0 {
-			// Write to the set the possible edits (possible no edits were made)
+			// Write to the set the possible edits (if emojis were replaced
+			// or removed)
 			Emoji.Skins = newSkins
-			frontEndEmojiSet.Emojis[char] = Emoji
+			set.Emojis[char] = Emoji
 		} else {
+			// If all skins have been removed, then mark the emoji for removal
 			emojisToRemove = append(emojisToRemove, char)
 		}
 	}
 
-	// This will remove from the front end set any emojis that does not exist
-	// within the backend set.
+	return emojisToRemove
+}
+
+// removeIncompatibleEmojis removes all the emojis in emojisToRemove from the
+// emojiMartData set.
+func removeIncompatibleEmojis(set *emojiMartData, emojisToRemove []emojiID) {
+	// Remove all incompatible emojis from the emojiMartData.Emojis list
 	for _, char := range emojisToRemove {
-		delete(frontEndEmojiSet.Emojis, char)
+		delete(set.Emojis, char)
 	}
 
-	return json.Marshal(frontEndEmojiSet)
+	// Remove all incompatible emojis from the emojiMartData.Categories list
+	for _, cat := range set.Categories {
+		// Iterate over the emoji list backwards to make removal of elements
+		// from the slice easier
+		for i := len(cat.Emojis) - 1; i >= 0; i-- {
+			for _, char := range emojisToRemove {
+				if cat.Emojis[i] == char {
+					cat.Emojis = append(cat.Emojis[:i], cat.Emojis[i+1:]...)
+				}
+			}
+		}
+	}
+
+	// Remove all incompatible emojis from the emojiMartData.Aliases list
+	for alias, id := range set.Aliases {
+		for _, char := range emojisToRemove {
+			if id == char {
+				delete(set.Aliases, alias)
+			}
+		}
+	}
 }
 
 // replace returns whether the front end Unicode codepoint must be replaced.
 // It will return a boolean on whether this codepoint needs to be replaced
 // and what the codepoint must be replaced with.
-func (es *Set) replace(codePoint string) (replacement string, replace bool) {
-	replacement, replace = es.replacementMap[codePoint]
+func (s *Set) replace(code codepoint) (replacement codepoint, replace bool) {
+	replacement, replace = s.replacementMap[code]
 	return replacement, replace
 }
 
 // remove returns true if the code point should be removed from the parent list.
-func (es *Set) remove(codePoint string) bool {
-	_, exists := es.backendEmojiList[codePoint]
+func (s *Set) remove(code codepoint) bool {
+	_, exists := s.supportedEmojis[code]
 	return !exists
 }
 
 // emojiListToMap constructs a map for simple lookup for gomoji.Emoji's
 // Unicode codepoint.
-func emojiListToMap(list []gomoji.Emoji) map[string]struct{} {
-	emojiMap := make(map[string]struct{}, len(list))
+func emojiListToMap(list []gomoji.Emoji) map[codepoint]struct{} {
+	emojiMap := make(map[codepoint]struct{}, len(list))
 	for _, emoji := range list {
 		emojiMap[backToFrontCodePoint(emoji.CodePoint)] = struct{}{}
 	}
@@ -109,6 +147,6 @@ func emojiListToMap(list []gomoji.Emoji) map[string]struct{} {
 // backToFrontCodePoint converts Unicode codepoint format found in gomoji.Emoji
 // to the one passed in by frontend. The specific conversion is making it
 // lowercase and replacing " " with "-".
-func backToFrontCodePoint(codePoint string) string {
-	return strings.ToLower(strings.ReplaceAll(codePoint, " ", "-"))
+func backToFrontCodePoint(code string) codepoint {
+	return codepoint(strings.ToLower(strings.ReplaceAll(code, " ", "-")))
 }

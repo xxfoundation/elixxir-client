0a609323991d85e82c5616149e32c6f3f6715f76
Richard T. Carback III <rick.carback@gmail.com>
2023-01-18 16:12:28 +0000

    status test lockin

---
+++ dm/status_test.go
@@ -0,0 +1,30 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file                                                               //
+////////////////////////////////////////////////////////////////////////////////
+
+package dm
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// These strings should never change, so lock them with a test.
+func TestStatusString(t *testing.T) {
+	unsent := Unsent
+	sent := Sent
+	received := Received
+	failed := Failed
+
+	invalid := Status(4)
+
+	require.Equal(t, unsent.String(), "unsent")
+	require.Equal(t, sent.String(), "sent")
+	require.Equal(t, received.String(), "received")
+	require.Equal(t, failed.String(), "failed")
+	require.Equal(t, invalid.String(), "Invalid SentStatus: 4")
+}

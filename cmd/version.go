6b208ab850d4c8f9fc5be381a989c86625ce35e6
Jake Taylor <jake@elixxir.io>
2023-01-19 11:34:51 -0600

    update version to 4.4.4

--- cmd/version.go
+++ cmd/version.go
@@ -18,7 +18,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "4.4.3"
+const currentVersion = "4.4.4"
 
 func Version() string {
 	out := fmt.Sprintf("Elixxir Client v%s -- %s\n\n", xxdk.SEMVER,

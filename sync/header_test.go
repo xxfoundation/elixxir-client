d5044af9591c8913e295ad2b1b774d67325e50a5
joshemb <josh@elixxir.io>
2023-02-02 09:58:41 -0800

    Add newline edge check test for sync.Header

--- sync/header_test.go
+++ sync/header_test.go
@@ -16,6 +16,30 @@ import (
 )
 
 const expectedJsonOutput = `{"version":0,"entries":{"key0":"val0","key1":"val1","key2":"val2","key3":"val3","key4":"val4","key5":"val5","key6":"val6","key7":"val7","key8":"val8","key9":"val9"}}`
+const (
+	// expectedHeaderJsonNewline is the result of calling json.MarshalIndent
+	// on a Header object. expectedHeaderJsonNewline is presented with idents to
+	// illustrate that the newline character `\n` is parsed as part of the key
+	// and not as the escape character. Note that if one were to json.Unmarshal
+	// this back into a Header object, then json.Marshal that object, the output
+	// would be a single line of text.
+	expectedHeaderJsonNewline = `{
+	"version": 0,
+	"entries": {
+		"edgeCheckKey\n": "edgeCheckVal",
+		"key0": "val0",
+		"key1": "val1",
+		"key2": "val2",
+		"key3": "val3",
+		"key4": "val4",
+		"key5": "val5",
+		"key6": "val6",
+		"key7": "val7",
+		"key8": "val8",
+		"key9": "val9"
+	}
+}`
+)
 
 // Unit test of NewHeader.
 func TestNewHeader(t *testing.T) {
@@ -62,7 +86,9 @@ func TestHeader_Set_Error(t *testing.T) {
 	require.Equal(t, originalVal, received)
 }
 
-// Smoke test for Header.MarshalJSON.
+// Smoke test for Header.MarshalJSON. Checks basic marshaling outputs expected
+// data. Further edge checks that when given a key with a newline character, the
+// character is parsed as part of a string value and not as an escape character.
 func TestHeader_MarshalJSON(t *testing.T) {
 	// Initialize header object
 	head := NewHeader()
@@ -80,6 +106,19 @@ func TestHeader_MarshalJSON(t *testing.T) {
 
 	// Check that marshaled data
 	require.Equal(t, expectedJsonOutput, string(marshaledData))
+
+	// Edge check: Add a key with a newline character
+	key, val := "edgeCheckKey\n", "edgeCheckVal"
+	require.NoError(t, head.Set(key, val))
+
+	marshaledData, err = json.MarshalIndent(head, "", "\t")
+	require.NoError(t, err)
+
+	t.Logf("%s", marshaledData)
+
+	// Ensure it outputs a single line, ie the newline character does not
+	// create a multi-line JSON file.
+	require.Equal(t, expectedHeaderJsonNewline, string(marshaledData))
 }
 
 // Smoke test for Header.UnmarshalJSON. Ensures that

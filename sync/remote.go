9e066ae6ca18a447f4fa1ffcae04bbd3b9e859ee
joshemb <josh@elixxir.io>
2023-02-02 13:22:03 -0800

    Document sync.FilesystemRemoteStorage

--- sync/remote.go
+++ sync/remote.go
@@ -12,16 +12,31 @@ import (
 	"time"
 )
 
+// FilesystemRemoteStorage is a structure adhering to RemoteStore. This
+// utilizes the os.File IO operations.
 type FilesystemRemoteStorage struct{}
 
+// Read reads data from path. This will return an error if it fails to read
+// from the file path.
+//
+// This utilizes utils.ReadFile under the hood.
 func (f *FilesystemRemoteStorage) Read(path string) ([]byte, error) {
 	return utils.ReadFile(path)
 }
 
+// Write will write data to path. This will return an error if it fails to write.
+//
+// This utilizes utils.WriteFileDef under the hood.
 func (f *FilesystemRemoteStorage) Write(path string, data []byte) error {
 	return utils.WriteFileDef(path, data)
 }
 
-func (f *FilesystemRemoteStorage) GetLastModified(path string) (time.Time, error) {
+// GetLastModified will return the last modified timestamp of the file at path.
+// It will return an error if it cannot retrieve any os.FileInfo from the file
+// path.
+//
+// This utilizes utils.GetLastModified under the hood.
+func (f *FilesystemRemoteStorage) GetLastModified(path string) (
+	time.Time, error) {
 	return utils.GetLastModified(path)
 }

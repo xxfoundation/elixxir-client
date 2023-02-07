73b87fc42e3fa9ebf84ab1a0434a061ad569c782
joshemb <josh@elixxir.io>
2023-02-06 16:00:35 -0800

    Finalize tests for TransactionLog

--- sync/transactionLog_test.go
+++ sync/transactionLog_test.go
@@ -8,15 +8,18 @@
 package sync
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/rand"
+	"encoding/base64"
 	"github.com/stretchr/testify/require"
 	"os"
 	"sort"
 	"strconv"
 	"testing"
-	"time"
+)
+
+const (
+	expectedTransactionLogSerializedBase64 = `WFhES1RYTE9HSERSZXlKMlpYSnphVzl1SWpvd0xDSmxiblJ5YVdWeklqcDdmWDA9MCxBUUlEQkFVR0J3Z0pDZ3NNRFE0UEVCRVNFeFFWRmhjWWRQNGgycHEzWGVrT0tGX0YyeU1oTEY3bE9VaFYwb204Vm9kTXd2eU5abXpyM1RSOENVS2FuWFBTNTUzbUNaUHNFMVNqdDVaRGpIS21pT2FPZ1NsZC1YVl9GdEg2QTVvVGVsM0JaR1dhYXA1YXdKeG8xLEdSb2JIQjBlSHlBaElpTWtKU1luS0NrcUt5d3RMaTh3NnRzd09LRTlCZEZIVS1DT1FYVmp4WjhTNWt4MFJMbWk5ZVNZb0VfVUg1d0hXcnlmYm5VcF95TlRqVjV4bXdNMHZhdG9xOUxYclQ0V0VsSEZuOWstM0pUb1QyQWxReUdYZFhWRjR2Y3V5ckJEaW5QQjIsTVRJek5EVTJOemc1T2pzOFBUNF9RRUZDUTBSRlJrZElZeFkyUk5QRlN0OXI0NHJOS0JJUksxOVZMWlROOWsxN2pFOFM4LVRUWENrMXRtbm55bmlhZ2h3enB0M1cwTVQ5SFMwSUMwaGRxY3RNSGtIY1hDeWZmb2JzLXk0ajRTSlgwTE4yR0VpZ3N6OEw0MXBqMyxTVXBMVEUxT1QxQlJVbE5VVlZaWFdGbGFXMXhkWGw5Z3pKVDdVaGZ5Q0FVNEhnamEzS0RURm5pSlR2YzNQQzRLdkx4TGR1Vmt0eDlOOHNPR2x1RFEySFUyZzFKcndBMm9TNkxyQWM1dzhwTDVQanhHNzY2ak1KX3JpeDYtRGtINVhDWi12aVB5NTVZbXlnRzI0LFlXSmpaR1ZtWjJocGFtdHNiVzV2Y0hGeWMzUjFkbmQ0RV9yeU1yXzl0VDBPQkx5WVZtd2g5bnRmcW1JRHlfWEltcE56MTZxa05MVHRZbzVMN21vWXVFRnNHZ0RSMUJIc29NTm9pdTQ3c2VtMGk5QWQ4UWNlWW1yWmVxS3JjUVZKWFVXZHl5Uk5hX082bC10YTUsZVhwN2ZIMS1mNENCZ29PRWhZYUhpSW1LaTR5TmpvLVFJTjFEWURNZzZMZElWWjFPTkRJTTdGekhhdC1nbmlIS01JN04yajBlQndXeDlZeUlwQVFyRzBJdHVEQmYtRHoyUVdFalpYR0JfSC1fR3FINEpOellnZnE5eTFRRTdnRWdsZWpuOVpPc21WNGVTOUNx`
 )
 
 // Smoke test for NewTransactionLog.
@@ -48,12 +51,6 @@ func TestNewTransactionLog(t *testing.T) {
 	txLog := NewTransactionLog(localStore, remoteStore, hdr, rand.Reader,
 		logPath, deviceSecret)
 
-	// Construct reader/writer for the buffer
-	var (
-		writer = bufio.NewWriter(&bytes.Buffer{})
-		reader = bufio.NewReader(bytes.NewReader([]byte{}))
-	)
-
 	// Construct expected transaction log object
 	expected := &TransactionLog{
 		path:         logPath,
@@ -61,7 +58,7 @@ func TestNewTransactionLog(t *testing.T) {
 		remote:       remoteStore,
 		hdr:          hdr,
 		txs:          make([]Transaction, 0),
-		curBuf:       bufio.NewReadWriter(reader, writer),
+		curBuf:       &bytes.Buffer{},
 		deviceSecret: deviceSecret,
 		rng:          rand.Reader,
 	}
@@ -82,7 +79,7 @@ func TestTransactionLog_Insert(t *testing.T) {
 		require.NoError(t, os.RemoveAll(baseDir))
 
 	}()
-	
+
 	// Construct remote store
 	remoteStore := NewFileSystemRemoteStorage()
 
@@ -120,44 +117,50 @@ func TestTransactionLog_Insert(t *testing.T) {
 }
 
 func TestTransactionLog_Serialize(t *testing.T) {
-}
-
-// constructTimestamps is a testing utility function. It constructs a list of
-// out-of order mock timestamps.
-func constructTimestamps(t *testing.T) []time.Time {
-	var (
-		timestamp0, timestamp1, timestamp2, timestamp3, timestamp4,
-		timestamp5 time.Time
-		err error
-	)
-
-	// Construct timestamps. All of these are the same date but with different
-	// years.
-	timestamp0, err = time.Parse(time.RFC3339,
-		"2015-12-21T22:08:41+00:00")
+	// Construct local store
+	baseDir, password := "testDir", "password"
+	localStore, err := NewEkvLocalStore(baseDir, password)
 	require.NoError(t, err)
 
-	timestamp1, err = time.Parse(time.RFC3339,
-		"2013-12-21T22:08:41+00:00")
-	require.NoError(t, err)
+	// Delete the test file at the end
+	defer func() {
+		require.NoError(t, os.RemoveAll(baseDir))
 
-	timestamp2, err = time.Parse(time.RFC3339,
-		"2003-12-21T22:08:41+00:00")
-	require.NoError(t, err)
+	}()
 
-	timestamp3, err = time.Parse(time.RFC3339,
-		"2012-12-21T22:08:41+00:00")
-	require.NoError(t, err)
+	// Construct remote store
+	remoteStore := NewFileSystemRemoteStorage()
 
-	timestamp4, err = time.Parse(time.RFC3339,
-		"2014-12-21T22:08:41+00:00")
-	require.NoError(t, err)
+	// Construct header
+	hdr := NewHeader()
 
-	timestamp5, err = time.Parse(time.RFC3339,
-		"2001-12-21T22:08:41+00:00")
-	require.NoError(t, err)
+	// Construct log pth
+	logPath := baseDir + "/test.log"
 
-	return []time.Time{
-		timestamp0, timestamp1, timestamp2, timestamp3, timestamp4, timestamp5,
+	// Construct device secret
+	deviceSecret := []byte("deviceSecret")
+
+	// Construct transaction log
+	txLog := NewTransactionLog(localStore, remoteStore, hdr,
+		&CountingReader{count: 0}, logPath, deviceSecret)
+
+	// Construct timestamps
+	mockTimestamps := constructTimestamps(t)
+
+	for cnt, curTs := range mockTimestamps {
+		// Construct transaction
+		key, val := "key"+strconv.Itoa(cnt), "val"+strconv.Itoa(cnt)
+		newTx := NewTransaction(curTs, key, []byte(val))
+
+		// Insert transaction
+		txLog.append(newTx)
 	}
+
+	data, err := txLog.serialize()
+	require.NoError(t, err)
+
+	data64 := base64.StdEncoding.EncodeToString(data)
+
+	require.Equal(t, expectedTransactionLogSerializedBase64, data64)
+
 }
--- sync/utils_test.go
+++ sync/utils_test.go
@@ -1,1 +1,62 @@
 package sync
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+// CountingReader is a platform-independent deterministic RNG that adheres to
+// io.Reader.
+type CountingReader struct {
+	count uint8
+}
+
+// Read just counts until 254 then starts over again
+func (c *CountingReader) Read(b []byte) (int, error) {
+	for i := 0; i < len(b); i++ {
+		c.count = (c.count + 1) % 255
+		b[i] = c.count
+	}
+	return len(b), nil
+}
+
+// constructTimestamps is a testing utility function. It constructs a list of
+// out-of order mock timestamps.
+func constructTimestamps(t *testing.T) []time.Time {
+	var (
+		timestamp0, timestamp1, timestamp2, timestamp3, timestamp4,
+		timestamp5 time.Time
+		err error
+	)
+
+	// Construct timestamps. All of these are the same date but with different
+	// years.
+	timestamp0, err = time.Parse(time.RFC3339,
+		"2015-12-21T22:08:41+00:00")
+	require.NoError(t, err)
+
+	timestamp1, err = time.Parse(time.RFC3339,
+		"2013-12-21T22:08:41+00:00")
+	require.NoError(t, err)
+
+	timestamp2, err = time.Parse(time.RFC3339,
+		"2003-12-21T22:08:41+00:00")
+	require.NoError(t, err)
+
+	timestamp3, err = time.Parse(time.RFC3339,
+		"2012-12-21T22:08:41+00:00")
+	require.NoError(t, err)
+
+	timestamp4, err = time.Parse(time.RFC3339,
+		"2014-12-21T22:08:41+00:00")
+	require.NoError(t, err)
+
+	timestamp5, err = time.Parse(time.RFC3339,
+		"2001-12-21T22:08:41+00:00")
+	require.NoError(t, err)
+
+	return []time.Time{
+		timestamp0, timestamp1, timestamp2, timestamp3, timestamp4, timestamp5,
+	}
+}

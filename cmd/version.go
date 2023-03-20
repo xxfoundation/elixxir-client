3d22d9a8d45b6d788e0bf80c7a9a4e318e7012b9
Richard T. Carback III <rick.carback@gmail.com>
2023-03-20 20:54:29 +0000

    Bump version to v4.6.1

--- cmd/version.go
+++ cmd/version.go
@@ -18,7 +18,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "4.6.0"
+const currentVersion = "4.6.1"
 
 func Version() string {
 	out := fmt.Sprintf("Elixxir Client v%s -- %s\n\n", xxdk.SEMVER,

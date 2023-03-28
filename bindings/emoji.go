d74d7549229b5feed321a3305f03676d74c22e82
Jono Wenger <jono@elixxir.io>
2023-03-28 10:37:42 -0700

    Remove SupportedEmojisMap from bindings and deprecate SupportedEmojis

--- bindings/emoji.go
+++ bindings/emoji.go
@@ -46,45 +46,10 @@ import (
 //      "subgroup": "warning"
 //	  }
 //	]
+//
+// Deprecated: ValidateReaction no longer checks this list to validate emojis.
 func SupportedEmojis() ([]byte, error) {
 	return json.Marshal(emoji.SupportedEmojis())
-}
-
-// SupportedEmojisMap returns a map of emojis that are supported by the backend.
-//
-// Returns:
-//   - []byte - JSON of a map of emoji.Emoji.
-//
-// Example JSON:
-//
-//	{
-//	  "☹️": {
-//	    "character": "☹️",
-//	    "name": "frowning face",
-//	    "comment": "E0.7",
-//	    "codePoint": "2639 FE0F",
-//	    "group": "Smileys \u0026 Emotion",
-//	    "subgroup": "face-concerned"
-//	  },
-//	  "☺️": {
-//	    "character": "☺️",
-//	    "name": "smiling face",
-//	    "comment": "E0.6",
-//	    "codePoint": "263A FE0F",
-//	    "group": "Smileys \u0026 Emotion",
-//	    "subgroup": "face-affection"
-//	  },
-//	  "☢️": {
-//	    "character": "☢️",
-//	    "name": "radioactive",
-//	    "comment": "E1.0",
-//	    "codePoint": "2622 FE0F",
-//	    "group": "Symbols",
-//	    "subgroup": "warning"
-//	  },
-//	}
-func SupportedEmojisMap() ([]byte, error) {
-	return json.Marshal(emoji.SupportedEmojisMap())
 }
 
 // ValidateReaction checks that the reaction only contains a single grapheme

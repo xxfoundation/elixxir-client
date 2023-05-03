8e6f32dedf7972a87fb89c4d8d51056b55778911
joshemb <josh@elixxir.io>
2023-05-03 11:59:58 -0700

    Add example json for xxdk.CMIXParams

--- xxdk/params.go
+++ xxdk/params.go
@@ -7,12 +7,8 @@
 
 package xxdk
 
-// params.go define the high level parameters structures (which embed E2E and
-// CMIX params respectively) that are passed down into the core xxdk modules.
-
 import (
 	"encoding/json"
-
 	"gitlab.com/elixxir/client/v4/auth"
 	"gitlab.com/elixxir/client/v4/cmix"
 	"gitlab.com/elixxir/client/v4/e2e"
@@ -20,11 +16,70 @@ import (
 	"gitlab.com/elixxir/client/v4/e2e/rekey"
 )
 
+// params.go define the high level parameters structures (which embed E2E and
+// CMIX params respectively) that are passed down into the core xxdk modules.
+
 // CMIXParams contains the parameters for Network tracking and for specific CMIX
 // messaging settings.
 //
+// Example JSON:
+//	{
+//	  "Network": {
+//	    "TrackNetworkPeriod": 1000000000,
+//	    "MaxCheckedRounds": 500,
+//	    "RegNodesBufferLen": 1000,
+//	    "NetworkHealthTimeout": 15000000000,
+//	    "ParallelNodeRegistrations": 20,
+//	    "KnownRoundsThreshold": 1500,
+//	    "FastPolling": true,
+//	    "VerboseRoundTracking": false,
+//	    "RealtimeOnly": false,
+//	    "ReplayRequests": true,
+//	    "Rounds": {
+//	      "MaxHistoricalRounds": 100,
+//	      "HistoricalRoundsPeriod": 100000000,
+//	      "HistoricalRoundsBufferLen": 1000,
+//	      "MaxHistoricalRoundsRetries": 3
+//	    },
+//	  "Pickup": {
+//	    "NumMessageRetrievalWorkers": 2,
+//	    "LookupRoundsBufferLen": 100000,
+//	    "MaxHistoricalRoundsRetries": 2,
+//	    "UncheckRoundPeriod": 120000000000,
+//	    "ForceMessagePickupRetry": false,
+//	    "SendTimeout": 3000000000,
+//	    "RealtimeOnly": false,
+//	    "ForceHistoricalRounds": false
+//	  },
+//	  "Message": {
+//	    "MessageReceptionBuffLen": 500,
+//	    "MessageReceptionWorkerPoolSize": 2,
+//	    "MaxChecksInProcessMessage": 10,
+//	    "InProcessMessageWait": 900000000000,
+//	    "RealtimeOnly": false
+//	  },
+//	  "Historical": {
+//	    "MaxHistoricalRounds": 100,
+//	    "HistoricalRoundsPeriod": 100000000,
+//	    "HistoricalRoundsBufferLen": 1000,
+//	    "MaxHistoricalRoundsRetries": 3
+//	  },
+//	  "MaxParallelIdentityTracks": 5,
+//	  "EnableImmediateSending": false
+//	  },
+//	  "CMIX": {
+//	    "RoundTries": 10,
+//	    "Timeout": 45000000000,
+//	    "RetryDelay": 1000000000,
+//	    "SendTimeout": 3000000000,
+//	    "DebugTag": "External",
+//	    "BlacklistedNodes": {},
+//	    "Critical": false
+//	  }
+//	 }
+//
 // FIXME: this breakdown could be cleaner and is an unfortunate side effect of
-//        several refactors of the codebase.
+//	several refactors of the codebase.
 type CMIXParams struct {
 	Network cmix.Params
 	CMIX    cmix.CMIXParams

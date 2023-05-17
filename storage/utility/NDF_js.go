f9c42d906595d9e2329bee64992aeb101daa6835
Jake Taylor <jtaylorapps@gmail.com>
2023-05-17 12:37:57 -0500

    placed NDF storage for JS behind an interface

--- storage/utility/NDF_js.go
+++ storage/utility/NDF_js.go
@@ -16,15 +16,17 @@ import (
 
 const NdfStorageKeyNamePrefix = "ndfStorageKey/"
 
-var localStorage = js.Global().Get("localStorage")
+// stateKV is a global that allows switching out the storage backend for
+// certain KV operations. Defaults to backed by local storage.
+var stateKV = newLocalStorageState()
 
 func LoadNDF(_ *versioned.KV, key string) (*ndf.NetworkDefinition, error) {
-	keyValue := localStorage.Call("getItem", NdfStorageKeyNamePrefix+key)
-	if keyValue.IsNull() {
-		return nil, os.ErrNotExist
+	value, err := stateKV.Get(NdfStorageKeyNamePrefix + key)
+	if err != nil {
+		return nil, err
 	}
 
-	return ndf.Unmarshal([]byte(keyValue.String()))
+	return ndf.Unmarshal(value)
 }
 
 func SaveNDF(_ *versioned.KV, key string, ndf *ndf.NetworkDefinition) error {
@@ -33,8 +35,34 @@ func SaveNDF(_ *versioned.KV, key string, ndf *ndf.NetworkDefinition) error {
 		return err
 	}
 
-	localStorage.Call("setItem",
-		NdfStorageKeyNamePrefix+key, string(marshaled))
+	return stateKV.Set(NdfStorageKeyNamePrefix+key, marshaled)
+}
 
+// WebState defines an interface for setting permanent state in a KV format
+// specifically for web-based implementations.
+type WebState interface {
+	Get(key string) ([]byte, error)
+	Set(key string, value []byte) error
+}
+
+// localStorageState implements WebState backed by local storage.
+type localStorageState struct {
+	kv js.Value
+}
+
+func newLocalStorageState() WebState {
+	return &localStorageState{kv: js.Global().Get("localStorage")}
+}
+
+func (l *localStorageState) Get(key string) ([]byte, error) {
+	value := l.kv.Call("getItem", key)
+	if value.IsNull() {
+		return nil, os.ErrNotExist
+	}
+	return []byte(value.String()), nil
+}
+
+func (l *localStorageState) Set(key string, value []byte) error {
+	l.kv.Call("setItem", key, string(value))
 	return nil
 }

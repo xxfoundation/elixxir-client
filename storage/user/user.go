b98c924aa4aacbec6b3a5bcb85b901ef3946a125
Richard T. Carback III <rick.carback@gmail.com>
2023-05-30 21:11:53 +0000

    Revert user to use synchronized kv explicitly

--- storage/user/user.go
+++ storage/user/user.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"gitlab.com/elixxir/client/v4/collective"
 	"gitlab.com/elixxir/client/v4/storage/versioned"
 	"gitlab.com/elixxir/crypto/cyclic"
 	"gitlab.com/elixxir/crypto/rsa"
@@ -34,26 +35,36 @@ type User struct {
 }
 
 // builds a new user.
-func NewUser(syncKV versioned.KV, transmissionID, receptionID *id.ID, transmissionSalt,
+func NewUser(kv versioned.KV, transmissionID, receptionID *id.ID, transmissionSalt,
 	receptionSalt []byte, transmissionRsa, receptionRsa rsa.PrivateKey, isPrecanned bool,
 	e2eDhPrivateKey, e2eDhPublicKey *cyclic.Int) (*User, error) {
+
+	remote, err := kv.Prefix(collective.StandardRemoteSyncPrefix)
+	if err != nil {
+		return nil, err
+	}
 
 	ci := newCryptographicIdentity(transmissionID, receptionID,
 		transmissionSalt, receptionSalt, transmissionRsa,
 		receptionRsa, isPrecanned,
-		e2eDhPrivateKey, e2eDhPublicKey, syncKV)
+		e2eDhPrivateKey, e2eDhPublicKey, kv)
 
-	return &User{CryptographicIdentity: ci, kv: syncKV}, nil
+	return &User{CryptographicIdentity: ci, kv: remote}, nil
 }
 
-func LoadUser(syncKV versioned.KV) (*User, error) {
-	ci, err := loadCryptographicIdentity(syncKV)
+func LoadUser(kv versioned.KV) (*User, error) {
+	remote, err := kv.Prefix(collective.StandardRemoteSyncPrefix)
+	if err != nil {
+		return nil, err
+	}
+
+	ci, err := loadCryptographicIdentity(kv)
 	if err != nil {
 		return nil, errors.WithMessage(err, "Failed to load user "+
 			"due to failure to load cryptographic identity")
 	}
 
-	u := &User{CryptographicIdentity: ci, kv: syncKV}
+	u := &User{CryptographicIdentity: ci, kv: remote}
 	u.loadTransmissionRegistrationValidationSignature()
 	u.loadReceptionRegistrationValidationSignature()
 	u.loadUsername()

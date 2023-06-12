a68dc7f0e6a393f892afa1690beca3be11be8489
Jono Wenger <jono@elixxir.io>
2023-06-12 14:10:17 -0700

    Add back BlockedTimestamp since it is not completely removed yet

--- dm/views.go
+++ dm/views.go
@@ -7,9 +7,14 @@
 
 package dm
 
+import "time"
+
 type ModelConversation struct {
 	Pubkey         []byte `json:"pub_key"`
 	Nickname       string `json:"nickname"`
 	Token          uint32 `json:"token"`
 	CodesetVersion uint8  `json:"codeset_version"`
+
+	// Deprecated: KV is the source of truth for blocked users.
+	BlockedTimestamp *time.Time `json:"blocked_timestamp"`
 }

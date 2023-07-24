51a5cf55e51834a1fb728b015a2caa8e94179834
Jono Wenger <jono@elixxir.io>
2023-07-24 11:22:43 -0700

    Fix TestParams_MarshalUnmarshal

--- connect/params_test.go
+++ connect/params_test.go
@@ -15,37 +15,34 @@ import (
 	"gitlab.com/elixxir/client/v4/xxdk"
 )
 
-func TestParams_MarshalUnmarshal(t *testing.T) {
+// Tests that xxdk.E2EParams can be JSON marshalled and unmarshalled.
+func TestE2EParams_JsonMarshalUnmarshal(t *testing.T) {
 	// Construct a set of params
-	p := xxdk.GetDefaultE2EParams()
+	expected := xxdk.GetDefaultE2EParams()
 
 	// Marshal the params
-	data, err := json.Marshal(&p)
+	data, err := json.Marshal(expected)
 	if err != nil {
-		t.Fatalf("Marshal error: %v", err)
+		t.Fatalf("Failed to JSON marshal E2EParams: %+v", err)
 	}
 
-	t.Logf("%s", string(data))
-
 	// Unmarshal the params object
-	received := xxdk.E2EParams{}
+	var received xxdk.E2EParams
 	err = json.Unmarshal(data, &received)
 	if err != nil {
-		t.Fatalf("Unmarshal error: %v", err)
+		t.Fatalf("Failed to JSON unmarshal E2EParams: %+v", err)
 	}
 
 	// Re-marshal this params object
 	data2, err := json.Marshal(received)
 	if err != nil {
-		t.Fatalf("Marshal error: %v", err)
+		t.Fatalf("Failed to re-JSON marshal E2EParams: %+v", err)
 	}
 
-	t.Logf("%s", string(data2))
-
-	// Check that they match (it is done this way to avoid
-	// false failures with the reflect.DeepEqual function and
-	// pointers)
+	// Check that the JSON matches (this is done to avoid false failures with
+	// the reflect.DeepEqual function and pointers)
 	if !bytes.Equal(data, data2) {
-		t.Fatalf("Data was lost in marshal/unmarshal.")
+		t.Fatalf("Data was lost in JSON marshal/unmarshal."+
+			"\nexpected: %s\nreceived: %s", data, data2)
 	}
 }

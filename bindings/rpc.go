52017f857b7367c8d42e437cb857af076ec29bd4
Richard T. Carback III <rick.carback@gmail.com>
2024-06-17 13:36:24 -0400

    Add RPC Server functions

--- bindings/rpc.go
+++ bindings/rpc.go
@@ -16,10 +16,31 @@ import (
 	"gitlab.com/xx_network/primitives/id"
 )
 
+// Response interface for RPC responses from the [Send]
+// function. Provides for the ability to call a callback on error or
+// success or to listen on the channels directly.
 type RPCResponse interface {
+	// Callback calls success or error function if/when the
+	// response is received. Meant to be very similar to Promise/async
+	// found in other languages.
+	// RPC will call respFn 3 times: sent, round finished, and
+	// response received.
 	Callback(responseFn func(response []byte),
-		errorFn func(errorStr string))
+		errorFn func(errorStr string)) RPCResponse
+	// Wait waits until the response is complete returns the final
+	// response bytes
 	Wait() []byte
+}
+
+// RPCServer is the interface for the RPC server, users should
+// add a callback then start listening for RPC requests.
+type RPCServer interface {
+	// Start listening on the server's cMix Identity
+	Start()
+	// Stop listening on the server's cMix Identity
+	// Note: if you want to stop all network traffic, do
+	// so with the cMix network client.
+	Stop()
 }
 
 // RPCSend sends an RPC `request` to the provided server `recipient` and
@@ -57,17 +78,96 @@ type rpcResponse struct {
 }
 
 func (r *rpcResponse) Callback(responseFn func(response []byte),
-	errorFn func(errorStr string)) {
+	errorFn func(errorStr string)) RPCResponse {
 	if r.err != nil {
 		errorFn(fmt.Sprintf("%+v", r.err))
-		return
+		return r
 	}
 	r.response.Callback(responseFn,
 		func(err error) {
 			errorFn(fmt.Sprintf("%+v", err))
 		})
+	return r
 }
 
 func (r *rpcResponse) Wait() []byte {
 	return r.response.Wait()
+}
+
+// NewRPCServer returns a new RPC server with the specified
+// reception ID and private Key
+func NewRPCServer(cMixID int, callbackFn func(sender, request []byte) []byte,
+	receptionID, privateKey []byte) (RPCServer, error) {
+	net, err := cmixTrackerSingleton.get(cMixID)
+	if err != nil {
+		return nil, err
+	}
+
+	serverID, err := id.Unmarshal(receptionID)
+	if err != nil {
+		return nil, err
+	}
+
+	serverKey := ecdh.ECDHNIKE.NewEmptyPrivateKey()
+	err = serverKey.FromBytes(privateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	cbFn := func(id *id.ID, request []byte) []byte {
+		idBytes := id.Marshal()
+		return callbackFn(idBytes, request)
+	}
+
+	net.EKVSet("rpcServerID", receptionID)
+	net.EKVSet("rpcServerKey", privateKey)
+
+	server := rpc.NewServer(net.api.GetCmix(), cbFn, serverID, serverKey)
+	return server, nil
+}
+
+// GenerateRandomReceptionID generates a random well formed reception id
+func GenerateRandomReceptionID(cMixID int) ([]byte, error) {
+	net, err := cmixTrackerSingleton.get(cMixID)
+	if err != nil {
+		return nil, err
+	}
+	newID, err := rpc.GenerateRandomID(net.api.GetCmix())
+	if err != nil {
+		return nil, err
+	}
+	return newID.Marshal(), nil
+}
+
+// GenerateRandomRPCKey generates a random private key for a server
+func GenerateRandomRPCKey(cMixID int) ([]byte, error) {
+	net, err := cmixTrackerSingleton.get(cMixID)
+	if err != nil {
+		return nil, err
+	}
+
+	rng := net.api.GetRng().GetStream()
+	defer rng.Close()
+	prk, _ := ecdh.ECDHNIKE.NewKeypair(rng)
+	return prk.Bytes(), nil
+}
+
+// LoadRPCServer load key and id from disk and return an RPC server
+func LoadRPCServer(cMixID int, callbackFn func(sender, request []byte) []byte) (
+	RPCServer, error) {
+	net, err := cmixTrackerSingleton.get(cMixID)
+	if err != nil {
+		return nil, err
+	}
+
+	serverID, err := net.EKVGet("rpcServerID")
+	if err != nil {
+		return nil, err
+	}
+	serverKey, err := net.EKVGet("rpcServerKey")
+	if err != nil {
+		return nil, err
+	}
+
+	return NewRPCServer(cMixID, callbackFn, serverID, serverKey)
 }

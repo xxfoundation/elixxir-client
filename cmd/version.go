55e9cb55b87147ba7a2ec100916fb5f3e68b9b38
Richard T. Carback III <rick.carback@gmail.com>
2025-11-11 01:09:04 -0500

    Bump to v4.7.8

--- cmd/version.go
+++ cmd/version.go
@@ -18,7 +18,7 @@ import (
 )
 
 // Change this value to set the version for this build
-const currentVersion = "4.7.7"
+const currentVersion = "4.7.8"
 
 func Version() string {
 	out := fmt.Sprintf("Elixxir Client v%s -- %s\n\n", xxdk.SEMVER,
